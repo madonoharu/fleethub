9ad2bc9e0e23b44bd35f9cda311e049a9d75d5e1
まどの <madonoharu@gmail.com>
2020-04-08 16:13:20 +0900

    add createShallowEqualSelector

--- packages/client/src/utils/index.ts
+++ packages/client/src/utils/index.ts
@@ -1,4 +1,18 @@
 import { isNonNullable } from "@fleethub/kcsim"
+import { EntityId } from "@reduxjs/toolkit"
+import { createSelectorCreator, defaultMemoize } from "reselect"
+import { shallowEqual } from "react-redux"
+
+export const createShallowEqualSelector = createSelectorCreator(defaultMemoize, shallowEqual)
+
+export const isEntityId = (id: unknown): id is EntityId => {
+  switch (typeof id) {
+    case "string":
+    case "number":
+      return true
+  }
+  return false
+}
 
 let uidCount = 0
 export const getUid = () => `${uidCount++}`
