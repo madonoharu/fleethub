b7882c6aabe8e107208d84e88153f85d80a8e653
まどの <madonoharu@gmail.com>
2020-04-10 13:29:15 +0900

    update ship select

--- packages/client/src/store/shipSelectSlice.ts
+++ packages/client/src/store/shipSelectSlice.ts
@@ -1,17 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
 import { ShipPosition } from "./entities"
 
 export type ShipSelectState = {
+  abyssal: boolean
+  shipTypeFilter: string
   position?: ShipPosition
 }
 
-const initialState: ShipSelectState = {}
+const initialState: ShipSelectState = {
+  abyssal: false,
+  shipTypeFilter: "戦艦級",
+}
 
 export default createSlice({
   name: "shipSelect",
   initialState,
   reducers: {
-    set: (state, { payload }: PayloadAction<ShipSelectState>) => {
+    set: (state, { payload }: PayloadAction<Partial<ShipSelectState>>) => {
       Object.assign(state, payload)
     },
   },
