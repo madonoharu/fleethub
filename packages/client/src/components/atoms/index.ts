09d54ed5535166c43ae35e27aadfa6bccffe1cda
まどの <madonoharu@gmail.com>
2020-04-10 18:29:08 +0900

    move ShipBanner

--- packages/client/src/components/atoms/index.ts
+++ packages/client/src/components/atoms/index.ts
@@ -4,6 +4,8 @@ export { default as Select } from "./Select"
 export { default as Slider } from "./Slider"
 export { default as NumberInput } from "./NumberInput"
 export { default as Text } from "./Text"
+export { default as Divider } from "./Divider"
 
+export { default as ShipBanner } from "./ShipBanner"
 export { default as GearIcon } from "./GearIcon"
 export { default as StatIcon } from "./StatIcon"
