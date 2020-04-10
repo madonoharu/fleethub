32f156d5107068f4e7e267e91b8fd45cfbf12c01
まどの <madonoharu@gmail.com>
2020-04-10 19:44:19 +0900

    update

--- packages/client/src/components/templates/GlobalDialogs/index.tsx
+++ packages/client/src/components/templates/GlobalDialogs/index.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Dialog as MuiDialog, DialogProps } from "@material-ui/core"
+import { Dialog as MuiDialog, DialogProps, useTheme } from "@material-ui/core"
 
 import { useShipSelect, useGearSelect } from "../../../hooks"
 import { ShipSelect, GearSelect } from "../../../components"
 
 const Dialog: React.FC<DialogProps> = ({ children, ...rest }) => {
+  const theme = useTheme()
+  const width = theme.breakpoints.width("md")
   return (
-    <MuiDialog maxWidth="md" PaperProps={{ style: { height: "80vh" } }} {...rest}>
+    <MuiDialog maxWidth="md" PaperProps={{ style: { height: "80vh", width, padding: 8 } }} {...rest}>
       <div>{children}</div>
     </MuiDialog>
   )
