239f3c6d066d3befeee90c03beccc0ab4043c17b
まどの <madonoharu@gmail.com>
2020-04-10 18:29:40 +0900

    add Divider

---
+++ packages/client/src/components/atoms/Divider/index.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Typography, Divider as MuiDivider, DividerProps } from "@material-ui/core"
+
+const StyledDivider = styled(MuiDivider)`
+  flex-grow: 1;
+  margin-left: 8px;
+`
+
+type Props = DividerProps & {
+  label: React.ReactNode
+}
+
+const Divider: React.FCX<Props> = ({ className, label, ...muiProps }) => {
+  return (
+    <div className={className}>
+      <Typography variant="caption" color="textSecondary">
+        {label}
+      </Typography>
+      <StyledDivider {...muiProps} />
+    </div>
+  )
+}
+
+export default styled(Divider)`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`
