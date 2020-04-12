92257a007df0b49449e2dae99a79fe3b95b94a10
まどの <madonoharu@gmail.com>
2020-04-12 09:03:38 +0900

    update GearTooltip

--- packages/client/src/components/organisms/GearTooltip/index.tsx
+++ packages/client/src/components/organisms/GearTooltip/index.tsx
@@ -2,40 +2,11 @@ import React from "react"
 import { GearBase } from "@fleethub/core"
 import styled from "styled-components"
 
-import { Tooltip, Box, Table as MuiTable, TableBody, TableRow, TableCell as MuiTableCell } from "@material-ui/core"
+import { Tooltip, Box } from "@material-ui/core"
 
-import { Flexbox, Text, GearNameplate, StatIcon } from "../../../components"
-import { StatKeyDictionary } from "../../../utils"
+import { GearNameplate } from "../../../components"
 
-const Table = styled(MuiTable)`
-  width: auto;
-`
-
-const TableCell = styled(MuiTableCell)`
-  padding: 0 4px;
-  border: none;
-  font-size: 0.75rem;
-`
-
-const keys = [
-  "armor",
-  "firepower",
-  "torpedo",
-  "speed",
-  "bombing",
-  "antiAir",
-  "asw",
-  "accuracy",
-  "interception",
-  "evasion",
-  "antiBomber",
-  "los",
-  "luck",
-
-  "range",
-  "radius",
-  "cost",
-] as const
+import GearStatList from "./GearStatList"
 
 type Props = {
   gear: GearBase
@@ -43,8 +14,6 @@ type Props = {
 }
 
 const GearTooltip: React.FC<Props> = ({ gear, ...rest }) => {
-  const stats = keys.map((key) => [key, gear[key]] as const).filter(([key, stat]) => stat !== 0)
-
   return (
     <Tooltip
       enterDelay={300}
@@ -52,23 +21,7 @@ const GearTooltip: React.FC<Props> = ({ gear, ...rest }) => {
       title={
         <Box>
           <GearNameplate wrap size="small" iconId={gear.iconId} name={gear.name} />
-          <Table size="small">
-            <TableBody>
-              <TableRow>
-                <TableCell>ID</TableCell>
-                <TableCell>{gear.gearId}</TableCell>
-              </TableRow>
-              {stats.map(([key, value]) => (
-                <TableRow key={key}>
-                  <TableCell>
-                    <StatIcon icon={key} />
-                  </TableCell>
-                  <TableCell>{StatKeyDictionary[key]}</TableCell>
-                  <TableCell>{value}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <GearStatList gear={gear} />
         </Box>
       }
       {...rest}
