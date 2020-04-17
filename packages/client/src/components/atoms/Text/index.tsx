0ac558490d8510fd6fbbe514a6828c45876c94d1
まどの <madonoharu@gmail.com>
2020-04-17 10:31:43 +0900

    Add color

--- packages/client/src/components/atoms/Text/index.tsx
+++ packages/client/src/components/atoms/Text/index.tsx
@@ -1,11 +1,17 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { DefaultTheme } from "styled-components"
 
-import Typography from "@material-ui/core/Typography"
+import { Typography, TypographyProps } from "@material-ui/core"
 
-const Component = styled(Typography)`
+type Props = Omit<TypographyProps, "color"> & {
+  color?: keyof DefaultTheme["kc"]["palette"]
+}
+
+const Text: React.FC<Props> = ({ color, ...muiProps }) => <Typography {...muiProps} />
+
+export default styled(Text)`
   font-size: 0.75rem;
   line-height: 1.66;
-`
 
-export default Component
+  color: ${({ theme, color }) => color && theme.kc.palette[color]};
+`
