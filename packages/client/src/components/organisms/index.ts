31f2934f73af5bd0963edc4901b3812425c5b670
まどの <madonoharu@gmail.com>
2020-04-17 12:22:25 +0900

    Rename ShipControlCard -> ShipCard

--- packages/client/src/components/organisms/index.ts
+++ packages/client/src/components/organisms/index.ts
@@ -2,7 +2,7 @@ export { default as DialogButton } from "./DialogButton"
 
 export { default as GearTooltip } from "./GearTooltip"
 export { default as GearLabel } from "./GearLabel"
-export { default as ShipControlCard } from "./ShipControlCard"
+export { default as ShipCard } from "./ShipCard"
 export { default as EquipmentSlotList } from "./EquipmentSlotList"
 
 export { default as AppBar } from "./AppBar"
--- packages/client/src/components/templates/FleetPanel/index.tsx
+++ packages/client/src/components/templates/FleetPanel/index.tsx
@@ -4,7 +4,7 @@ import { EntityId } from "@reduxjs/toolkit"
 
 import { Container, Typography, TextField } from "@material-ui/core"
 
-import { RemoveButton, Select } from "../../../components"
+import { RemoveButton, Flexbox, SelectButtons } from "../../../components"
 import { useFleet } from "../../../hooks"
 
 import ShipList from "./ShipList"
@@ -17,25 +17,37 @@ type Props = {
 const FleetPanel: React.FC<Props> = ({ className, uid }) => {
   const { entity, actions } = useFleet(uid)
 
+  const [fleetIndex, setFleetIndex] = React.useState(0)
+
   if (!entity) {
     return <Typography color="error">error</Typography>
   }
 
   return (
     <Container className={className} maxWidth="md">
-      <TextField
-        placeholder="編成名"
-        value={entity.name}
-        onChange={(event) => actions.update({ name: event.currentTarget.value })}
-      />
+      <Flexbox>
+        <TextField
+          placeholder="編成名"
+          value={entity.name}
+          onChange={(event) => actions.update({ name: event.currentTarget.value })}
+        />
+        <RemoveButton onClick={actions.remove} />
+      </Flexbox>
+
+      <SelectButtons value={fleetIndex} options={[0, 1]} onChange={setFleetIndex} />
       <div>
-        fleet {uid} <RemoveButton onClick={actions.remove} />
-      </div>
-      <div>
-        <Typography>第一</Typography>
-        <ShipList fleet={uid} ships={entity.main} role="main" onAdd={actions.openShipSelect} />
-        <Typography>第二</Typography>
-        <ShipList fleet={uid} ships={entity.escort} role="escort" onAdd={actions.openShipSelect} />
+        {fleetIndex === 0 && (
+          <>
+            <Typography>第一</Typography>
+            <ShipList fleet={uid} ships={entity.main} role="main" onAdd={actions.openShipSelect} />
+          </>
+        )}
+        {fleetIndex === 1 && (
+          <>
+            <Typography>第二</Typography>
+            <ShipList fleet={uid} ships={entity.escort} role="escort" onAdd={actions.openShipSelect} />
+          </>
+        )}
       </div>
     </Container>
   )
