d673b1ed02dda0ae1e770192038d53330a47723d
まどの <madonoharu@gmail.com>
2020-04-17 19:12:37 +0900

    Add mapValues

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -2,6 +2,17 @@ export const softcap = (cap: number, value: number) => (value <= cap ? value : c
 
 export const isNonNullable = <T>(item: T): item is NonNullable<T> => item !== undefined && item !== null
 
+export const mapValues = <T, R>(obj: T, fn: (value: T[keyof T], key: keyof T) => R): Record<keyof T, R> => {
+  const nextObj: Record<string, R> = {}
+
+  for (const key in obj) {
+    const value = obj[key]
+    nextObj[key] = fn(value, key)
+  }
+
+  return nextObj as Record<keyof T, R>
+}
+
 export type NullableArray<T> = Array<T | undefined>
 export type PickByValue<T, ValueType> = Pick<T, { [Key in keyof T]-?: T[Key] extends ValueType ? Key : never }[keyof T]>
 
