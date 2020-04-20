68df41a62edfb4fec9f54aaecac04434c51508a5
まどの <madonoharu@gmail.com>
2020-04-20 22:56:57 +0900

    Improve constructor

--- packages/core/src/gear/MasterGear.ts
+++ packages/core/src/gear/MasterGear.ts
@@ -13,38 +13,34 @@ export interface GearBase extends Omit<Required<GearData>, "id"> {
 }
 
 export default class MasterGear implements GearBase, Required<GearData> {
-  public readonly id = 0
+  public readonly id = this.data.id || 0
+  public readonly category = this.data.category || 0
+  public readonly iconId = this.data.iconId || 0
+  public readonly name = this.data.name || ""
 
-  public readonly category = 0
-  public readonly iconId = 0
-  public readonly name = ""
+  public readonly firepower = this.data.firepower || 0
+  public readonly torpedo = this.data.torpedo || 0
+  public readonly antiAir = this.data.antiAir || 0
+  public readonly bombing = this.data.bombing || 0
+  public readonly asw = this.data.asw || 0
+  public readonly accuracy = this.data.accuracy || 0
+  public readonly evasion = this.data.evasion || 0
+  public readonly antiBomber = this.data.antiBomber || 0
+  public readonly interception = this.data.interception || 0
+  public readonly los = this.data.los || 0
+  public readonly armor = this.data.armor || 0
+  public readonly range = this.data.range || 0
+  public readonly radius = this.data.radius || 0
+  public readonly cost = this.data.cost || 0
+  public readonly improvable = this.data.improvable || false
 
-  public readonly firepower = 0
-  public readonly torpedo = 0
-  public readonly antiAir = 0
-  public readonly bombing = 0
-  public readonly asw = 0
-  public readonly accuracy = 0
-  public readonly evasion = 0
-  public readonly antiBomber = 0
-  public readonly interception = 0
-  public readonly los = 0
-  public readonly armor = 0
-
-  public readonly range = 0
-  public readonly radius = 0
-  public readonly cost = 0
-
-  public readonly improvable = false
-
-  public readonly hp = 0
-  public readonly speed = 0
-  public readonly luck = 0
+  public readonly hp = this.data.hp || 0
+  public readonly speed = this.data.speed || 0
+  public readonly luck = this.data.luck || 0
 
   public readonly attrs: GearAttribute[]
 
-  constructor(data: Partial<GearData>) {
-    Object.assign(this, data)
+  constructor(private data: Partial<GearData>) {
     this.attrs = createGearAttrs(this)
   }
 
