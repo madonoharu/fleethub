e8fb86f784c44410df2e914c95a6df8eef6b198b
まどの <madonoharu@gmail.com>
2020-04-21 21:19:08 +0900

    Add RateMap

---
+++ packages/core/src/attacks/RateMap.ts
@@ -0,0 +1,17 @@
+export default class RateMap<Key> extends Map<Key, number> {
+  constructor() {
+    super()
+  }
+
+  get total() {
+    let total = 0
+    for (const value of this.values()) {
+      total += value
+    }
+    return total
+  }
+
+  get complement() {
+    return 1 - this.total
+  }
+}
