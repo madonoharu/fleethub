976079ec9bdee819904fe86386f80be83fac00b0
まどの <madonoharu@gmail.com>
2020-04-22 20:02:46 +0900

    Remove softcap

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,5 +1,3 @@
-export const softcap = (cap: number, value: number) => (value <= cap ? value : cap + Math.sqrt(value - cap))
-
 export const isNonNullable = <T>(item: T): item is NonNullable<T> => item !== undefined && item !== null
 
 export const isString = (value: unknown): value is string => typeof value === "string"
