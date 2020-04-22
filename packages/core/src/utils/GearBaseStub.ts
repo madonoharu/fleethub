8b8a38dabd8e37364040858d6dfd729301c6869f
まどの <madonoharu@gmail.com>
2020-04-22 23:55:21 +0900

    Add GearBaseStub

---
+++ packages/core/src/utils/GearBaseStub.ts
@@ -0,0 +1,39 @@
+import { GearBase, GearAttribute } from "../gear"
+import { GearCategoryKey, GearCategory } from "@fleethub/data"
+
+export class GearBaseStub implements GearBase {
+  public static from = (src: Partial<GearBase>) => Object.assign(new GearBaseStub(), src)
+
+  public gearId = 0
+  public category = 0
+  public iconId = 0
+  public name = ""
+  public firepower = 0
+  public torpedo = 0
+  public antiAir = 0
+  public bombing = 0
+  public asw = 0
+  public accuracy = 0
+  public evasion = 0
+  public antiBomber = 0
+  public interception = 0
+  public los = 0
+  public armor = 0
+  public range = 0
+  public radius = 0
+  public cost = 0
+  public improvable = false
+  public hp = 0
+  public speed = 0
+  public luck = 0
+  public attrs: GearAttribute[] = []
+  public specialCategory = 0
+
+  public is = (attr: GearAttribute) => this.attrs.includes(attr)
+
+  public in = (...attrs: GearAttribute[]) => attrs.some(this.is)
+
+  public categoryIs = (key: GearCategoryKey) => this.category === GearCategory[key]
+
+  public categoryIn = (...keys: GearCategoryKey[]) => keys.some(this.categoryIs)
+}
