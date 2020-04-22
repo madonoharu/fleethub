bd516c87a867509254499759b65d8a1463c33be0
まどの <madonoharu@gmail.com>
2020-04-22 23:51:09 +0900

    Fix

--- packages/core/src/gear/index.ts
+++ packages/core/src/gear/index.ts
@@ -1,3 +1,4 @@
 export { default as MasterGear, GearBase } from "./MasterGear"
 export { Gear, GearState, GearImpl } from "./Gear"
 export { default as Proficiency } from "./Proficiency"
+export { GearAttribute } from "./GearAttribute"
