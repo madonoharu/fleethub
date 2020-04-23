61a77d512d06743a767ebb95e3e2606410e2c13b
まどの <madonoharu@gmail.com>
2020-04-23 23:30:11 +0900

    Update Improvements

---
+++ packages/core/src/gear/Improvement.test.ts
@@ -0,0 +1,167 @@
+import { createImprovement, Improvement } from "./Improvement"
+
+import { GearName } from "@fleethub/data"
+import { range } from "lodash-es"
+
+import { GearBaseStub } from "../utils/GearBaseStub"
+import { makeGear } from "../utils/testUtils"
+import { GearBase } from "./MasterGear"
+
+type Case = [GearName, number, number]
+
+const starsRange = range(11)
+
+const expectMultiplierSqrtStars = (getBonus: (stars: number) => number, multiplier: number) => {
+  const bonuses = starsRange.map(getBonus)
+  const expected = starsRange.map((stars) => multiplier * Math.sqrt(stars))
+  expect(bonuses).toEqual(expected)
+}
+
+const expectMultiplierStars = (getBonus: (stars: number) => number, multiplier: number) => {
+  const bonuses = starsRange.map(getBonus)
+  const expected = starsRange.map((stars) => multiplier * stars)
+  expect(bonuses).toEqual(expected)
+}
+
+describe("createImprovement", () => {
+  it("デフォルトの戻り値は0", () => {
+    const gear = new GearBaseStub()
+    expect(createImprovement(gear, 0)).toEqual<ReturnType<typeof createImprovement>>({
+      contactSelectionBonus: 0,
+      fighterPowerBonus: 0,
+      adjustedAntiAirBonus: 0,
+      fleetAntiAirBonus: 0,
+      shellingPowerBonus: 0,
+    })
+  })
+
+  describe("contactSelectionBonus", () => {
+    it.each<[GearName, number]>([
+      ["二式艦上偵察機", 0.25],
+      ["試製景雲(艦偵型)", 0.4],
+      ["零式水上観測機", 0.2],
+      ["零式水上偵察機", 0.14],
+      ["Ro.43水偵", 0.14],
+      ["九八式水上偵察機(夜偵)", 0.1],
+    ])("%s -> %s * ☆", (name, expected) => {
+      const gear = makeGear(name)
+      expectMultiplierStars((stars) => createImprovement(gear, stars).contactSelectionBonus, expected)
+    })
+  })
+
+  describe("fighterPowerBonus ", () => {
+    it("戦闘機 -> 0.2 * ☆", () => {
+      const gear = GearBaseStub.fromAttrs("Fighter")
+      const expected = 0.2
+
+      expectMultiplierStars((stars) => createImprovement(gear, stars).fighterPowerBonus, expected)
+    })
+
+    it("爆戦 -> 0.25 * ☆", () => {
+      const gear = GearBaseStub.fromAttrs("FighterBomber")
+      const expected = 0.25
+
+      expectMultiplierStars((stars) => createImprovement(gear, stars).fighterPowerBonus, expected)
+    })
+
+    it("陸攻 -> 0.5 * sqrt(☆)", () => {
+      const gear = GearBaseStub.fromCategory("LandBasedAttackAircraft")
+      const expected = 0.5
+
+      expectMultiplierSqrtStars((stars) => createImprovement(gear, stars).fighterPowerBonus, expected)
+    })
+  })
+
+  describe("adjustedAntiAirBonus", () => {
+    it("対空8以上の対空機銃 -> 6 * sqrt(☆)", () => {
+      const gear = GearBaseStub.fromCategory("AntiAircraftGun")
+      gear.antiAir = 8
+      const expected = 6
+
+      expectMultiplierSqrtStars((stars) => createImprovement(gear, stars).adjustedAntiAirBonus, expected)
+    })
+
+    it("対空7以下の対空機銃 -> 4 * sqrt(☆)", () => {
+      const gear = GearBaseStub.fromCategory("AntiAircraftGun")
+      gear.antiAir = 7
+      const expected = 4
+
+      expectMultiplierSqrtStars((stars) => createImprovement(gear, stars).adjustedAntiAirBonus, expected)
+    })
+
+    it("対空8以上の高射装置,高角砲 -> 3 * sqrt(☆)", () => {
+      const aafd = GearBaseStub.fromCategory("AntiAircraftFireDirector")
+      aafd.antiAir = 8
+      const ham = GearBaseStub.fromAttrs("HighAngleMount")
+      ham.antiAir = 8
+
+      const expected = 3
+
+      expectMultiplierSqrtStars((stars) => createImprovement(aafd, stars).adjustedAntiAirBonus, expected)
+      expectMultiplierSqrtStars((stars) => createImprovement(ham, stars).adjustedAntiAirBonus, expected)
+    })
+
+    it("対空7以下の高射装置,高角砲 -> 2 * sqrt(☆)", () => {
+      const aafd = GearBaseStub.fromCategory("AntiAircraftFireDirector")
+      aafd.antiAir = 7
+      const ham = GearBaseStub.fromAttrs("HighAngleMount")
+      ham.antiAir = 7
+
+      const expected = 2
+
+      expectMultiplierSqrtStars((stars) => createImprovement(aafd, stars).adjustedAntiAirBonus, expected)
+      expectMultiplierSqrtStars((stars) => createImprovement(ham, stars).adjustedAntiAirBonus, expected)
+    })
+  })
+
+  describe("fleetAntiAirBonus", () => {
+    it("対空8以上の高射装置,高角砲 -> 3 * sqrt(☆)", () => {
+      const aafd = GearBaseStub.fromCategory("AntiAircraftFireDirector")
+      aafd.antiAir = 8
+      const ham = GearBaseStub.fromAttrs("HighAngleMount")
+      ham.antiAir = 8
+
+      const expected = 3
+
+      expectMultiplierSqrtStars((stars) => createImprovement(aafd, stars).fleetAntiAirBonus, expected)
+      expectMultiplierSqrtStars((stars) => createImprovement(ham, stars).fleetAntiAirBonus, expected)
+    })
+
+    it("対空7以下の高射装置,高角砲 -> 2 * sqrt(☆)", () => {
+      const aafd = GearBaseStub.fromCategory("AntiAircraftFireDirector")
+      aafd.antiAir = 7
+      const ham = GearBaseStub.fromAttrs("HighAngleMount")
+      ham.antiAir = 7
+
+      const expected = 2
+
+      expectMultiplierSqrtStars((stars) => createImprovement(aafd, stars).fleetAntiAirBonus, expected)
+      expectMultiplierSqrtStars((stars) => createImprovement(ham, stars).fleetAntiAirBonus, expected)
+    })
+
+    it("対空1以上の電探 -> 1.5 * sqrt(☆)", () => {
+      const gear = GearBaseStub.fromAttrs("Radar")
+      gear.antiAir = 1
+
+      const expected = 1.5
+
+      expectMultiplierSqrtStars((stars) => createImprovement(gear, stars).fleetAntiAirBonus, expected)
+    })
+  })
+
+  describe("shellingPowerBonus", () => {
+    it("火力13以上 -> 1.5 * sqrt(☆)", () => {
+      const fire12 = GearBaseStub.from({ firepower: 12 })
+      const fire13 = GearBaseStub.from({ firepower: 13 })
+
+      expectMultiplierSqrtStars((stars) => createImprovement(fire12, stars).shellingPowerBonus, 0)
+      expectMultiplierSqrtStars((stars) => createImprovement(fire13, stars).shellingPowerBonus, 1.5)
+    })
+
+    it("艦上攻撃機 -> 0.2 * ☆", () => {
+      const gear = GearBaseStub.fromCategory("CarrierBasedTorpedoBomber")
+
+      expectMultiplierStars((stars) => createImprovement(gear, stars).shellingPowerBonus, 0.2)
+    })
+  })
+})
--- packages/core/src/gear/Improvement.ts
+++ packages/core/src/gear/Improvement.ts
@@ -1,46 +1,164 @@
 import { GearBase } from "./MasterGear"
 import { GearAttribute } from "./GearAttribute"
-import { GearCategory } from "@fleethub/data"
+import { GearCategory, GearId } from "@fleethub/data"
 
 type CalculationMethod = number | "average" | "sum"
 
-type AttackModifiers = {
-  power: number
-  accuracy: number
-  evasion: number
+export type Improvement = {
+  contactSelectionBonus: number
+
+  fighterPowerBonus: number
+  adjustedAntiAirBonus: number
+  fleetAntiAirBonus: number
+
+  shellingPowerBonus: number
+  shellingAccuracyBonus: number
+
+  aswPowerBonus: number
+  aswAccuracyBonus: number
+
+  torpedoPowerBonus: number
+  torpedoAccuracyBonus: number
+  torpedoEvasionBonus: number
+
+  nightPowerBonus: number
+  nightAccuracyBonus: number
+
+  effectiveLosBonus: number
+  defensePowerBonus: number
 }
 
-type CombatModifiers = {
-  shelling: AttackModifiers
-  asw: AttackModifiers
-  torpedo: AttackModifiers
-  night: AttackModifiers
+type GearParams = Pick<GearBase, "gearId" | "category" | "is" | "firepower" | "antiAir" | "los">
+
+type ImprovementBonusCalculator = (gear: GearBase, stars: number) => number
+
+/**
+ * @see https://t.co/Ou8KzFANPK
+ * @see https://twitter.com/shiro_sh39/status/1103281372548878337
+ */
+const calcContactSelectionBonus: ImprovementBonusCalculator = (gear, stars) => {
+  const { category, los } = gear
+
+  if (category === GearCategory.CarrierBasedReconnaissanceAircraft) {
+    // 二式艦偵 [0.25, 3) または√☆
+    if (los === 7) return 0.25 * stars
+
+    // 景雲 (0.333..., 0.4]
+    if (los === 11) return 0.4 * stars
+  }
+
+  if (category === GearCategory.ReconnaissanceSeaplane) {
+    // 零観 (0.166..., 0.2]
+    if (los === 6) return 0.2 * stars
+
+    // 零偵 Ro.43 (0.125, 0.1428...]
+    if (los >= 4) return 0.14 * stars
+
+    // 夜偵 (0, 0.1]
+    if (los === 3) return 0.1 * stars
+  }
+
+  return 0
 }
 
-export type ImprovementModifiers = {
-  contactSelectionModifier: number
+/**
+ * @see https://twitter.com/yukicacoon/status/1212566867933450241
+ */
+const calcFighterPowerBonus: ImprovementBonusCalculator = (gear, stars) => {
+  if (gear.is("Fighter")) return 0.2 * stars
+  if (gear.is("FighterBomber")) return 0.25 * stars
+  if (gear.category === GearCategory.LandBasedAttackAircraft) return 0.5 * Math.sqrt(stars)
 
-  fighterPowerModifier: number
-  adjustedAntiAirModifier: number
-  fleetAntiAirModifier: number
-
-  shellingPowerModifier: number
-  shellingAccuracyModifier: number
-
-  aswPowerModifier: number
-  aswAccuracyModifier: number
-
-  torpedoPowerModifier: number
-  torpedoAccuracyModifier: number
-  torpedoEvasionModifier: number
-
-  nightPowerModifier: number
-  nightAccuracyModifier: number
-
-  effectiveLosModifier: number
-  defensePowerModifier: number
+  return 0
 }
 
-export default class Improvement {
-  constructor(private gear: GearBase, private stars: number) {}
+/**
+ * @see  https://twitter.com/CitrusJ9N/status/1056224720712921088
+ */
+const calcAdjustedAntiAirBonus: ImprovementBonusCalculator = (gear, stars) => {
+  const { antiAir, category } = gear
+  if (antiAir === 0) {
+    return 0
+  }
+
+  let multiplier = 0
+  if (category === GearCategory.AntiAircraftGun) {
+    multiplier = antiAir <= 7 ? 4 : 6
+  } else if (category === GearCategory.AntiAircraftFireDirector || gear.is("HighAngleMount")) {
+    multiplier = antiAir <= 7 ? 2 : 3
+  }
+  return multiplier * Math.sqrt(stars)
 }
+
+const calcFleetAntiAirBonus = ({ antiAir, is, category }: GearParams, stars: number) => {
+  if (antiAir === 0) {
+    return 0
+  }
+  // 装備定数B
+  let multiplier = 0
+  if (category === GearCategory.AntiAircraftFireDirector || is("HighAngleMount")) {
+    multiplier = antiAir <= 7 ? 2 : 3
+  } else if (is("AirRadar")) {
+    multiplier = 1.5
+  }
+  return multiplier * Math.sqrt(stars)
+}
+
+const calcShellingPowerBonus: ImprovementBonusCalculator = ({ gearId, is, category, categoryIn, firepower }, stars) => {
+  if (firepower > 12) {
+    return 1.5 * Math.sqrt(stars)
+  }
+
+  if (category === GearCategory.CarrierBasedTorpedoBomber) {
+    return 0.2 * stars
+  }
+
+  if (
+    [
+      GearId["12.7cm連装高角砲"],
+      GearId["8cm高角砲"],
+      GearId["8cm高角砲改+増設機銃"],
+      GearId["10cm連装高角砲改+増設機銃"],
+    ].includes(gearId)
+  ) {
+    return 0.2 * stars
+  }
+
+  if ([GearId["15.5cm三連装副砲"], GearId["15.5cm三連装副砲改"], GearId["15.2cm三連装砲"]].includes(gearId)) {
+    return 0.3 * stars
+  }
+
+  if (
+    categoryIn(
+      "SmallCaliberMainGun",
+      "MediumCaliberMainGun",
+      "LargeCaliberMainGun",
+      "SecondaryGun",
+      "ArmorPiercingShell",
+      "AntiAircraftFireDirector",
+      "Searchlight",
+      "AntiAircraftGun",
+      "LandingCraft",
+      "SpecialAmphibiousTank",
+      "AntiAircraftShell",
+      "AntiGroundEquipment"
+    )
+  ) {
+    return Math.sqrt(stars)
+  }
+
+  if (categoryIn("Sonar", "LargeSonar") || is("DepthChargeProjector") || is("Mortar")) {
+    return 0.75 * Math.sqrt(stars)
+  }
+
+  return 0
+}
+
+export const createImprovement = (gear: GearBase, stars: number) => ({
+  contactSelectionBonus: calcContactSelectionBonus(gear, stars),
+
+  fighterPowerBonus: calcFighterPowerBonus(gear, stars),
+  adjustedAntiAirBonus: calcAdjustedAntiAirBonus(gear, stars),
+  fleetAntiAirBonus: calcFleetAntiAirBonus(gear, stars),
+  shellingPowerBonus: calcShellingPowerBonus(gear, stars),
+})
