96ff4124b8001f72101f2340637ad391e947ab18
まどの <madonoharu@gmail.com>
2020-04-27 21:20:06 +0900

    Add ShipMock

--- packages/core/src/utils/ShipMock.ts
+++ packages/core/src/utils/ShipMock.ts
@@ -2,36 +2,8 @@ import { Equipment } from "../equipment"
 import { Ship, ShipStat } from "../ship"
 import { HealthImpl } from "../ship/Health"
 
-class EquipmentMock implements Equipment {
-  public src = []
-  public size = 0
-
-  public defaultSlots = []
-  public currentSlots = []
-
-  public gears = []
-
-  public forEach = jest.fn()
-  public filter = jest.fn()
-  public map = jest.fn()
-  public sumBy = jest.fn()
-  public maxValueBy = jest.fn()
-  public has = jest.fn()
-  public count = jest.fn()
-  public hasAircraft = jest.fn()
-  public countAircraft = jest.fn()
-}
-
-class ShipStatMock implements Required<ShipStat> {
-  public key = "" as any
-  public displayed = 0
-  public naked = 0
-  public left = 0
-  public right = 0
-  public modernization = 0
-  public equipment = 0
-  public bonus = 0
-}
+import EquipmentMock from "./EquipmentMock"
+import { ShipStatStub } from "./ShipStatsStub"
 
 export default class ShipMock implements Ship {
   public name = ""
@@ -45,17 +17,17 @@ export default class ShipMock implements Ship {
 
   public level = 0
 
-  public firepower = new ShipStatMock()
-  public torpedo = new ShipStatMock()
-  public antiAir = new ShipStatMock()
-  public armor = new ShipStatMock()
-  public asw = new ShipStatMock()
-  public los = new ShipStatMock()
-  public evasion = new ShipStatMock()
-  public maxHp = new ShipStatMock()
-  public speed = new ShipStatMock()
-  public range = new ShipStatMock()
-  public luck = new ShipStatMock()
+  public firepower = new ShipStatStub()
+  public torpedo = new ShipStatStub()
+  public antiAir = new ShipStatStub()
+  public armor = new ShipStatStub()
+  public asw = new ShipStatStub()
+  public los = new ShipStatStub()
+  public evasion = new ShipStatStub()
+  public maxHp = new ShipStatStub()
+  public speed = new ShipStatStub()
+  public range = new ShipStatStub()
+  public luck = new ShipStatStub()
 
   public health = new HealthImpl(0)
 
