85713ddec4570ddddc58d423ed9a24a676922325
まどの <madonoharu@gmail.com>
2020-04-30 06:15:57 +0900

    Add test

--- packages/core/src/ship/Ship.test.ts
+++ packages/core/src/ship/Ship.test.ts
@@ -1,30 +1,56 @@
 import { ShipImpl } from "./Ship"
-import { ShipCommonBase } from "./MasterShip"
-import ShipStatsStub from "../utils/ShipStatsStub"
-import EquipmentMock from "../utils/EquipmentMock"
-import ShipHealthMock from "../utils/ShipHealthMock"
+import { ShipCommonBase } from "./types"
+
+import { ShipStatsStub, EquipmentMock, GearStub } from "../utils"
+
+const getMocks = () => {
+  const base: ShipCommonBase = {
+    shipId: 1,
+    shipClass: 2,
+    shipType: 3,
+    sortId: 4,
+    name: "5",
+    ruby: "6",
+    is: jest.fn(),
+    canEquip: jest.fn(),
+  }
+
+  const stats = new ShipStatsStub()
+  const equipment = new EquipmentMock()
+  const getNextBonusesMockFn = jest.fn()
+
+  return [base, stats, equipment, getNextBonusesMockFn] as const
+}
 
 describe("ShipImpl", () => {
-  it.skip("constructor", () => {
-    const base: ShipCommonBase = {
-      shipId: 0,
-      shipClass: 0,
-      shipType: 0,
-      name: "",
-      ruby: "",
-      sortId: 0,
-      is: jest.fn(),
-      canEquip: jest.fn(),
-    }
+  it("constructor", () => {
+    const [base, stats, equipment, getNextBonusesMockFn] = getMocks()
+    const ship = new ShipImpl(base, stats, equipment, getNextBonusesMockFn)
 
-    const stats = new ShipStatsStub()
-    const equipment = new EquipmentMock()
-    const health = new ShipHealthMock()
+    expect(base).toMatchObject(base)
+    expect(ship.equipment).toBe(equipment)
+    expect(ship.getNextBonuses).toBe(getNextBonusesMockFn)
 
-    expect(new ShipImpl(base, stats, equipment, health)).toBeTruthy()
+    const statKeys = Object.keys(stats) as Array<keyof typeof stats>
+    statKeys.forEach((key) => {
+      expect(ship[key]).toBe(stats[key])
+    })
   })
 
-  it.skip("canEquip")
+  it("fleetLosFactor = naked-los + sum(ObservationSeaplane-los * [sqrt(slotSize)])", () => {
+    const [base, stats, equipment, getNextBonusesMockFn] = getMocks()
+    const ship = new ShipImpl(base, stats, equipment, getNextBonusesMockFn)
+    stats.los.naked = 37
 
-  it.skip("fleetLosFactor")
+    equipment.sumBy.mockImplementationOnce((cb) => cb(new GearStub(), null, 0))
+    expect(ship.fleetLosFactor).toBe(37 + 0)
+
+    const observation = GearStub.fromAttrs("ObservationSeaplane")
+    observation.los = 8
+    equipment.sumBy.mockImplementationOnce((cb) => cb(observation, null, 0))
+    expect(ship.fleetLosFactor).toBe(37 + 0)
+
+    equipment.sumBy.mockImplementationOnce((cb) => cb(observation, null, 5))
+    expect(ship.fleetLosFactor).toBe(37 + 8 * Math.floor(Math.sqrt(5)))
+  })
 })
