e56e49e7992988fb61d62ed98866d42ab591d19f
まどの <madonoharu@gmail.com>
2020-05-05 15:10:09 +0900

    Add aviationDetectionScore

--- packages/core/src/fleet/FleetImpl.ts
+++ packages/core/src/fleet/FleetImpl.ts
@@ -3,15 +3,37 @@ import { sumBy } from "lodash-es"
 import { NullableArray, isNonNullable } from "../utils"
 import { Ship } from "../ship"
 
+const calcShipAviationDetectionScore = ({ equipment }: Ship) =>
+  equipment.sumBy((gear, index, slotSize) => {
+    if (!slotSize) return 0
+
+    if (gear.categoryIn("LargeFlyingBoat")) {
+      return gear.los * Math.sqrt(slotSize)
+    }
+
+    if (gear.categoryIn("ReconSeaplane", "SeaplaneBomber")) {
+      return gear.los * Math.sqrt(Math.sqrt(slotSize))
+    }
+
+    return 0
+  })
+
 type Fleet = {
   fleetLosModifier: number
+  aviationDetectionScore: number
 }
 
 export class FleetImpl implements Fleet {
   constructor(public ships: NullableArray<Ship>) {}
 
+  public sumBy = (callbackFn: (ship: Ship) => number) => sumBy(this.ships.filter(isNonNullable), callbackFn)
+
   get fleetLosModifier() {
-    const base = sumBy(this.ships.filter(isNonNullable), (ship) => ship.fleetLosFactor)
+    const base = this.sumBy((ship) => ship.fleetLosFactor)
     return Math.floor(Math.sqrt(base) + 0.1 * base)
+  }
+
+  get aviationDetectionScore() {
+    return this.sumBy(calcShipAviationDetectionScore)
   }
 }
