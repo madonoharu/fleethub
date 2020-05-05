89d68e1c93a2f97600b72c328b0a66db81817556
まどの <madonoharu@gmail.com>
2020-05-05 15:34:01 +0900

    Move expedition

--- packages/core/src/fleet/expedition.ts
+++ packages/core/src/fleet/expedition.ts
@@ -5,8 +5,6 @@ import { isNonNullable } from "./utils"
 import { Gear } from "./gear"
 import { Ship } from "./ship"
 
-const clamp = (value: number, lower: number, upper: number) => Math.min(Math.max(value, lower), upper)
-
 const getBonus2 = (gears: Gear[]) => {
   const lcCount = gears.filter((gear) => gear.gearId === GearId["大発動艇"]).length
   const tokuCount = gears.filter((gear) => gear.gearId === GearId["特大発動艇"]).length
