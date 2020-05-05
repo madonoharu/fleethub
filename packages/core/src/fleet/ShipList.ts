e811dd605642515a1d2c625dae53f2dbbd8b2bb6
まどの <madonoharu@gmail.com>
2020-05-05 14:28:40 +0900

    Remove ShipList

--- packages/core/src/fleet/ShipList.ts
+++
@@ -1,7 +0,0 @@
-import { Ship } from "../ship"
-import { sumBy } from "lodash-es"
-
-const calcFleetLosModifier = (ships: Ship[]) => {
-  const base = sumBy(ships, (ship) => ship.fleetLosFactor)
-  return Math.floor(Math.sqrt(base) + 0.1 * base)
-}
