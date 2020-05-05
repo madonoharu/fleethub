ec9a70e9a5d1f04aa1369eac8f7c132c65272b8b
まどの <madonoharu@gmail.com>
2020-05-05 13:52:51 +0900

    Improve stats UI

--- packages/client/src/components/organisms/ShipCard/ShipStatTooltip.tsx
+++ packages/client/src/components/organisms/ShipCard/ShipStatTooltip.tsx
@@ -2,19 +2,20 @@ import React from "react"
 import styled from "styled-components"
 import { BasicStatKey, ShipStats } from "@fleethub/core"
 
-import { Tooltip, TooltipProps } from "@material-ui/core"
+import { Tooltip, TooltipProps, Typography } from "@material-ui/core"
 
-import { StatIcon, Text } from "../../../components"
-import { withSign, getRangeName, getSpeedName, StatKeyDictionary } from "../../../utils"
-import { Flexbox } from "../../atoms"
+import { StatIcon, Text, Flexbox } from "../../../components"
+import { withSign, getRangeName, getSpeedName, StatKeyDictionary, getBonusText } from "../../../utils"
 
-import { getDisplayedStr } from "./ShipStatLabel"
+export const getDisplayedStr = (key: string, value: number) => {
+  if (key === "speed") return getSpeedName(value)
+  if (key === "range") return getRangeName(value)
+  return value.toString()
+}
 
-const isNumber = (value: unknown): value is number => typeof value === "number"
+type Stat = Partial<ShipStats["firepower"]> & { displayed: number }
 
-type Stat = Partial<ShipStats["firepower"]>
-
-type StatProps<K extends keyof ShipStats> = {
+export type StatProps<K extends keyof ShipStats> = {
   statKey: K
   stat: Stat
 }
@@ -27,18 +28,59 @@ type Props =
   | StatProps<"luck">
   | StatProps<"accuracy">
 
-const ShipStatTooltip: React.FC<Props & Pick<TooltipProps, "children">> = ({ statKey, stat, children }) => {
-  const statNemae = StatKeyDictionary[statKey]
+const StatTitle: React.FCX<{ statKey: Props["statKey"]; displayed: string }> = ({ className, statKey, displayed }) => {
+  const statNeme = StatKeyDictionary[statKey]
+  return (
+    <Flexbox className={className}>
+      <StatIcon icon={statKey} />
+      <Typography variant="subtitle2">
+        <span>{statNeme}</span>
+        {displayed}
+      </Typography>
+    </Flexbox>
+  )
+}
 
+const SpaceBetween = styled(Flexbox)`
+  justify-content: space-between;
+`
+
+const StyledStatTitle = styled(StatTitle)`
+  ${StatIcon} {
+    vertical-align: sub;
+  }
+
+  span {
+    margin: 0 4px;
+    color: ${({ theme, statKey }) => theme.kc.palette[statKey]};
+  }
+`
+
+const ShipStatTooltip: React.FC<Props & Pick<TooltipProps, "children">> = ({ statKey, stat, children }) => {
   const { displayed, bonus, increase, equipment } = stat
 
   const title = (
     <>
-      <Text>{statNemae}</Text>
-      {isNumber(displayed) && <Text>表示 {getDisplayedStr(statKey, displayed)}</Text>}
-      {isNumber(bonus) && <Text>装備ボーナス {bonus}</Text>}
-      {isNumber(increase) && <Text>増加値 {increase}</Text>}
-      {isNumber(equipment) && <Text>装備合計 {equipment}</Text>}
+      <StyledStatTitle statKey={statKey} displayed={getDisplayedStr(statKey, displayed)} />
+
+      {bonus ? (
+        <SpaceBetween>
+          <Text>装備ボーナス</Text>
+          <Text color="bonus">{getBonusText(statKey, bonus)}</Text>
+        </SpaceBetween>
+      ) : null}
+      {increase ? (
+        <SpaceBetween>
+          <Text>増加値</Text>
+          <Text color="increase">{withSign(increase)}</Text>
+        </SpaceBetween>
+      ) : null}
+      {equipment ? (
+        <SpaceBetween>
+          <Text>装備合計</Text>
+          <Text>{withSign(equipment)}</Text>
+        </SpaceBetween>
+      ) : null}
     </>
   )
 
