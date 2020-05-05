1c038eb90a0dcefb52750b7d2109b91cf436b3f0
まどの <madonoharu@gmail.com>
2020-05-05 13:55:19 +0900

    Add BonusText

--- packages/client/src/components/organisms/ShipCard/ShipStatLabel.tsx
+++ packages/client/src/components/organisms/ShipCard/ShipStatLabel.tsx
@@ -6,10 +6,29 @@ import { StatIcon, Text } from "../../../components"
 import { withSign, getRangeName, getSpeedName } from "../../../utils"
 import { Flexbox } from "../../atoms"
 
-import ShipStatTooltip from "./ShipStatTooltip"
+import ShipStatTooltip, { getDisplayedStr } from "./ShipStatTooltip"
+
+const BonusText = styled(Text)`
+  color: ${({ theme }) => theme.kc.palette.bonus};
+  position: absolute;
+  font-size: 10px;
+  bottom: -3px;
+  left: 10px;
+`
+
+const IncreaseText = styled(Text)`
+  color: ${({ theme }) => theme.kc.palette.increase};
+  position: absolute;
+  font-size: 10px;
+  top: -3px;
+  left: 10px;
+`
 
 const DisplayedText = styled(Text)`
   margin-left: 4px;
+  min-width: 24px;
+  text-align: right;
+  white-space: nowrap;
 `
 
 type StatProps<K extends keyof ShipStats> = {
@@ -25,20 +44,24 @@ type Props =
   | StatProps<"luck">
   | StatProps<"accuracy">
 
-export const getDisplayedStr = (key: string, value: number) => {
-  if (key === "speed") return getSpeedName(value)
-  if (key === "range") return getRangeName(value)
-  return value.toString()
-}
-
 const ShipStatLabel: React.FCX<Props> = (props) => {
   const { className, statKey, stat } = props
   const displayedStr = getDisplayedStr(statKey, stat.displayed)
+  let bonusStr: string | undefined
+  let increaseStr: string | undefined
+  if ("bonus" in stat && stat.bonus !== 0 && !["speed"].includes(statKey)) {
+    bonusStr = withSign(stat.bonus)
+  }
+  if ("increase" in stat && stat.increase !== 0) {
+    increaseStr = withSign(stat.increase)
+  }
 
   return (
     <ShipStatTooltip {...props}>
       <Flexbox className={className}>
         <StatIcon icon={statKey} />
+        <IncreaseText>{increaseStr}</IncreaseText>
+        <BonusText>{bonusStr}</BonusText>
         <DisplayedText>{displayedStr}</DisplayedText>
       </Flexbox>
     </ShipStatTooltip>
@@ -46,6 +69,7 @@ const ShipStatLabel: React.FCX<Props> = (props) => {
 }
 
 export default styled(ShipStatLabel)`
+  position: relative;
   font-size: 0.75rem;
   line-height: 1.5;
 `
