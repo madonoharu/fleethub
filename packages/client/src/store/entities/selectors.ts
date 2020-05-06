75d00660f6f62db862704f5522ce7f28c1dd042f
まどの <madonoharu@gmail.com>
2020-05-06 20:01:44 +0900

    Add state equal

--- packages/client/src/store/entities/selectors.ts
+++ packages/client/src/store/entities/selectors.ts
@@ -1,11 +1,24 @@
-import { useSelector, useDispatch, DefaultRootState } from "react-redux"
-import { EntityId, createSelector } from "@reduxjs/toolkit"
+import { shallowEqual, DefaultRootState } from "react-redux"
+import { EntityId } from "@reduxjs/toolkit"
+import { NullableArray, isNonNullable, ShipState } from "@fleethub/core"
 
 import { gearsSelectors } from "./gears"
 import { shipsSelectors } from "./ships"
 import { fleetsSelectors } from "./fleets"
-import { NullableArray } from "@fleethub/core"
-import { createShallowEqualSelector } from "../../utils"
+
+export const shipStateEqual = (left?: ShipState, right?: ShipState) => {
+  if (!left || !right) return left === right
+
+  const { gears: leftGears, ...leftRest } = left
+  const { gears: rightGears, ...rightRest } = right
+  return shallowEqual(leftGears, rightGears) && shallowEqual(leftRest, rightRest)
+}
+
+export const fleetStateEqual = (left?: NullableArray<ShipState>, right?: NullableArray<ShipState>) => {
+  if (!left || !right) return left === right
+
+  return left.every((ship, index) => shipStateEqual(ship, left[index]))
+}
 
 const getGearEntity = (state: DefaultRootState, id: EntityId) => gearsSelectors.selectById(state, id)
 
@@ -13,49 +26,18 @@ const getShipEntity = (state: DefaultRootState, id: EntityId) => shipsSelectors.
 
 const getFleetEntity = (state: DefaultRootState, id: EntityId) => fleetsSelectors.selectById(state, id)
 
-const makeGetGearStates = () =>
-  createShallowEqualSelector(
-    (state: DefaultRootState, ids: NullableArray<EntityId>) =>
-      ids.map((id) => (id ? getGearEntity(state, id) : undefined)),
-    (gears) => gears
-  )
+export const getShipState = (state: DefaultRootState, id: EntityId) => {
+  const entity = getShipEntity(state, id)
+  if (!entity) return
 
-export const makeGetShipState = () => {
-  const getGearStates = makeGetGearStates()
-
-  return createSelector(
-    getShipEntity,
-    (state, id) => {
-      const entity = getShipEntity(state, id)
-      return entity && getGearStates(state, entity.gears)
-    },
-    (entity, gears) => {
-      if (!entity || gears === undefined) return
-      return { ...entity, gears }
-    }
-  )
+  const gears = entity.gears.map((gearId) => (gearId ? getGearEntity(state, gearId) : undefined))
+  return { ...entity, gears }
 }
 
-const makeGetShipStates = () => {
-  const getShipState = makeGetShipState()
+export const getFleetState = (state: DefaultRootState, id: EntityId) => {
+  const entity = getFleetEntity(state, id)
+  if (!entity) return
 
-  return createShallowEqualSelector(
-    (state: DefaultRootState, ids: NullableArray<EntityId>) =>
-      ids.map((id) => (id ? getShipState(state, id) : undefined)),
-    (ships) => ships
-  )
-}
-
-export const makeGetFleetState = () => {
-  const getShipStates = makeGetShipStates()
-
-  return createSelector(
-    getFleetEntity,
-    (state: DefaultRootState, id: EntityId) => {
-      const fleetEntity = getFleetEntity(state, id)
-      if (!fleetEntity) return
-      return getShipStates(state, fleetEntity.main)
-    },
-    (entity, ships) => ships
-  )
+  const ships = entity.main.map((shipId) => (shipId ? getShipState(state, shipId) : undefined))
+  return { ...entity, ships }
 }
