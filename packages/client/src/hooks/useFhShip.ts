2e2c163b66f519c0bafbe6ac2cf67584a9d5b95d
まどの <madonoharu@gmail.com>
2020-05-06 20:01:44 +0900

    Add useFhShip

---
+++ packages/client/src/hooks/useFhShip.ts
@@ -0,0 +1,15 @@
+import React from "react"
+import { useSelector } from "react-redux"
+import { EntityId } from "@reduxjs/toolkit"
+
+import { makeGetShipState } from "../store"
+
+import { useFhSystem } from "./useFhSystem"
+
+export const useFhShip = (id: EntityId) => {
+  const getShipState = React.useMemo(makeGetShipState, [id])
+  const fhSystem = useFhSystem()
+
+  const state = useSelector((state) => getShipState(state, id))
+  return state && fhSystem.createShip(state)
+}
