f440d0f9e500b3d744b54908f62eb1c4ac9a6250
まどの <madonoharu@gmail.com>
2020-05-07 15:31:29 +0900

    Remove gears.spec

--- packages/client/src/store/entities/gears.spec.ts
+++
@@ -1,5 +0,0 @@
-import { gearToEntity, GearEntity } from "./gears"
-
-it("gearToEntity", () => {
-  expect(gearToEntity({ gearId: 2 })).toMatchObject<Partial<GearEntity>>({ gearId: 2, stars: 0, exp: 0 })
-})
