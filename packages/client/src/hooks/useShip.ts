990fce3109bab95569649d8a2bf9f61ae502f8fa
まどの <madonoharu@gmail.com>
2020-05-07 15:31:29 +0900

    Remove createFhShipSelector

--- packages/client/src/hooks/useShip.ts
+++ packages/client/src/hooks/useShip.ts
@@ -1,27 +1,10 @@
 import React from "react"
-import { useSelector, useDispatch, shallowEqual, DefaultRootState } from "react-redux"
-import { EntityId, createSelector } from "@reduxjs/toolkit"
-import { fhSystem, GearState, NullableArray } from "@fleethub/core"
+import { useSelector, useDispatch, shallowEqual } from "react-redux"
+import { EntityId } from "@reduxjs/toolkit"
 import { range } from "lodash-es"
 
-import { isEntityId, createShallowEqualSelector } from "../utils"
-import { entitiesSlice, ShipEntity, shipsSelectors, gearsSelectors, gearSelectSlice } from "../store"
-
-export const createFhShipSelector = (id: EntityId) => {
-  const getShipEntity = (state: DefaultRootState) => shipsSelectors.selectEntities(state)[id]
-
-  const getGearEntities = (state: DefaultRootState) =>
-    getShipEntity(state)?.gears.map((gearId) =>
-      isEntityId(gearId) ? gearsSelectors.selectEntities(state)[gearId] : undefined
-    )
-
-  const gearEntitiesSelector = createShallowEqualSelector(getGearEntities, (entities) => entities)
-
-  return createSelector(getShipEntity, gearEntitiesSelector, (shipEntity, gearEntities) => {
-    if (shipEntity === undefined || gearEntities === undefined) return
-    return fhSystem.createShip({ ...shipEntity, gears: gearEntities })
-  })
-}
+import { entitiesSlice, ShipEntity, shipsSelectors, gearSelectSlice } from "../store"
+import { useFhShip } from "./useFhShip"
 
 export const useShip = (id: EntityId) => {
   const dispatch = useDispatch()
@@ -45,8 +28,7 @@ export const useShip = (id: EntityId) => {
     [dispatch, id]
   )
 
-  const fhShipSelector = React.useCallback(createFhShipSelector(id), [id])
-  const fhShip = useSelector(fhShipSelector)
+  const fhShip = useFhShip(id)
 
   const gears = useSelector((state) => {
     const entityGears = shipsSelectors.selectEntities(state)[id]?.gears
