f660f163999de6c843c5ed19c62108ab58426010
まどの <madonoharu@gmail.com>
2020-05-08 22:31:18 +0900

    Fix types

--- packages/core/src/fleet/types.ts
+++ packages/core/src/fleet/types.ts
@@ -1,7 +1,9 @@
 import { NullableArray } from "../utils"
-import { ShipState } from "../ship"
+import { ShipState, Ship } from "../ship"
 
 export type Fleet = {
+  ships: NullableArray<Ship>
+
   fleetLosModifier: number
   aviationDetectionScore: number
   transportPoint: number
