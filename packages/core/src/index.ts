0848cdac1a283367e681afb9b3f540f6cd908936
まどの <madonoharu@gmail.com>
2020-05-08 22:31:18 +0900

    Add airbase

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -9,6 +9,7 @@ export const fhSystem = new FhSystem(factory)
 export * from "./ship"
 export * from "./gear"
 export * from "./fleet"
+export * from "./airbase"
 export * from "./utils"
 
 export { ShipClass, ShipClassKey, ShipType, HullCode } from "@fleethub/data"
