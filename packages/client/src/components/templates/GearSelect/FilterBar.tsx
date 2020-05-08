82b3f73f5a29bb181cff9deb42b64f4afba542b8
まどの <madonoharu@gmail.com>
2020-05-09 05:42:31 +0900

    Fix FilterBar

--- packages/client/src/components/templates/GearSelect/FilterBar.tsx
+++ packages/client/src/components/templates/GearSelect/FilterBar.tsx
@@ -1,54 +1,54 @@
 import React from "react"
 import styled from "styled-components"
 import { GearCategory } from "@fleethub/data"
-import { GearBase } from "@fleethub/core"
 
-import { AppBar, Toolbar, Checkbox, FormControlLabel, Box, Typography } from "@material-ui/core"
+import { Checkbox, FormControlLabel } from "@material-ui/core"
 
-import { GearSelectState } from "../../../store"
-import { Flexbox, SelectButtons, Select } from "../../../components"
+import { Flexbox } from "../../../components"
 
 import FilterButtons from "./FilterButtons"
 import CategorySelect from "./CategorySelect"
-import { getFilter } from "./filters"
-import { defaultComparer, idComparer } from "./comparers"
+import { GearSelectProps } from "./GearSelect"
 
 const Right = styled(Flexbox)`
   margin-left: auto;
   margin-bottom: -2px;
 `
 
-const getVisibleCategories = (gears: GearBase[]) => {
-  const categories = [...new Set(gears.map((gear) => gear.category))]
-  return categories.concat(0).sort((a, b) => a - b)
-}
-
 type Props = {
-  equippableGears: GearBase[]
-  state: GearSelectState
-  setState: (state: Partial<GearSelectState>) => void
+  visibleCategories: GearCategory[]
+  visibleFilterKeys: string[]
+
+  abyssal: boolean
+  filterKey: string
+  category: number
+
+  onUpdate: GearSelectProps["onUpdate"]
 }
 
-const FilterBar: React.FCX<Props> = ({ className, equippableGears, state, setState }) => {
-  const handleFilterChange = (filter: string) => setState({ filter, category: 0 })
+const FilterBar: React.FCX<Props> = ({
+  className,
+  visibleFilterKeys,
+  visibleCategories,
+  abyssal,
+  filterKey,
+  category,
+  onUpdate,
+}) => {
+  const handleFilterChange = (filterKey: string) => onUpdate({ filterKey, category: 0 })
 
-  const handleCategoryChange = (category: number) => setState({ category })
+  const handleCategoryChange = (category: number) => onUpdate({ category })
 
-  const toggleAbyssal = () => setState({ abyssal: !state.abyssal })
-
-  const visibleGears = equippableGears.filter(getFilter(state.filter)).sort(idComparer)
-
-  const visibleCategories = getVisibleCategories(visibleGears)
-
+  const toggleAbyssal = () => onUpdate({ abyssal: !abyssal })
   return (
     <>
       <div className={className}>
-        <FilterButtons value={state.filter} onChange={handleFilterChange} equippableGears={equippableGears} />
-        <CategorySelect value={state.category} options={visibleCategories} onChange={handleCategoryChange} />
+        <FilterButtons value={filterKey} options={visibleFilterKeys} onChange={handleFilterChange} />
+        <CategorySelect value={category} options={visibleCategories} onChange={handleCategoryChange} />
         <Right>
           <FormControlLabel
             label="深海"
-            control={<Checkbox size="small" checked={state.abyssal} onClick={toggleAbyssal} />}
+            control={<Checkbox size="small" checked={abyssal} onClick={toggleAbyssal} />}
           />
         </Right>
       </div>
