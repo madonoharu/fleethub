6b239463c67f3f91166646ebdc02aa38321893c7
まどの <madonoharu@gmail.com>
2020-05-08 22:31:18 +0900

    Add plans

--- packages/client/src/store/entities/plans.ts
+++ packages/client/src/store/entities/plans.ts
@@ -1,12 +1,6 @@
-import { FleetState } from "@fleethub/core"
 import { createEntityAdapter, EntityId } from "@reduxjs/toolkit"
 
 import { selectId, Entity } from "./entity"
-
-export type PlanState = {
-  name: string
-  fleets: FleetState[]
-}
 
 export type PlanEntity = Entity & {
   name: string
