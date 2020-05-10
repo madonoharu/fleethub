42f08f878b991d665183ee0cb634dd54cc845271
まどの <madonoharu@gmail.com>
2020-05-10 12:27:28 +0900

    Fix exports

--- packages/client/src/components/templates/index.ts
+++ packages/client/src/components/templates/index.ts
@@ -1,4 +1,5 @@
 export { default as FleetPanel } from "./FleetPanel"
+export { default as PlanPanel } from "./PlanPanel"
 export { default as ShipSelect } from "./ShipSelect"
 export { default as GearSelect } from "./GearSelect"
-export { default as GlobalDialogs } from "./GlobalDialogs"
+export { default as GlobalDialogsProvider } from "./GlobalDialogsProvider"
