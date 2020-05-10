85222f579851ef800fb7877564bc16af574fe236
まどの <madonoharu@gmail.com>
2020-05-10 12:27:28 +0900

    Fix gears page

--- packages/client/src/pages/gears.tsx
+++ packages/client/src/pages/gears.tsx
@@ -4,11 +4,15 @@ import { PageProps } from "gatsby"
 import { Container } from "@material-ui/core"
 
 import { GearSelect } from "../components"
+import { useGearSelectState } from "../hooks"
 
-const GearsPage: React.FC<PageProps> = () => (
-  <Container>
-    <GearSelect />
-  </Container>
-)
+const GearsPage: React.FC<PageProps> = () => {
+  const { state, actions } = useGearSelectState()
+  return (
+    <Container>
+      <GearSelect state={state} onUpdate={actions.update} />
+    </Container>
+  )
+}
 
 export default GearsPage
