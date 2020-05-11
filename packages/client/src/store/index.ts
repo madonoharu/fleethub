350f85032cc0fa47e2f8c5b5a05a34226ffeaa2d
まどの <madonoharu@gmail.com>
2020-05-11 18:13:01 +0900

    Remove gearSelectSlice

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import entitiesSlice from "./entities"
-import gearSelectSlice from "./gearSelectSlice"
 import shipSelectSlice from "./shipSelectSlice"
 
 const rootReducer = combineReducers({
   entities: entitiesSlice.reducer,
-  gearSelect: gearSelectSlice.reducer,
   shipSelect: shipSelectSlice.reducer,
 })
 
@@ -19,8 +17,7 @@ export const setupStore = () => {
 }
 
 export * from "./entities"
-export * from "./gearSelectSlice"
 export * from "./shipSelectSlice"
-export { entitiesSlice, gearSelectSlice, shipSelectSlice }
+export { entitiesSlice, shipSelectSlice }
 
 export type AppDispatch = ReturnType<typeof setupStore>["dispatch"]
