69a11f4a0465759821d6f4f3f2b9952201d6e2c7
まどの <madonoharu@gmail.com>
2020-05-12 15:38:38 +0900

    Fix useEquipmentGear

--- packages/client/src/components/organisms/EquipmentList/useEquipmentGear.ts
+++ packages/client/src/components/organisms/EquipmentList/useEquipmentGear.ts
@@ -1,26 +1,23 @@
 import { useMemo } from "react"
-import { GearState, EquipmentState, EquipmentKey } from "@fleethub/core"
+import { GearState, EquipmentState, EquipmentGearKey, getEquipmentSlotKey } from "@fleethub/core"
 
 import { useGearSelectActions, useFhSystem } from "../../../hooks"
 import { Update } from "../../../utils"
 
 export type EquipmentGearProps = {
   state?: GearState
-  equipmentKey: EquipmentKey
+  gearKey: EquipmentGearKey
   updateEquipment: Update<EquipmentState>
 }
 
-export const useEquipmentGear = ({ state, equipmentKey: key, updateEquipment }: EquipmentGearProps) => {
+export const useEquipmentGear = ({ state, gearKey, updateEquipment }: EquipmentGearProps) => {
   const gearSelectActions = useGearSelectActions()
   const fhSystem = useFhSystem()
 
   const actions = useMemo(() => {
     const createGear = (gearState: GearState) => {
       updateEquipment((draft) => {
-        if (!draft.gears) {
-          draft.gears = {}
-        }
-        draft.gears[key] = gearState
+        draft[gearKey] = gearState
       })
     }
 
@@ -30,29 +27,28 @@ export const useEquipmentGear = ({ state, equipmentKey: key, updateEquipment }: 
       })
     }
 
-    const changeSlotSize = (next: number) => {
+    const slotKey = getEquipmentSlotKey(gearKey)
+
+    const changeSlotSize = (next?: number) => {
       updateEquipment((draft) => {
-        if (!draft.slots) {
-          draft.slots = {}
-        }
-        draft.slots[key] = next
+        draft[slotKey] = next
       })
     }
 
     const updateGear: Update<GearState> = (updater) =>
       updateEquipment((draft) => {
-        const gearState = draft.gears?.[key]
-        gearState && updater(gearState)
+        const state = draft[gearKey]
+        state && updater(state)
       })
 
     const remove = () => {
       updateEquipment((draft) => {
-        delete draft.gears?.[key]
+        delete draft[gearKey]
       })
     }
 
     return { openGearSelect, updateGear, changeSlotSize, remove }
-  }, [key, updateEquipment, gearSelectActions])
+  }, [gearKey, updateEquipment, gearSelectActions])
 
   const gear = state && fhSystem.createGear(state)
 
