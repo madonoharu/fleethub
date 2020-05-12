39b202cab1acdfee26731c4b5f11f501db286a59
まどの <madonoharu@gmail.com>
2020-05-12 15:38:38 +0900

    Add FleetEditor

--- packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
+++ packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
@@ -1,14 +1,56 @@
 import React from "react"
-import { PlanState, FleetState } from "@fleethub/core"
+import { PlanState, FleetState, ShipState } from "@fleethub/core"
+import produce, { Draft } from "immer"
 
-const FleetEditor: React.FC<{ state: FleetState }> = ({ state }) => {
-  return <>{state.ships.length}</>
+import { Container, Paper, TextField, Button } from "@material-ui/core"
+
+import { FleetEditor } from "../../../components"
+import { useShipSelectActions, useFhSystem } from "../../../hooks"
+
+const getFleetState = (): FleetState => ({
+  ships: [...Array(6)],
+})
+
+export type Update<T> = (updater: (draft: Draft<T>) => void) => void
+
+const initialPlanState: PlanState = {
+  name: "PlanEditor",
+  fleets: [getFleetState(), getFleetState()],
+  airbases: [],
 }
 
 const PlanEditor: React.FC = (props) => {
-  const [state, setState] = React.useState<PlanState>({ name: "", fleets: [], airbases: [] })
+  const [state, setState] = React.useState<PlanState>(initialPlanState)
   const [fleetIndex, setFleetIndex] = React.useState(0)
-  return <></>
+
+  const update: Update<PlanState> = React.useCallback((updater) => setState(produce(updater)), [])
+
+  const fleetState = state.fleets[fleetIndex]
+
+  const updateFleet: Update<FleetState> = React.useCallback(
+    (updater) => {
+      update((draft) => updater(draft.fleets[fleetIndex]))
+    },
+    [fleetIndex, update]
+  )
+
+  const fleetElement = React.useMemo(() => {
+    return <FleetEditor state={fleetState} update={updateFleet} />
+  }, [fleetState, updateFleet])
+
+  return (
+    <Container>
+      <TextField
+        value={state.name}
+        onChange={(event) => {
+          update((draft) => {
+            draft.name = event.currentTarget.value
+          })
+        }}
+      />
+      {fleetElement}
+    </Container>
+  )
 }
 
 export default PlanEditor
