a0eb67930cf3f9f239760e9da7e57cf8084b4593
まどの <madonoharu@gmail.com>
2020-05-12 15:38:38 +0900

    Remove PlanPanel

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,32 +1,13 @@
 import React from "react"
-import { useSelector, useDispatch } from "react-redux"
 
 import { Button } from "@material-ui/core"
 
-import { PlanPanel } from "../components"
-import { entitiesSlice, plansSelectors } from "../store"
-
-const getInitalFleet = () => ({ ships: [...Array(6)] })
+import { PlanEditor } from "../components"
 
 const IndexPage: React.FC = () => {
-  const dispatch = useDispatch()
-  const planIds = useSelector((state) => plansSelectors.selectIds(state.entities))
-
-  const createPlan = () =>
-    dispatch(
-      entitiesSlice.actions.createPlan({
-        name: "a",
-        fleets: [getInitalFleet(), getInitalFleet(), getInitalFleet(), getInitalFleet()],
-        airbases: [],
-      })
-    )
-
   return (
     <>
-      <Button onClick={createPlan}>add plan</Button>
-      {planIds.map((id) => (
-        <PlanPanel key={id} plan={id} />
-      ))}
+      <PlanEditor />
     </>
   )
 }
