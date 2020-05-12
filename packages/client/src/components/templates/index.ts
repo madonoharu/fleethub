d2211f5d9e394c7e0b8dda6f4953ba7820589e2e
まどの <madonoharu@gmail.com>
2020-05-12 15:38:38 +0900

    Remove PlanPanel

--- packages/client/src/components/templates/index.ts
+++ packages/client/src/components/templates/index.ts
@@ -1,7 +1,7 @@
 export { default as FleetEditor } from "./FleetEditor"
+export { default as PlanEditor } from "./PlanEditor"
 
 export { default as FleetPanel } from "./FleetPanel"
-export { default as PlanPanel } from "./PlanPanel"
 export { default as ShipSelect } from "./ShipSelect"
 export { default as GearSelect } from "./GearSelect"
 export { default as GlobalDialogsProvider } from "./GlobalDialogsProvider"
