fa9b4437abc67084df58a33f4783b4b61d10d5de
まどの <madonoharu@gmail.com>
2020-05-14 18:47:54 +0900

    Remove useEquipmentGear

--- packages/client/src/components/organisms/EquipmentList/EquipmentListItem.tsx
+++ packages/client/src/components/organisms/EquipmentList/EquipmentListItem.tsx
@@ -1,22 +1,63 @@
 import React from "react"
-import { Gear, EquipmentState, NullableArray } from "@fleethub/core"
+import { Gear, GearState, getEquipmentSlotKey, EquipmentState, EquipmentGearKey } from "@fleethub/core"
 
 import { Container, Paper, TextField, Button } from "@material-ui/core"
 
 import { GearLabel, Flexbox } from "../../../components"
+import { useGearSelectActions } from "../../../hooks"
+import { Update } from "../../../utils"
 
 import SlotSizeButton from "./SlotSizeButton"
 import AddGearButton from "./AddGearButton"
-import { useEquipmentGear, EquipmentGearProps } from "./useEquipmentGear"
 
-type Props = EquipmentGearProps & {
+type Props = {
   gear?: Gear
   currentSlotSize?: number
   maxSlotSize?: number
+
+  gearKey: EquipmentGearKey
+  updateEquipment: Update<EquipmentState>
+}
+
+const useEquipmentGearActions = ({ gearKey, updateEquipment }: Props) => {
+  const gearSelectActions = useGearSelectActions()
+
+  return React.useMemo(() => {
+    console.log(gearKey)
+    const createGear = (gearState: GearState) => {
+      updateEquipment((draft) => {
+        draft[gearKey] = gearState
+      })
+    }
+
+    const openGearSelect = () => gearSelectActions.open(createGear)
+
+    const slotKey = getEquipmentSlotKey(gearKey)
+
+    const changeSlotSize = (next?: number) => {
+      updateEquipment((draft) => {
+        draft[slotKey] = next
+      })
+    }
+
+    const updateGear: Update<GearState> = (updater) =>
+      updateEquipment((draft) => {
+        const state = draft[gearKey]
+        state && updater(state)
+      })
+
+    const remove = () => {
+      updateEquipment((draft) => {
+        delete draft[gearKey]
+      })
+    }
+
+    return { openGearSelect, updateGear, changeSlotSize, remove }
+  }, [gearKey, updateEquipment, gearSelectActions])
 }
 
 const EquipmentListItem: React.FCX<Props> = ({ className, gear, currentSlotSize, maxSlotSize, ...rest }) => {
-  const { openGearSelect, updateGear, changeSlotSize, remove } = useEquipmentGear(rest)
+  const { openGearSelect, updateGear, changeSlotSize, remove } = useEquipmentGearActions(rest)
 
   return (
     <Flexbox className={className}>
