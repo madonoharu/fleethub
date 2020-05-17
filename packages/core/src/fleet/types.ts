9b13d11e6cef218e75a53351fd14301a3f4c240c
まどの <madonoharu@gmail.com>
2020-05-17 20:13:01 +0900

    Fix FleetState

--- packages/core/src/fleet/types.ts
+++ packages/core/src/fleet/types.ts
@@ -1,9 +1,11 @@
 import { NullableArray } from "../utils"
 import { ShipState, Ship } from "../ship"
 
-export type FleetState = {
-  ships: NullableArray<ShipState>
-}
+export type ShipKey = "s1" | "s2" | "s3" | "s4" | "s5" | "s6" | "s7"
+
+type ShipRecord = Partial<Record<ShipKey, ShipState>>
+
+export type FleetState = ShipRecord
 
 export type Fleet = {
   state: FleetState
