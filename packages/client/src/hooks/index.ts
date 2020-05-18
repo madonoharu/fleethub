cc6f900e68191393a3858d4199d42e1d737a54c6
まどの <madonoharu@gmail.com>
2020-05-18 18:10:01 +0900

    Remove entities

--- packages/client/src/hooks/index.ts
+++ packages/client/src/hooks/index.ts
@@ -1,24 +1,4 @@
 import React from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { EntityId } from "@reduxjs/toolkit"
-
-import { entitiesSlice, plansSelectors } from "../store"
-
-export const usePlan = (id: EntityId) => {
-  const dispatch = useDispatch()
-  const entity = useSelector((state) => plansSelectors.selectById(state.entities, id))
-
-  const actions = React.useMemo(
-    () => ({
-      createFleet: () => {
-        dispatch(entitiesSlice.actions.createFleet({ ships: [...Array(6)] }))
-      },
-    }),
-    [dispatch]
-  )
-
-  return { actions, entity }
-}
 
 export const useRenderCount = () => {
   const renderCountRef = React.useRef(0)
@@ -34,10 +14,6 @@ export * from "./handlers"
 
 export * from "./useFhSystem"
 export * from "./useCachedFhFactory"
-
-export * from "./useGear"
-export * from "./useShip"
-export * from "./useFleet"
 
 export * from "./useGearSelectContext"
 export * from "./useShipSelectContext"
