0bd25f8b3f093a2172db46ed14a020bc6ed2a95f
まどの <madonoharu@gmail.com>
2020-05-19 21:30:38 +0900

    Add FleetTabPanel

---
+++ packages/client/src/components/templates/PlanEditor/FleetTabPanel.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { PlanState, FleetState, FleetKey, Fleet } from "@fleethub/core"
+
+import { Container, Paper, TextField, Button } from "@material-ui/core"
+
+import { FleetEditor } from "../../../components"
+import { Update } from "../../../utils"
+
+type Props = {
+  fleet: Fleet
+  fleetKey: FleetKey
+  updatePlan: Update<PlanState>
+}
+
+const FleetTabPanel: React.FC<Props> = ({ fleet, fleetKey, updatePlan }) => {
+  const updateFleet: Update<FleetState> = React.useCallback(
+    (updater) => {
+      updatePlan((draft) => {
+        const fleetState = draft[fleetKey]
+        if (fleetState) updater(fleetState)
+      })
+    },
+    [fleetKey, updatePlan]
+  )
+
+  return <FleetEditor fleet={fleet} update={updateFleet} />
+}
+
+export default FleetTabPanel
