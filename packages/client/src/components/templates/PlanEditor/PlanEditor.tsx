8dc26625edb4cc9b382b14cea126603574d1390f
まどの <madonoharu@gmail.com>
2020-05-23 05:20:31 +0900

    Add PlansPage

--- packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
+++ packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
@@ -1,32 +1,24 @@
 import React from "react"
-import { PlanState } from "@fleethub/core"
-import produce from "immer"
+import { EntityId } from "@reduxjs/toolkit"
 
 import { Container, Paper, TextField, Button } from "@material-ui/core"
 
 import { SelectButtons } from "../../../components"
-import { useCachedFhFactory } from "../../../hooks"
-import { Update } from "../../../utils"
+import { usePlan } from "../../../hooks"
 
 import FleetTabPanel from "./FleetTabPanel"
 
-const initialPlanState: PlanState = {
-  name: "PlanEditor",
-  f1: {},
-  f2: {},
-  a1: {},
-}
-
 const tabOptions = ["f1", "f2", "lb"] as const
 
-const PlanEditor: React.FC = (props) => {
-  const [state, setState] = React.useState<PlanState>(initialPlanState)
+type Props = {
+  planId: EntityId
+}
+
+const PlanEditor: React.FC<Props> = ({ planId }) => {
+  const { state, actions, plan } = usePlan(planId)
   const [tabKey, setTabKey] = React.useState<typeof tabOptions[number]>("f1")
 
-  const update: Update<PlanState> = React.useCallback((updater) => setState(produce(updater)), [])
-
-  const { createPlan } = useCachedFhFactory()
-  const plan = createPlan(state)
+  if (!state || !plan) return null
 
   const fleetEntry = plan.fleetEntries.find(([key]) => key === tabKey)
 
@@ -35,13 +27,13 @@ const PlanEditor: React.FC = (props) => {
       <TextField
         value={state.name}
         onChange={(event) => {
-          update((draft) => {
+          actions.update((draft) => {
             draft.name = event.currentTarget.value
           })
         }}
       />
       <SelectButtons options={tabOptions} value={tabKey} onChange={setTabKey} />
-      {fleetEntry && <FleetTabPanel fleet={fleetEntry[1]} fleetKey={fleetEntry[0]} updatePlan={update} />}
+      {fleetEntry && <FleetTabPanel fleet={fleetEntry[1]} fleetKey={fleetEntry[0]} updatePlan={actions.update} />}
     </Container>
   )
 }
