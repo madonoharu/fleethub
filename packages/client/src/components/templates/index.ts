3de4ff67af482fb4449c0da152725401e95c054d
まどの <madonoharu@gmail.com>
2020-05-23 05:20:31 +0900

    Add PlanList

--- packages/client/src/components/templates/index.ts
+++ packages/client/src/components/templates/index.ts
@@ -1,6 +1,6 @@
 export { default as FleetEditor } from "./FleetEditor"
 export { default as PlanEditor } from "./PlanEditor"
-
+export { default as PlanList } from "./PlanList"
 export { default as ShipSelect } from "./ShipSelect"
 export { default as GearSelect } from "./GearSelect"
 export { default as GlobalDialogsProvider } from "./GlobalDialogsProvider"
