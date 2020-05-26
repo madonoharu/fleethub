3d0dbcd37a45f75467e7ea0f15db79be2d5a8802
まどの <madonoharu@gmail.com>
2020-05-26 23:30:28 +0900

    Add close action

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 
 import { PlanList, PlanEditor } from "../components"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { planEditorSlice } from "../store"
 
 const IndexPage: React.FC = () => {
   const planId = useSelector((state) => state.planEditor.planId)
+  const dispatch = useDispatch()
 
   if (planId === undefined) return <PlanList />
-  return <PlanEditor planId={planId} />
+  return <PlanEditor planId={planId} onClose={() => dispatch(planEditorSlice.actions.close())} />
 }
 
 export default IndexPage
