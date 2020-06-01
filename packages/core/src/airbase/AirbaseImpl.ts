68e06252114c7d0e4abd5fed5ce3b5356ba7a7c3
まどの <madonoharu@gmail.com>
2020-06-01 23:43:13 +0900

    Add airbase.canEquip

--- packages/core/src/airbase/AirbaseImpl.ts
+++ packages/core/src/airbase/AirbaseImpl.ts
@@ -1,6 +1,12 @@
 import { Airbase } from "./types"
 import { Equipment } from "../equipment"
 
+const canEquip: Airbase["canEquip"] = (gear) => {
+  return gear.in("CbAircraft", "Seaplane", "JetAircraft", "LbAircraft")
+}
+
 export class AirbaseImpl implements Airbase {
   constructor(public equipment: Equipment) {}
+
+  public canEquip = canEquip
 }
--- packages/core/src/airbase/types.ts
+++ packages/core/src/airbase/types.ts
@@ -1,4 +1,4 @@
-import { GearState, Gear } from "../gear"
+import { GearState, Gear, GearBase } from "../gear"
 import { NullableArray } from "../utils"
 import { EquipmentState, Equipment } from "../equipment"
 
@@ -6,4 +6,5 @@ export type AirbaseState = EquipmentState
 
 export type Airbase = {
   equipment: Equipment
+  canEquip: (gear: GearBase) => boolean
 }
