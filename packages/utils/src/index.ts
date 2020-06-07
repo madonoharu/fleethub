ee55970de4d3c4f1ab83749f331db9b91bb6bd40
まどの <madonoharu@gmail.com>
2020-06-07 20:18:50 +0900

    Add @fleethub/utils

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,19 @@
+export const isNonNullable = <T>(item: T): item is NonNullable<T> => item !== undefined && item !== null
+
+export const isString = (value: unknown): value is string => typeof value === "string"
+
+export const mapValues = <T, R>(obj: T, fn: (value: T[keyof T], key: keyof T) => R): Record<keyof T, R> => {
+  const nextObj: Record<string, R> = {}
+
+  for (const key in obj) {
+    const value = obj[key]
+    nextObj[key] = fn(value, key)
+  }
+
+  return nextObj as Record<keyof T, R>
+}
+
+export type Dict<T> = Record<string, T | undefined>
+
+export type NullableArray<T> = Array<T | undefined>
+export type PickByValue<T, ValueType> = Pick<T, { [Key in keyof T]-?: T[Key] extends ValueType ? Key : never }[keyof T]>
