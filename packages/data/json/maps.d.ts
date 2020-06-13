b7660f1fb540839e5cdb6975cab4861124cf2b7d
まどの <madonoharu@gmail.com>
2020-06-13 23:45:24 +0900

    Fix map types

--- packages/data/json/maps.d.ts
+++ packages/data/json/maps.d.ts
@@ -7,31 +7,11 @@ export type MapEnemyFleet = {
   difficulty?: number
 }
 
-export const enum MapNodeType {
-  Unknown = -1,
-  Start = 0,
-  Resource = 2,
-  Maelstrom = 3,
-  Normal = 4,
-  Boss = 5,
-  Transport = 6,
-  Aerial = 7,
-  Bounty = 8,
-  AerialReconnaissance = 9,
-  AirDefense = 10,
-  NightBattle = 11,
-  LongRangeRadarAmbush = 13,
-  EmergencyAnchorageRepair = 14,
-
-  NoEnemy = 90,
-  Selector = 91,
-}
-
 export type MapNode = {
   point: string
   x: number
   y: number
-  type: MapNodeType
+  type: number
   d?: number
   enemies?: MapEnemyFleet[]
 }
