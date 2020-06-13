af8886e3449468524e399b6909f96d295dd8d746
まどの <madonoharu@gmail.com>
2020-06-13 23:45:24 +0900

    Fix map types

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,7 +1,27 @@
 import { default as ships, ShipData } from "../json/ships"
 import { default as gears, GearData } from "../json/gears"
 import { default as equippable, EquippableData, ShipEquippable, ShipTypeEquippable } from "../json/equippable"
-import { default as maps, MapEnemyFleet, MapNodeType, MapNode, MapLink, MapData } from "../json/maps"
+import { default as maps, MapEnemyFleet, MapNode, MapLink, MapData } from "../json/maps"
+
+export enum MapNodeType {
+  Unknown = -1,
+  Start = 0,
+  Resource = 2,
+  Maelstrom = 3,
+  Normal = 4,
+  Boss = 5,
+  Transport = 6,
+  Aerial = 7,
+  Bounty = 8,
+  AerialReconnaissance = 9,
+  AirDefense = 10,
+  NightBattle = 11,
+  LongRangeRadarAmbush = 13,
+  EmergencyAnchorageRepair = 14,
+
+  NoEnemy = 90,
+  Selector = 91,
+}
 
 export {
   ships,
@@ -14,7 +34,6 @@ export {
   ShipTypeEquippable,
   maps,
   MapEnemyFleet,
-  MapNodeType,
   MapNode,
   MapLink,
   MapData,
