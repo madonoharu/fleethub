9c3993e730592212d57a53586cf84f599bc9464d
まどの <madonoharu@gmail.com>
2020-06-16 23:40:43 +0900

    Add Dialog fullHeight

--- packages/client/src/components/organisms/Dialog/Dialog.tsx
+++ packages/client/src/components/organisms/Dialog/Dialog.tsx
@@ -1,29 +1,43 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-import { Dialog as MuiDialog, DialogProps as MuiDialogProps, withStyles } from "@material-ui/core"
+import { Dialog as MuiDialog, DialogProps as MuiDialogProps } from "@material-ui/core"
 
 import { CloseButton } from "../../../components"
 
 const StyledCloseButton = styled(CloseButton)`
   position: absolute;
-  top: -40px;
-  right: -40px;
+  top: 0px;
+  right: 0px;
 `
 
-const StyledMuiDialog = withStyles({
-  paper: {
-    overflowY: "visible",
-  },
-})(MuiDialog)
+type ContainerProps = {
+  fullHeight?: boolean
+}
 
-export type DialogProps = Partial<MuiDialogProps>
+const Container: React.FCX<ContainerProps> = ({ fullHeight, ...divProps }) => <div {...divProps} />
 
-const Dialog: React.FC<DialogProps> = ({ children, ...rest }) => (
-  <StyledMuiDialog open={false} {...rest}>
-    <StyledCloseButton onClick={(event) => rest.onClose?.(event, "backdropClick")} />
-    {children}
-  </StyledMuiDialog>
+const ScrollContainer = styled(Container)`
+  max-width: 100%;
+  max-height: 100%;
+  overflow: scroll;
+
+  ${(props) =>
+    props.fullHeight &&
+    css`
+      height: calc(100vh - 64px);
+    `}
+`
+
+export type DialogProps = Partial<MuiDialogProps> & {
+  fullHeight?: boolean
+}
+
+const Dialog: React.FC<DialogProps> = ({ children, fullHeight, ...rest }) => (
+  <MuiDialog open={false} {...rest}>
+    <StyledCloseButton size="small" onClick={(event) => rest.onClose?.(event, "backdropClick")} />
+    <ScrollContainer fullHeight={fullHeight}>{children}</ScrollContainer>
+  </MuiDialog>
 )
 
 export default Dialog
