f4ed6205dd6094b7167f445673d99f713e4c2077
まどの <madonoharu@gmail.com>
2020-06-17 22:45:11 +0900

    Remove some dead code

--- packages/client/src/components/atoms/Acrylic/Acrylic.tsx
+++ packages/client/src/components/atoms/Acrylic/Acrylic.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import styled, { css } from "styled-components"
 
-import { Dialog as MuiDialog, DialogProps as MuiDialogProps, Paper } from "@material-ui/core"
-
-import { CloseButton } from "../../../components"
+import { Paper } from "@material-ui/core"
 
 const acrylicStyle = css`
   background: rgba(255, 255, 255, 0.1);
