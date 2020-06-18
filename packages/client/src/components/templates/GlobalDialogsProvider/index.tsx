e7354964ee23fce1e7ddad5d0cb1b9dbcfa7a994
まどの <madonoharu@gmail.com>
2020-06-18 21:45:32 +0900

    Fix Dialogs

--- packages/client/src/components/templates/GlobalDialogsProvider/index.tsx
+++ packages/client/src/components/templates/GlobalDialogsProvider/index.tsx
@@ -1,28 +1,21 @@
 import React from "react"
 import styled from "styled-components"
 
-import { Dialog as MuiDialog, DialogProps, useTheme } from "@material-ui/core"
-
 import { GearSelectProvider, useGearSelectContext, useShipSelectContext, ShipSelectProvider } from "../../../hooks"
-import { ShipSelect, GearSelect } from "../../../components"
+import { ShipSelect, GearSelect, Dialog, DialogProps } from "../../../components"
 
-const Dialog: React.FC<DialogProps> = ({ children, ...rest }) => {
-  const theme = useTheme()
-  const width = theme.breakpoints.width("md")
-  return (
-    <MuiDialog maxWidth="md" PaperProps={{ style: { height: "80vh", width, padding: 8 } }} {...rest}>
-      <div>{children}</div>
-    </MuiDialog>
-  )
-}
-
+const StyledDialog: React.FC<DialogProps> = ({ children, ...rest }) => (
+  <Dialog fullHeight fullWidth PaperProps={{ style: { padding: 8 } }} {...rest}>
+    <div>{children}</div>
+  </Dialog>
+)
 const ShipSelectDialog: React.FC = () => {
   const { state, actions } = useShipSelectContext()
   const open = Boolean(state.onSelect)
   return (
-    <Dialog open={open} onClose={actions.close}>
+    <StyledDialog open={open} onClose={actions.close}>
       {open && <ShipSelect />}
-    </Dialog>
+    </StyledDialog>
   )
 }
 
@@ -31,9 +24,9 @@ const GearSelectDialog: React.FC = () => {
   const open = Boolean(state.onSelect)
 
   return (
-    <Dialog open={open} onClose={actions.close}>
+    <StyledDialog open={open} onClose={actions.close}>
       {open && <GearSelect state={state} onUpdate={actions.update} />}
-    </Dialog>
+    </StyledDialog>
   )
 }
 
