c6a3fa6b7027812e12a9a8ffcd326d8463573af5
まどの <madonoharu@gmail.com>
2020-06-24 10:08:28 +0900

    Move hooks

--- packages/client/src/hooks/index.ts
+++ packages/client/src/hooks/index.ts
@@ -1,11 +1,26 @@
-import React from "react"
+import { useState, useRef, useEffect, useDebugValue } from "react"
+
+export const useSelect = <T>(options: readonly T[], defaultOption: T = options[0]) => {
+  const [value, onChange] = useState(defaultOption)
+
+  useEffect(() => {
+    if (!options.includes(value)) {
+      onChange(defaultOption)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [options, onChange])
+
+  useEffect(() => onChange(defaultOption), [defaultOption])
+
+  return { options, value, onChange }
+}
 
 export const useRenderCount = () => {
-  const renderCountRef = React.useRef(0)
+  const renderCountRef = useRef(0)
 
-  React.useDebugValue(`このコンポーネントは${renderCountRef.current}回再描画されました`)
+  useDebugValue(`このコンポーネントは${renderCountRef.current}回再描画されました`)
 
-  React.useEffect(() => {
+  useEffect(() => {
     renderCountRef.current++
   })
 }
