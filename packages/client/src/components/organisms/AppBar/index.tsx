e08b5828758d69b1ee0091e5dc9c4d34d501812f
まどの <madonoharu@gmail.com>
2020-06-25 19:21:52 +0900

    Add Undo Redo Button

--- packages/client/src/components/organisms/AppBar/index.tsx
+++ packages/client/src/components/organisms/AppBar/index.tsx
@@ -5,13 +5,29 @@ import { useTranslation } from "react-i18next"
 import AppBar from "@material-ui/core/AppBar"
 import Box from "@material-ui/core/Box"
 import MuiButton from "@material-ui/core/Button"
+import UndoIcon from "@material-ui/icons/Undo"
+import RedoIcon from "@material-ui/icons/Redo"
 
 import { Link, Select } from "../../../components"
 
 import LanguageSelect from "./LanguageSelect"
 
+import { useDispatch, useSelector } from "react-redux"
+import { ActionCreators } from "redux-undo"
+import { withIconButton } from "../../molecules"
+
+const UndoButton = withIconButton(UndoIcon)
+const RedoButton = withIconButton(RedoIcon)
+
 const Component: React.FCX = ({ className }) => {
   const { t } = useTranslation()
+
+  const dispatch = useDispatch()
+  const canUndo = useSelector((state) => state.entities.past.length > 0)
+  const canRedo = useSelector((state) => state.entities.future.length > 0)
+
+  const undo = () => canUndo && dispatch(ActionCreators.undo())
+  const redo = () => canRedo && dispatch(ActionCreators.redo())
 
   const paths = [
     { to: "/", children: t("home") },
@@ -25,6 +41,8 @@ const Component: React.FCX = ({ className }) => {
         {paths.map((path) => (
           <Link key={path.to} {...path} />
         ))}
+        <UndoButton size="small" disabled={!canUndo} onClick={undo} />
+        <RedoButton size="small" disabled={!canRedo} onClick={redo} />
         <LanguageSelect />
       </Box>
     </AppBar>
