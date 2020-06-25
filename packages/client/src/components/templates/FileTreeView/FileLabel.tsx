367ec1f7c9ce2c399df61bfd7d67624d3cf21342
まどの <madonoharu@gmail.com>
2020-06-25 22:20:20 +0900

    Improve FileTreeView

---
+++ packages/client/src/components/templates/FileTreeView/FileLabel.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Flexbox } from "../../../components"
+
+const FileLabelAction = styled.div`
+  display: none;
+  margin-left: auto;
+`
+
+const FileLabelText = styled.div`
+  width: calc(100% - 120px);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`
+
+type Props = {
+  icon: React.ReactNode
+  text: React.ReactNode
+  action: React.ReactNode
+  onClick?: () => void
+}
+
+const handleActionClick = (event: React.MouseEvent) => event.preventDefault()
+
+const FileLabel: React.FCX<Props> = ({ className, icon, text, action, onClick }) => {
+  return (
+    <Flexbox className={className}>
+      {icon}
+      <FileLabelText onClick={onClick}>{text}</FileLabelText>
+      <FileLabelAction onClick={handleActionClick}>{action}</FileLabelAction>
+    </Flexbox>
+  )
+}
+
+export default styled(FileLabel)`
+  height: 24px;
+
+  :hover ${FileLabelAction} {
+    display: block;
+  }
+`
