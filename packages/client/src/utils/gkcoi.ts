a86b60a0b34936559ece83790dee25086f75ca9b
まどの <madonoharu@gmail.com>
2020-06-29 02:02:06 +0900

    Add Deck4 airbase

--- packages/client/src/utils/gkcoi.ts
+++ packages/client/src/utils/gkcoi.ts
@@ -1,5 +1,5 @@
-import { DeckBuilder, DeckBuilderFleet, DeckBuilderShip } from "gkcoi"
-import { Ship, Fleet, Plan, Equipment, ShipKey, FleetKey } from "@fleethub/core"
+import { DeckBuilder, DeckBuilderFleet, DeckBuilderShip, DeckBuilderAirbase } from "gkcoi"
+import { Ship, Fleet, Plan, Equipment, ShipKey, FleetKey, AirbaseKey } from "@fleethub/core"
 import { Dict } from "@fleethub/utils"
 
 const getDeckItems = (equipment: Equipment) => {
@@ -88,14 +88,21 @@ const getDeck4Fleet = (fleet: Fleet) => {
   return deckFleet
 }
 
-type Deck4 = { version: 4; hqlv: number } & Dict<FleetKey, ReturnType<typeof getDeck4Fleet>>
+type Deck4 = { version: 4; hqlv: number } & Dict<FleetKey, ReturnType<typeof getDeck4Fleet>> &
+  Dict<AirbaseKey, DeckBuilderAirbase>
 
 export const getDeck4 = (plan: Plan) => {
   const deck: Deck4 = { version: 4, hqlv: plan.hqLevel }
 
   plan.fleetEntries.forEach(([key, fleet]) => {
     if (fleet.ships.length === 0) return
-    deck[key] = fleetToDeck(fleet)
+    deck[key] = getDeck4Fleet(fleet)
+  })
+
+  plan.airbaseEntries.forEach(([key, airbase]) => {
+    if (airbase.equipment.gears.length === 0) return
+    const items = getDeckItems(airbase.equipment)
+    deck[key] = { items }
   })
 
   return deck
