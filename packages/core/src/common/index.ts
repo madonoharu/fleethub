a9fa42f268f07addb80cb0c128a755a6dc87bcea
まどの <madonoharu@gmail.com>
2020-06-30 00:32:44 +0900

    Fix utils

--- packages/core/src/common/index.ts
+++ packages/core/src/common/index.ts
@@ -3,5 +3,5 @@ export { Formation } from "./Formation"
 export { default as Engagement } from "./Engagement"
 export { default as FleetTypeName } from "./FleetTypeName"
 export { default as BattleType } from "./BattleType"
-
+export { default as ProficiencyExp } from "./ProficiencyExp"
 export { default as EnemyTypeId } from "./EnemyTypeId"
