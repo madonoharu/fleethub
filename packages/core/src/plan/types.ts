54260ab6867bfc33ab83c0ed75b262dc88fadf47
まどの <madonoharu@gmail.com>
2020-06-30 04:18:16 +0900

    Add Organization

--- packages/core/src/plan/types.ts
+++ packages/core/src/plan/types.ts
@@ -3,13 +3,10 @@ import { Dict } from "@fleethub/utils"
 
 import { FleetState, Fleet } from "../fleet"
 import { AirbaseState, Airbase } from "../airbase"
-import { Formation } from "../common"
+import { Formation, FleetKey, AirbaseKey } from "../common"
 import { EnemyFleetState } from "../enemy"
 
-export type FleetKey = "f1" | "f2" | "f3" | "f4"
 type FleetDict = Dict<FleetKey, FleetState>
-
-export type AirbaseKey = "a1" | "a2" | "a3"
 type AirbaseDict = Dict<AirbaseKey, AirbaseState>
 
 export type NodePlan = Pick<MapNode, "type" | "d" | "point"> & {
@@ -27,6 +24,8 @@ export type PlanStateBase = {
 
 export type PlanState = PlanStateBase & FleetDict & AirbaseDict
 
+export type Organization = Record<FleetKey, Fleet> & Record<AirbaseKey, Airbase>
+
 export type Plan = Required<PlanStateBase> & {
   state: PlanState
 
@@ -35,4 +34,4 @@ export type Plan = Required<PlanStateBase> & {
 
   interceptionPower: number
   highAltitudeInterceptionPower: number
-}
+} & Organization
