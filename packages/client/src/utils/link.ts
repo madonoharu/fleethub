2bdddeca2d10bbe17791fa04ec41d063e2e34d11
まどの <madonoharu@gmail.com>
2020-06-29 21:36:29 +0900

    Make link

---
+++ packages/client/src/utils/link.ts
@@ -0,0 +1,27 @@
+import { Plan, getDeck4 } from "@fleethub/core"
+
+const getPredeckUrl = (base: string, plan: Plan) => {
+  const url = new URL("https://noro6.github.io/kcTools/")
+  const deck = getDeck4(plan)
+
+  url.searchParams.set("predeck", JSON.stringify(deck))
+  if (url.href.length < 7900) return url.href
+
+  delete deck.f4
+  url.searchParams.set("predeck", JSON.stringify(deck))
+  if (url.href.length < 7900) return url.href
+
+  delete deck.f3
+  url.searchParams.set("predeck", JSON.stringify(deck))
+  return url.href
+}
+
+export const openKctools = (plan: Plan) => {
+  const url = getPredeckUrl("https://noro6.github.io/kcTools/", plan)
+  window.open(url, "_blank", "noopener")
+}
+
+export const openDeckbuilder = (plan: Plan) => {
+  const url = getPredeckUrl("http://kancolle-calc.net/deckbuilder.html", plan)
+  window.open(url, "_blank", "noopener")
+}
