846f38362223a386242e70554ca37b045374f155
まどの <madonoharu@gmail.com>
2020-06-29 21:54:31 +0900

    Add parseDeck

--- packages/client/src/components/organisms/PlanImportContent/PlanImportContent.tsx
+++ packages/client/src/components/organisms/PlanImportContent/PlanImportContent.tsx
@@ -2,21 +2,47 @@ import React from "react"
 import { Plan } from "@fleethub/core"
 import styled from "styled-components"
 
-import { TextField } from "@material-ui/core"
-import OpenInNew from "@material-ui/icons/OpenInNew"
+import { TextField, Button } from "@material-ui/core"
 
-import { uiSlice, plansSelectors, filesSlice, openFirstPlan } from "../../../store"
-import { CopyTextButton, Flexbox } from "../../../components"
-import { getDeck4, openKctools, openDeckbuilder } from "../../../utils"
+import { useSnackbar, useFhSystem } from "../../../hooks"
 
 type Props = {
-  onImport: () => void
+  onOverwrite?: (plan: Plan) => void
+  onImport?: (plan: Plan) => void
 }
-const PlanImportContent: React.FCX<{ plan: Plan }> = ({ className, plan }) => {
-  const [deck, setDeck] = React.useState("")
+const PlanImportContent: React.FCX<Props> = ({ className, onOverwrite, onImport }) => {
+  const [str, setStr] = React.useState("")
+  const Snackbar = useSnackbar()
+
+  const fh = useFhSystem()
+
+  const parseDeck = (str: string): Plan | undefined => {
+    try {
+      const deck = JSON.parse(str)
+      return fh.createPlanByDeck(deck)
+    } catch {
+      Snackbar.show({ message: "読み込みに失敗しました", severity: "error" })
+      return
+    }
+  }
+
+  const handleOverwrite = () => {
+    const plan = parseDeck(str)
+    if (plan) onOverwrite?.(plan)
+  }
+
+  const handleImport = () => {
+    const plan = parseDeck(str)
+    if (plan) onImport?.(plan)
+  }
+
   return (
     <div className={className}>
-      <TextField value={deck} onChange={(event) => setDeck(event.currentTarget.value)} />
+      <TextField label="デッキビルダー形式" value={str} onChange={(event) => setStr(event.currentTarget.value)} />
+      <Button onClick={handleOverwrite}>現在の編成に上書き</Button>
+      <Button onClick={handleImport}>新しい編成を作成</Button>
+
+      <Snackbar />
     </div>
   )
 }
