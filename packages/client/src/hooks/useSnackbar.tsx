bb9b54998ea981779387b3af5d2a74f54d8e2f33
まどの <madonoharu@gmail.com>
2020-06-29 18:23:21 +0900

    Add useSnackbar

---
+++ packages/client/src/hooks/useSnackbar.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useCallback } from "react"
+
+import { Snackbar as MuiSnackbar, SnackbarProps as MuiSnackbarProps } from "@material-ui/core"
+import Alert, { AlertProps } from "@material-ui/lab/Alert"
+
+type SnackbarProps = MuiSnackbarProps &
+  Pick<AlertProps, "severity"> & {
+    message?: React.ReactNode
+  }
+
+const Snackbar: React.FC<SnackbarProps> = ({ severity, message, ...rest }) => (
+  <MuiSnackbar {...rest}>
+    <Alert variant="filled" severity={severity}>
+      {message}
+    </Alert>
+  </MuiSnackbar>
+)
+
+export const useSnackbar = () => {
+  const [state, setState] = useState<SnackbarProps>({})
+  const show = useCallback((props?: SnackbarProps) => setState({ open: true, ...props }), [])
+  const hide = useCallback(() => setState({ open: false }), [])
+
+  const Modal: React.FC<SnackbarProps> = useCallback((props) => <Snackbar onClose={hide} {...state} {...props} />, [
+    state,
+    hide,
+  ])
+
+  return Object.assign(Modal, {
+    isOpen: state.open,
+    show,
+    hide,
+  })
+}
