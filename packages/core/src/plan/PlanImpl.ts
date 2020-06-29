eaeaf2ac0f5c935582b2224332ad41bc4b1b7d4c
まどの <madonoharu@gmail.com>
2020-06-30 04:18:16 +0900

    Add Organization

--- packages/core/src/plan/PlanImpl.ts
+++ packages/core/src/plan/PlanImpl.ts
@@ -1,22 +1,46 @@
-import { Plan } from "./types"
+import { FleetKeys, AirbaseKeys } from "../common"
+
+import { Plan, Organization } from "./types"
 
 export class PlanImpl implements Plan {
   public name: Plan["name"]
   public hqLevel: Plan["hqLevel"]
   public nodes: Plan["nodes"]
 
-  constructor(
-    public state: Plan["state"],
-    public fleetEntries: Plan["fleetEntries"],
-    public airbaseEntries: Plan["airbaseEntries"]
-  ) {
+  public f1: Plan["f1"]
+  public f2: Plan["f2"]
+  public f3: Plan["f3"]
+  public f4: Plan["f4"]
+
+  public a1: Plan["a1"]
+  public a2: Plan["a2"]
+  public a3: Plan["a3"]
+
+  constructor(public state: Plan["state"], { f1, f2, f3, f4, a1, a2, a3 }: Organization) {
     this.name = state.name || ""
     this.hqLevel = state.hqLevel || 120
     this.nodes = state.nodes || []
+
+    this.f1 = f1
+    this.f2 = f2
+    this.f3 = f3
+    this.f4 = f4
+
+    this.a1 = a1
+    this.a2 = a2
+    this.a3 = a3
+  }
+
+  get fleetEntries(): Plan["fleetEntries"] {
+    return FleetKeys.map((key) => [key, this[key]])
+  }
+
+  get airbaseEntries(): Plan["airbaseEntries"] {
+    return AirbaseKeys.map((key) => [key, this[key]])
   }
 
   get airbases() {
-    return this.airbaseEntries.map((entry) => entry[1])
+    return AirbaseKeys.map((key) => this[key])
   }
 
   get interceptionPower() {
