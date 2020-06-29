1465d94ec5879d47f136ee31d17894f367e4003d
まどの <madonoharu@gmail.com>
2020-06-29 17:02:11 +0900

    Make lazy AppContent

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,20 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
 import "./i18n"
 import { StoreProvider } from "./store"
 import { ThemeProvider } from "./theme"
-import { AppContent } from "./components"
+
+const AppContent = React.lazy(() => import("./components/templates/AppContent"))
 
 const App: React.FC = () => (
   <StoreProvider>
     <DndProvider backend={HTML5Backend}>
       <ThemeProvider>
-        <AppContent />
+        <Suspense fallback={"loading"}>
+          <AppContent />
+        </Suspense>
       </ThemeProvider>
     </DndProvider>
   </StoreProvider>
