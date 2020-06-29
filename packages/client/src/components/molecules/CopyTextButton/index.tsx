ce8b96fca35addbafe20316e4f394e5db0402a58
まどの <madonoharu@gmail.com>
2020-06-29 19:53:43 +0900

    Fix CopyTextButton

--- packages/client/src/components/molecules/CopyTextButton/index.tsx
+++ packages/client/src/components/molecules/CopyTextButton/index.tsx
@@ -1,19 +1,12 @@
 import React from "react"
 import copy from "copy-to-clipboard"
 
-import { Tooltip as MuiTooltip, withStyles } from "@material-ui/core"
 import Assignment from "@material-ui/icons/Assignment"
-import Alert from "@material-ui/lab/Alert"
 
 import { withIconButton } from "../IconButtons"
+import { useSnackbar } from "../../../hooks"
 
 const AssignmentButton = withIconButton(Assignment)
-
-const StyledTooltip = withStyles({
-  tooltip: {
-    padding: 0,
-  },
-})(MuiTooltip)
 
 type Props = {
   value: string
@@ -21,28 +14,19 @@ type Props = {
 }
 
 const CopyTextButton: React.FC<Props> = ({ value, message = "copied" }) => {
-  const [open, setOpen] = React.useState(false)
+  const Snackbar = useSnackbar()
 
   const handleClick = () => {
     const result = copy(value)
-    if (result) setOpen(true)
+    if (result) Snackbar.show({ message: "コピーしました" })
+    else Snackbar.show({ message: "失敗しました", severity: "error" })
   }
 
   return (
-    <StyledTooltip
-      open={open}
-      onClose={() => setOpen(false)}
-      placement="top"
-      title={
-        <Alert variant="outlined" severity="success">
-          {message}
-        </Alert>
-      }
-    >
-      <span>
-        <AssignmentButton title="コピー" onClick={handleClick} />
-      </span>
-    </StyledTooltip>
+    <>
+      <AssignmentButton title="コピー" onClick={handleClick} />
+      <Snackbar />
+    </>
   )
 }
 
