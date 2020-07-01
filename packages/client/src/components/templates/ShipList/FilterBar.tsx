b86910cfbd99868126fa46ed1e5ce5ddfc8d7417
まどの <madonoharu@gmail.com>
2020-07-01 23:44:49 +0900

    Fix ShipListState

--- packages/client/src/components/templates/ShipList/FilterBar.tsx
+++ packages/client/src/components/templates/ShipList/FilterBar.tsx
@@ -1,10 +1,21 @@
 import React from "react"
-import { shipCategoies } from "@fleethub/core"
+import { shipCategoies, ShipCategory } from "@fleethub/core"
 
 import { Checkbox, FormControlLabel } from "@material-ui/core"
 
 import { SelectButtons, Flexbox } from "../../../components"
 import { ShipListState } from "../../../store"
+
+const dict: Record<ShipCategory, string> = {
+  Battleship: "戦艦級",
+  AircraftCarrier: "航空母艦",
+  HeavyCruiser: "重巡級",
+  LightCruiser: "軽巡級",
+  Destroyer: "駆逐艦",
+  CoastalDefenseShip: "海防艦",
+  Submarine: "潜水艦",
+  SupportShip: "補助艦艇",
+}
 
 type Props = {
   state: ShipListState
@@ -12,20 +23,24 @@ type Props = {
 }
 
 const FilterBar: React.FCX<Props> = ({ state, onChange }) => {
-  const toggleCommonly = () => onChange({ commonly: !state.commonly })
+  const toggleAll = () => onChange({ all: !state.all })
   const toggleAbyssal = () => onChange({ abyssal: !state.abyssal })
 
   return (
     <div>
-      <FormControlLabel
-        label="未改造表示"
-        control={<Checkbox size="small" checked={state.commonly} onClick={toggleCommonly} />}
-      />
-
       <Flexbox>
-        <SelectButtons options={shipCategoies} value={state.category} onChange={(category) => onChange({ category })} />
+        <SelectButtons
+          options={shipCategoies}
+          value={state.category}
+          onChange={(category) => onChange({ category })}
+          getOptionLabel={(category) => dict[category]}
+        />
         <FormControlLabel
           style={{ marginLeft: "auto" }}
+          label="全表示"
+          control={<Checkbox size="small" checked={state.all} onClick={toggleAll} />}
+        />
+        <FormControlLabel
           label="深海"
           control={<Checkbox size="small" checked={state.abyssal} onClick={toggleAbyssal} />}
         />
