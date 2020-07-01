c8e10f4ad0a4ba977d1975390e29aa9e8a86ef48
まどの <madonoharu@gmail.com>
2020-07-01 18:48:28 +0900

    Fix modules

--- packages/client/src/components/molecules/index.ts
+++ packages/client/src/components/molecules/index.ts
@@ -1,6 +1,5 @@
 export { default as GearStarsSelect } from "./GearStarsSelect"
 export { default as GearExpSelect } from "./GearExpSelect"
-export { default as NumberInput } from "./NumberInput"
 export { default as SearchInput } from "./SearchInput"
 export { default as CopyTextButton } from "./CopyTextButton"
 export { default as SelectButtons } from "./SelectButtons"
@@ -9,6 +8,9 @@ export { default as GearNameplate } from "./GearNameplate"
 export { default as ShipBanner } from "./ShipBanner"
 export { default as GearIcon } from "./GearIcon"
 export { default as StatIcon } from "./StatIcon"
+
+export { default as NumberInput } from "./NumberInput"
+export * from "./NumberInput"
 
 export { default as KctoolsButton } from "./KctoolsButton"
 export * from "./KctoolsButton"
