256ae1d15f3b1dd508eac54421c2033b9dc2ef29
まどの <madonoharu@gmail.com>
2020-07-01 20:01:34 +0900

    Fix store files

--- packages/client/src/store/createStore.ts
+++ packages/client/src/store/createStore.ts
@@ -1,18 +1,13 @@
-import React from "react"
 import { combineReducers, configureStore, getDefaultMiddleware, AnyAction } from "@reduxjs/toolkit"
-import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
 import storage from "localforage"
 import undoable, { UndoableOptions } from "redux-undo"
 import { ThunkAction } from "redux-thunk"
-
-import { Provider as ReduxProvider } from "react-redux"
-import { PersistGate } from "redux-persist/integration/react"
 
 import { makeGroupBy } from "../utils"
 
 import { appSlice } from "./appSlice"
 import { entitiesReducer } from "./entities"
-import { removeFile } from "./filesSlice"
 import { mapListSlice } from "./mapListSlice"
 import { shipListSlice } from "./shipListSlice"
 import { gearListSlice } from "./gearListSlice"
@@ -20,7 +15,7 @@ import { gearListSlice } from "./gearListSlice"
 const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
 
 const undoableOptions: UndoableOptions = {
-  filter: (action, currentState, previousHistory) => {
+  filter: (action) => {
     return ["entities", appSlice.name].some((key) => (action.type as string).startsWith(key))
   },
   groupBy: makeGroupBy(),
@@ -55,35 +50,10 @@ export const createStore = () => {
     }),
   })
 
-  window.addEventListener("beforeunload", () => {
-    store.dispatch(removeFile("temp"))
-  })
-
   return store
-}
-
-export const StoreProvider: React.FC = ({ children }) => {
-  const store = createStore()
-  const persistor = persistStore(store)
-
-  return (
-    <ReduxProvider store={store}>
-      <PersistGate persistor={persistor}>{children}</PersistGate>
-    </ReduxProvider>
-  )
 }
 
 type Store = ReturnType<typeof createStore>
 export type RootState = ReturnType<Store["getState"]>
 export type AppDispatch = Store["dispatch"]
 export type AppThunk = ThunkAction<void, RootState, typeof extraArgument, AnyAction>
-
-export * from "./appSlice"
-export * from "./plansSlice"
-export * from "./filesSlice"
-export * from "./entities"
-export * from "./mapListSlice"
-export * from "./shipListSlice"
-export * from "./gearListSlice"
-
-export * from "./selectors"
---
+++ packages/client/src/store/index.ts
@@ -0,0 +1,13 @@
+export * from "./createStore"
+
+export * from "./appSlice"
+export * from "./plansSlice"
+export * from "./filesSlice"
+export * from "./entities"
+export * from "./mapListSlice"
+export * from "./shipListSlice"
+export * from "./gearListSlice"
+
+export * from "./selectors"
+
+export { default as StoreProvider } from "./StoreProvider"
