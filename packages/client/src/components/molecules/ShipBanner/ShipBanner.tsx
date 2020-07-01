d0b9fa3bd9a025ac850ba21494be9ca5a18c2e70
まどの <madonoharu@gmail.com>
2020-07-02 01:53:36 +0900

    Add size prop

---
+++ packages/client/src/components/molecules/ShipBanner/ShipBanner.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Image } from "../../atoms"
+
+type Props = {
+  shipId: number
+  size?: "small" | "medium"
+}
+
+const ShipBanner: React.FCX<Props> = ({ className, shipId, size = "small" }) => {
+  const scale = size === "small" ? 3 : 4
+  return <Image className={className} height={scale * 8} width={scale * 32} src={`ships/1.webp`} />
+}
+
+export default styled(ShipBanner)``
