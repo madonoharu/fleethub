3315ea900fd723253e45ad016193e8d39458bf82
まどの <madonoharu@gmail.com>
2020-07-01 18:24:44 +0900

    Add uniq

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -13,6 +13,8 @@ export const mapValues = <T, R>(obj: T, fn: (value: T[keyof T], key: keyof T) =>
   return nextObj as Record<keyof T, R>
 }
 
+export const uniq = <T>(array: T[]) => [...new Set(array)]
+
 export type Dict<K extends string | number | symbol, T> = Partial<Record<K, T>>
 
 export type NullableArray<T> = Array<T | undefined>
