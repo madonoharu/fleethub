044f8749ccc9f0fc45d61f4f3f5e115ac5a3f424
まどの <madonoharu@gmail.com>
2020-07-03 20:04:51 +0900

    Add LabeledValue

---
+++ packages/client/src/components/atoms/LabeledValue/LabeledValue.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+
+const LabeledValue: React.FCX<{ label: React.ReactNode; value: React.ReactNode }> = ({ className, label, value }) => (
+  <div className={className}>
+    <div>{label}</div>
+    <div>{value}</div>
+  </div>
+)
+
+export default styled(LabeledValue)`
+  display: flex;
+  justify-content: space-between;
+
+  > :first-child {
+    color: ${(props) => props.theme.palette.text.secondary};
+    font-size: 12px;
+    margin-right: 8px;
+  }
+`
