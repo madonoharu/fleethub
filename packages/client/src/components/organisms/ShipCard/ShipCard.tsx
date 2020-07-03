bdec7197b01f2dc07bba8048af383f0402e394f5
まどの <madonoharu@gmail.com>
2020-07-04 04:30:30 +0900

    Fix ShipCard

--- packages/client/src/components/organisms/ShipCard/ShipCard.tsx
+++ packages/client/src/components/organisms/ShipCard/ShipCard.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { Ship, ShipState } from "@fleethub/core"
 
-import { Box, Paper } from "@material-ui/core"
+import { Paper } from "@material-ui/core"
 
 import { EquipmentList, ShipBanner } from "../../../components"
 import { Update } from "../../../utils"
@@ -40,10 +40,10 @@ const ShipCard: React.FCX<Props> = ({ className, ship, update, onRemove }) => {
       <ShipHeader ship={ship} update={update} onRemove={onRemove} />
 
       <Content>
-        <Box width={128}>
-          <ShipBanner shipId={ship.shipId} />
+        <div>
+          <ShipBanner shipId={ship.shipId} size="medium" />
           <ShipStats ship={ship} />
-        </Box>
+        </div>
 
         <EquipmentList
           equipment={ship.equipment}
@@ -57,8 +57,6 @@ const ShipCard: React.FCX<Props> = ({ className, ship, update, onRemove }) => {
 }
 
 const Styled = styled(ShipCard)`
-  min-height: ${24 * 7}px;
-
   ${ShipHeader} svg {
     opacity: 0;
   }
