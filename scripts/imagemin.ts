e50c001b76d98317e26686036501ccf059098f69
まどの <madonoharu@gmail.com>
2020-07-04 00:24:48 +0900

    Fix imagemin

--- scripts/imagemin.ts
+++ scripts/imagemin.ts
@@ -1,11 +1,16 @@
 import imagemin from "imagemin"
 import imageminWebp from "imagemin-webp"
 
-const paths = ["filters", "gears", "proficiency", "ships", "stats"]
+const paths = ["filters", "gears", "proficiency", "stats", "icons"]
 
 paths.forEach((path) => {
-  imagemin([`images/${path}/*.{jpg,png}`], {
-    destination: `packages/client/quality/images/${path}`,
-    plugins: [imageminWebp({ method: 6, quality: 100 })],
+  imagemin([`packages/client/src/images/png/${path}/*.png`], {
+    destination: `packages/client/src/images/webp/${path}`,
+    plugins: [imageminWebp({ method: 6 })],
   })
+})
+
+imagemin([`private/ships/*.png`], {
+  destination: `packages/client/src/images/webp/ships`,
+  plugins: [imageminWebp({ method: 6, resize: { width: 160, height: 40 } })],
 })
