bd9350658ea94ffc33bf9b7ff15d076a3e916198
まどの <madonoharu@gmail.com>
2020-07-04 01:07:31 +0900

    Move GearNameplate

--- packages/client/src/components/molecules/index.ts
+++ packages/client/src/components/molecules/index.ts
@@ -3,7 +3,6 @@ export { default as GearExpSelect } from "./GearExpSelect"
 export { default as SearchInput } from "./SearchInput"
 export { default as CopyTextButton } from "./CopyTextButton"
 export { default as SelectButtons } from "./SelectButtons"
-export { default as GearNameplate } from "./GearNameplate"
 
 export { default as ShipBanner } from "./ShipBanner"
 export { default as GearIcon } from "./GearIcon"
