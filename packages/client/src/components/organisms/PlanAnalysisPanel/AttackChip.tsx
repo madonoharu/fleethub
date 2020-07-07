18a1c4d88b723320b624a6776797271ff71fd2aa
まどの <madonoharu@gmail.com>
2020-07-07 22:31:32 +0900

    Add night

--- packages/client/src/components/organisms/PlanAnalysisPanel/AttackChip.tsx
+++ packages/client/src/components/organisms/PlanAnalysisPanel/AttackChip.tsx
@@ -1,32 +1,32 @@
 import React from "react"
 import styled, { css } from "styled-components"
-import { DaySpecialAttack } from "@fleethub/core"
+import { DaySpecialAttack, NightSpecialAttack } from "@fleethub/core"
 
 import { Chip } from "@material-ui/core"
-import { orange } from "@material-ui/core/colors"
 
-const dictionary: Record<DaySpecialAttack["type"], string> = {
-  MainMain: "主主",
-  MainApShell: "主徹",
-  MainRader: "主電",
-  MainSecond: "主副",
-  DoubleAttack: "連撃",
-  Zuiun: "瑞雲",
-  Suisei: "海空",
-  FBA: "FBA",
-  BBA: "BBA",
-  BA: "BA",
+type Props = {
+  night?: boolean
+  attack: DaySpecialAttack | NightSpecialAttack
 }
 
-const AttackChip: React.FCX<{ attack: DaySpecialAttack }> = ({ className, attack }) => {
-  return <Chip className={className} variant="outlined" size="small" label={dictionary[attack.type]} />
+const AttackChip: React.FCX<Props> = ({ className, attack }) => {
+  return <Chip className={className} variant="outlined" size="small" label={attack.name} />
 }
+
+const dayStyle = css`
+  border-color: ${(props) => props.theme.kc.palette.shelling};
+  color: ${(props) => props.theme.kc.palette.shelling};
+`
+
+const nightStyle = css`
+  min-width: 48px;
+  border-color: ${(props) => props.theme.kc.palette.night};
+  color: ${(props) => props.theme.kc.palette.night};
+`
 
 export default styled(AttackChip)`
-  width: 48px;
+  min-width: 48px;
   border-radius: 4px;
-  ${(props) => css`
-    border-color: ${props.theme.kc.palette.shelling};
-    color: ${props.theme.kc.palette.shelling};
-  `}
+
+  ${(props) => (props.night ? nightStyle : dayStyle)}
 `
