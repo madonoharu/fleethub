1e6a554985c68796ed6ec5957bd76097ec3b9186
まどの <madonoharu@gmail.com>
2020-07-09 09:36:27 +0900

    Improve RateMap

--- packages/core/src/utils/RateMap.ts
+++ packages/core/src/utils/RateMap.ts
@@ -3,15 +3,32 @@ export class RateMap<Key> extends Map<Key, number> {
     super()
   }
 
+  public sumBy(cb: (rate: number, key: Key) => number) {
+    let result = 0
+
+    this.forEach((rate, key) => {
+      result += cb(rate, key)
+    })
+
+    return result
+  }
+
   get total() {
-    let total = 0
-    for (const value of this.values()) {
-      total += value
-    }
-    return total
+    return this.sumBy((rate) => rate)
   }
 
   get complement() {
     return 1 - this.total
+  }
+
+  public toArray(): Array<[Key, number]>
+  public toArray<R>(mapfn: (entry: [Key, number], index: number) => R): R[]
+  public toArray<R>(mapfn?: (entry: [Key, number], index: number) => R) {
+    return mapfn ? Array.from(this, mapfn) : Array.from(this)
+  }
+
+  public upsert(key: Key, cb: (old?: number) => number) {
+    const old = this.get(key)
+    this.set(key, cb(old))
   }
 }
