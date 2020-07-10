2cfdc13374aabf1e054e20869ab68640960b217c
まどの <madonoharu@gmail.com>
2020-07-11 07:16:13 +0900

    Add FleetTabLabel

--- packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
+++ packages/client/src/components/templates/PlanEditor/PlanEditor.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import styled from "styled-components"
-import { FleetTypes } from "@fleethub/core"
+import { FleetKey } from "@fleethub/core"
 
 import { Container } from "@material-ui/core"
 
-import { SelectButtons, PlanShareContent, Select, PlanAnalysisPanel } from "../../../components"
+import { PlanShareContent, PlanAnalysisPanel, Tabs } from "../../../components"
 import { usePlan, useModal } from "../../../hooks"
 
 import FleetTabPanel from "./FleetTabPanel"
@@ -12,8 +12,16 @@ import LandBaseTabPanel from "./LandBaseTabPanel"
 import GkcoiTabPanel from "./GkcoiTabPanel"
 import BattlePlanPanel from "./BattlePlanPanel"
 import PlanEditorHeader from "./PlanEditorHeader"
+import FleetTabLabel from "./FleetTabLabel"
 
-const tabOptions = ["f1", "f2", "f3", "f4", "lb", "Gkcoi"] as const
+const Cont = styled.div`
+  margin: 0 auto;
+  width: ${(props) => props.theme.breakpoints.width("md")}px;
+`
+
+const StyledPlanAnalysisPanel = styled(PlanAnalysisPanel)`
+  margin-top: 8px;
+`
 
 type Props = {
   planId: string
@@ -21,38 +29,45 @@ type Props = {
 
 const PlanEditor: React.FC<Props> = ({ planId }) => {
   const { plan, actions, state } = usePlan(planId)
-  const [tabKey, setTabKey] = React.useState<typeof tabOptions[number]>("f1")
 
   const Modal = useModal()
 
   if (!plan || !state) return null
 
-  const fleetEntry = plan.fleetEntries.find(([key]) => key === tabKey)
+  const handleSwap = (drag: FleetKey, drop: FleetKey) => {
+    actions.update((draft) => {
+      const a = draft[drag]
+      const b = draft[drop]
+      draft[drag] = b
+      draft[drop] = a
+    })
+  }
+
+  const getTabItem = (fleetKey: FleetKey) => ({
+    label: <FleetTabLabel fleetKey={fleetKey} onSwap={handleSwap} />,
+    panel: <FleetTabPanel fleet={plan[fleetKey]} fleetKey={fleetKey} updatePlan={actions.update} />,
+  })
 
   return (
-    <Container>
+    <Cont>
       <PlanEditorHeader plan={plan} update={actions.update} />
-      <Select
-        options={FleetTypes}
-        value={plan.fleetType}
-        onChange={(value) =>
-          actions.update((draft) => {
-            draft.fleetType = value
-          })
-        }
+
+      <Tabs
+        list={[
+          getTabItem("f1"),
+          getTabItem("f2"),
+          getTabItem("f3"),
+          getTabItem("f4"),
+          { label: "基地", panel: <LandBaseTabPanel plan={plan} update={actions.update} /> },
+          { label: "画像出力", panel: <GkcoiTabPanel plan={plan} /> },
+        ]}
       />
-
-      <SelectButtons options={tabOptions} value={tabKey} onChange={setTabKey} />
-      {fleetEntry && <FleetTabPanel fleet={fleetEntry[1]} fleetKey={fleetEntry[0]} updatePlan={actions.update} />}
-      {tabKey === "lb" && <LandBaseTabPanel plan={plan} update={actions.update} />}
-      {tabKey === "Gkcoi" && <GkcoiTabPanel plan={plan} />}
-
-      <PlanAnalysisPanel plan={plan} />
+      <StyledPlanAnalysisPanel plan={plan} />
 
       <Modal>
         <PlanShareContent plan={plan} />
       </Modal>
-    </Container>
+    </Cont>
   )
 }
 
