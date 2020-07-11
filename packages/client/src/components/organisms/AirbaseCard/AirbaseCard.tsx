13c2e19012d149a1b9eb4e951417772c4defa149
まどの <madonoharu@gmail.com>
2020-07-11 13:22:46 +0900

    Fix AirbaseCard style

--- packages/client/src/components/organisms/AirbaseCard/AirbaseCard.tsx
+++ packages/client/src/components/organisms/AirbaseCard/AirbaseCard.tsx
@@ -1,11 +1,29 @@
 import React from "react"
 import styled from "styled-components"
-import { Airbase, AirbaseState, AirbaseKey } from "@fleethub/core"
+import { Airbase, AirbaseState } from "@fleethub/core"
 
-import { Paper } from "@material-ui/core"
+import { Paper, Typography } from "@material-ui/core"
 
-import { EquipmentList, Select } from "../../../components"
 import { Update } from "../../../utils"
+import { Flexbox, LabeledValue } from "../../atoms"
+import { Select } from "../../molecules"
+
+import EquipmentList from "../EquipmentList"
+
+const AirbaseModes = ["Sortie", "AirDefense", "Standby"] as const
+const getAirbaseModeLabel = (mode: Airbase["mode"]) => ({ Sortie: "出撃", Standby: "待機", AirDefense: "防空" }[mode])
+
+const ModeSelect: typeof Select = styled(Select)`
+  margin-left: auto;
+`
+
+const StyledLabeledValue = styled(LabeledValue)`
+  margin-right: 8px;
+`
+
+const StyledEquipmentList = styled(EquipmentList)`
+  margin: 8px 0;
+`
 
 type Props = {
   label?: string
@@ -22,18 +40,28 @@ const AirbaseCard: React.FCX<Props> = ({ className, label, airbase, update }) =>
 
   return (
     <Paper className={className}>
-      {label}
-      <Select options={["Sortie", "Standby", "AirDefense"] as const} value={airbase.mode} onChange={handleModeChange} />
-      <EquipmentList equipment={airbase.equipment} update={update} canEquip={airbase.canEquip} />
-      制空: {airbase.fighterPower} 防空: {airbase.interceptionPower} 半径: {airbase.radius}
+      <Flexbox>
+        <Typography variant="subtitle2">{label}</Typography>
+        <ModeSelect
+          options={AirbaseModes}
+          value={airbase.mode}
+          onChange={handleModeChange}
+          getOptionLabel={getAirbaseModeLabel}
+        />
+      </Flexbox>
+
+      <Flexbox>
+        <StyledLabeledValue label="制空" value={airbase.fighterPower} />
+        <StyledLabeledValue label="防空" value={airbase.interceptionPower} />
+        <StyledLabeledValue label="半径" value={airbase.radius} />
+      </Flexbox>
+
+      <StyledEquipmentList equipment={airbase.equipment} update={update} canEquip={airbase.canEquip} />
     </Paper>
   )
 }
 
 export default styled(AirbaseCard)`
+  padding: 8px;
   min-width: 160px;
-
-  ${EquipmentList} {
-    margin: 24px;
-  }
 `
