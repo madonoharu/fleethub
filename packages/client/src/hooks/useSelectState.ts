535df344ba8c41cdc643e9ef5d134f476d5f9207
まどの <madonoharu@gmail.com>
2020-07-11 09:01:28 +0900

    Fix useSelectState

--- packages/client/src/hooks/useSelectState.ts
+++ packages/client/src/hooks/useSelectState.ts
@@ -4,7 +4,7 @@ export const useSelectState = <T>(options: readonly T[], defaultOption: T = opti
   const [value, onChange] = useState(defaultOption)
 
   useEffect(() => {
-    if (!options.includes(value)) return
+    if (options.includes(value)) return
     onChange(defaultOption)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [options, onChange])
