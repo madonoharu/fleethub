8ae354540939d8805e74586120efbca3f1895631
まどの <madonoharu@gmail.com>
2020-07-16 23:57:49 +0900

    Add PlanAction

---
+++ packages/client/src/components/templates/PlanEditor/PlanAction.tsx
@@ -0,0 +1,98 @@
+import React from "react"
+import styled from "styled-components"
+import copy from "copy-to-clipboard"
+import { Plan, PlanState } from "@fleethub/core"
+import { useAsyncCallback } from "react-async-hook"
+import { createSelector } from "@reduxjs/toolkit"
+
+import {
+  Flexbox,
+  TweetButton,
+  ShareButton,
+  PlanShareContent,
+  LinkButton,
+  KctoolsButton,
+  ImportButton,
+  PlanImportForm,
+} from "../../../components"
+import { useSnackbar, useModal } from "../../../hooks"
+import { Update, openKctools } from "../../../utils"
+import { publishPlan } from "../../../firebase"
+
+const urlSelector = createSelector(
+  (plan: PlanState) => plan,
+  (plan) => publishPlan(plan)
+)
+
+type TweetOption = {
+  text: string
+  url: string
+}
+
+const tweet = ({ text, url }: TweetOption) => {
+  const tweetUrl = new URL("https://twitter.com/intent/tweet")
+  tweetUrl.searchParams.set("text", text)
+  tweetUrl.searchParams.set("url", url)
+  window.open(tweetUrl.href, "_blank", "width=480,height=400,noopener")
+}
+
+type Props = {
+  plan: Plan
+  update: Update<PlanState>
+  onPlanImport?: (plan: Plan) => void
+}
+
+const PlanAction: React.FCX<Props> = ({ className, plan, update, onPlanImport }) => {
+  const ShareModal = useModal()
+  const ImportModal = useModal()
+  const Snackbar = useSnackbar()
+
+  const asyncOnLinkClick = useAsyncCallback(
+    async () => {
+      const url = await urlSelector(plan.state)
+      const result = copy(url)
+      if (!result) throw new Error("Failed to copy")
+
+      return url
+    },
+    {
+      onSuccess: () => Snackbar.show({ message: "共有URLをコピーしました", severity: "success" }),
+      onError: (error) => {
+        console.error(error)
+        Snackbar.show({ message: "失敗しました", severity: "error" })
+      },
+    }
+  )
+
+  const asyncOnTweetClick = useAsyncCallback(async () => {
+    const url = await urlSelector(plan.state)
+    tweet({ text: `【${plan.name}】`, url })
+  })
+
+  const handleOverwrite = (plan: Plan) => {
+    update((draft) => {
+      Object.assign(draft, plan.state)
+    })
+  }
+
+  return (
+    <Flexbox className={className}>
+      <LinkButton title="共有URLをコピー" onClick={asyncOnLinkClick.execute} disabled={asyncOnLinkClick.loading} />
+      <TweetButton title="編成をツイート" onClick={asyncOnTweetClick.execute} disabled={asyncOnTweetClick.loading} />
+      <KctoolsButton title="制空権シミュレータで開く" onClick={() => openKctools(plan)} />
+      <ImportButton title="デッキビルダー形式を読み込む" onClick={ImportModal.show} />
+      <ShareButton title="デッキビルダー形式を出力したりします" onClick={ShareModal.show} />
+
+      <ShareModal>
+        <PlanShareContent plan={plan} />
+      </ShareModal>
+      <ImportModal>
+        <PlanImportForm onOverwrite={handleOverwrite} onImport={onPlanImport} />
+      </ImportModal>
+
+      <Snackbar />
+    </Flexbox>
+  )
+}
+
+export default PlanAction
--- packages/client/src/components/templates/PlanEditor/PlanEditorHeader.tsx
+++ packages/client/src/components/templates/PlanEditor/PlanEditorHeader.tsx
@@ -1,43 +1,11 @@
 import React from "react"
 import styled from "styled-components"
-import copy from "copy-to-clipboard"
 import { Plan, PlanState, FleetType } from "@fleethub/core"
-import { useAsyncCallback } from "react-async-hook"
-import { createSelector } from "@reduxjs/toolkit"
 
-import {
-  Flexbox,
-  NumberInput,
-  TweetButton,
-  ShareButton,
-  PlanShareContent,
-  LinkButton,
-  KctoolsButton,
-  ImportButton,
-  PlanImportForm,
-  TextField,
-  FleetTypeSelect,
-} from "../../../components"
-import { useSnackbar, useModal } from "../../../hooks"
-import { Update, openKctools } from "../../../utils"
-import { publishPlan } from "../../../firebase"
+import { Flexbox, NumberInput, TextField, FleetTypeSelect } from "../../../components"
+import { Update } from "../../../utils"
 
-const urlSelector = createSelector(
-  (plan: PlanState) => plan,
-  (plan) => publishPlan(plan)
-)
-
-type TweetOption = {
-  text: string
-  url: string
-}
-
-const tweet = ({ text, url }: TweetOption) => {
-  const tweetUrl = new URL("https://twitter.com/intent/tweet")
-  tweetUrl.searchParams.set("text", text)
-  tweetUrl.searchParams.set("url", url)
-  window.open(tweetUrl.href, "_blank", "width=480,height=400,noopener")
-}
+import PlanAction from "./PlanAction"
 
 const LevelInput = styled(NumberInput)`
   input {
@@ -52,10 +20,6 @@ type Props = {
 }
 
 const PlanEditorHeader: React.FCX<Props> = ({ className, plan, update, onPlanImport }) => {
-  const ShareModal = useModal()
-  const ImportModal = useModal()
-  const Snackbar = useSnackbar()
-
   const handleNameChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     update((draft) => {
       draft.name = event.currentTarget.value
@@ -65,34 +29,6 @@ const PlanEditorHeader: React.FCX<Props> = ({ className, plan, update, onPlanImp
   const handleHqLevelChange = (next: number) => {
     update((draft) => {
       draft.hqLevel = next
-    })
-  }
-
-  const asyncOnLinkClick = useAsyncCallback(
-    async () => {
-      const url = await urlSelector(plan.state)
-      const result = copy(url)
-      if (!result) throw new Error("Failed to copy")
-
-      return url
-    },
-    {
-      onSuccess: () => Snackbar.show({ message: "共有URLをコピーしました", severity: "success" }),
-      onError: (error) => {
-        console.error(error)
-        Snackbar.show({ message: "失敗しました", severity: "error" })
-      },
-    }
-  )
-
-  const asyncOnTweetClick = useAsyncCallback(async () => {
-    const url = await urlSelector(plan.state)
-    tweet({ text: `【${plan.name}】`, url })
-  })
-
-  const handleOverwrite = (plan: Plan) => {
-    update((draft) => {
-      Object.assign(draft, plan.state)
     })
   }
 
@@ -108,20 +44,7 @@ const PlanEditorHeader: React.FCX<Props> = ({ className, plan, update, onPlanImp
       <LevelInput startLabel="司令部Lv" value={plan.hqLevel} min={1} max={120} onChange={handleHqLevelChange} />
       <FleetTypeSelect fleetType={plan.fleetType} onChange={handleFleetTypeChange} />
 
-      <LinkButton title="共有URLをコピー" onClick={asyncOnLinkClick.execute} disabled={asyncOnLinkClick.loading} />
-      <TweetButton title="編成をツイート" onClick={asyncOnTweetClick.execute} disabled={asyncOnTweetClick.loading} />
-      <KctoolsButton title="制空権シミュレータで開く" onClick={() => openKctools(plan)} />
-      <ImportButton title="デッキビルダー形式を読み込む" onClick={ImportModal.show} />
-      <ShareButton title="デッキビルダー形式を出力したりします" onClick={ShareModal.show} />
-
-      <ShareModal>
-        <PlanShareContent plan={plan} />
-      </ShareModal>
-      <ImportModal>
-        <PlanImportForm onOverwrite={handleOverwrite} onImport={onPlanImport} />
-      </ImportModal>
-
-      <Snackbar />
+      <PlanAction plan={plan} update={update} onPlanImport={onPlanImport} />
     </Flexbox>
   )
 }
