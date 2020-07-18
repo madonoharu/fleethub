fd94b37025d7064e40e1a5eb0b3e1774d4b046ab
まどの <madonoharu@gmail.com>
2020-07-18 21:34:14 +0900

    Fix selectors

--- packages/client/src/store/selectors.ts
+++ packages/client/src/store/selectors.ts
@@ -1,9 +1,11 @@
 import { DefaultRootState } from "react-redux"
 
-export const getState = (root: DefaultRootState) => root.present
+const getPresentState = (root: DefaultRootState) => root.present
 
-export const selectAppState = (root: DefaultRootState) => getState(root).app
+export const selectAppState = (root: DefaultRootState) => getPresentState(root).app
 
-export const selectGearListState = (root: DefaultRootState) => getState(root).gearList
-export const selectShipListState = (root: DefaultRootState) => getState(root).shipList
-export const selectMapListState = (root: DefaultRootState) => getState(root).mapList
+export const selectFilesState = (root: DefaultRootState) => getPresentState(root).entities.files
+
+export const selectGearListState = (root: DefaultRootState) => getPresentState(root).gearList
+export const selectShipListState = (root: DefaultRootState) => getPresentState(root).shipList
+export const selectMapListState = (root: DefaultRootState) => getPresentState(root).mapList
