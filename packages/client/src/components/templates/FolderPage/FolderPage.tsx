2c31b0a44735fa1ab9711f26cea5b4047feb81d6
まどの <madonoharu@gmail.com>
2020-07-18 21:32:17 +0900

    Fix DirectoryPage to FolderPage

--- packages/client/src/components/templates/FolderPage/FolderPage.tsx
+++ packages/client/src/components/templates/FolderPage/FolderPage.tsx
@@ -6,42 +6,44 @@ import { Container, Button } from "@material-ui/core"
 import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder"
 import AddIcon from "@material-ui/icons/Add"
 
-import { Directory, filesSlice } from "../../../store"
+import { Directory, filesSlice, appSlice, FolderEntity } from "../../../store"
 
 import FileListItem from "./FileListItem"
+import { TextField } from "../../atoms"
 
 type Props = {
-  directory: Directory
+  folder: FolderEntity
 }
 
-const DirectoryPage: React.FC<Props> = ({ directory }) => {
+const FolderPage: React.FC<Props> = ({ folder }) => {
   const dispatch = useDispatch()
 
   const handlePlanCreate = () => {
-    dispatch(filesSlice.actions.createPlan({ parent: directory.id }))
+    dispatch(filesSlice.actions.createPlan({ to: folder.id }))
   }
 
   const handleFolderCreate = () => {
-    dispatch(filesSlice.actions.createFolder(directory.id))
+    dispatch(filesSlice.actions.createFolder(folder.id))
   }
 
-  const moveFileToParent = (id: string) => {
-    dispatch(filesSlice.actions.move({ id, to: directory.id }))
+  const handleNameChange = (name: string) => {
+    dispatch(filesSlice.actions.update({ id: folder.id, changes: { name } }))
   }
 
   return (
     <Container>
+      <TextField value={folder.name} onChange={(event) => handleNameChange(event.currentTarget.value)} />
       <Button onClick={handlePlanCreate} startIcon={<AddIcon />}>
         編成を作成
       </Button>
       <Button onClick={handleFolderCreate} startIcon={<CreateNewFolderIcon />}>
         フォルダを作成
       </Button>
-      {directory.children.map((id) => (
-        <FileListItem key={id} id={id} parent={directory.id} />
+      {folder.children.map((id) => (
+        <FileListItem key={id} id={id} parent={folder.id} />
       ))}
     </Container>
   )
 }
 
-export default DirectoryPage
+export default FolderPage
