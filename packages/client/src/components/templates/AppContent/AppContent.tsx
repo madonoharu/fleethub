af5414363a6efbf88e80e5a25585f862e438de4d
まどの <madonoharu@gmail.com>
2020-07-18 21:30:42 +0900

    Add FileViewer

--- packages/client/src/components/templates/AppContent/AppContent.tsx
+++ packages/client/src/components/templates/AppContent/AppContent.tsx
@@ -1,54 +1,15 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import styled from "styled-components"
+import { useAsync } from "react-async-hook"
 
-import { Container } from "@material-ui/core"
-
-import { PlanEditor, DirectoryBreadcrumbs } from "../../../components"
-import { filesSlice, openFirstPlan, selectAppState, filesSelectors, FileEntity, appSlice } from "../../../store"
+import { filesSlice, selectAppState, appSlice } from "../../../store"
 import { parseUrlEntities } from "../../../firebase"
-import { useFetch, DragLayerProvider } from "../../../hooks"
-
-import DirectoryPage from "../DirectoryPage"
+import { DragLayerProvider } from "../../../hooks"
 
 import AppBar from "./AppBar"
 import ExplorerDrawer from "./ExplorerDrawer"
-
-const renderFile = (file: FileEntity) => {
-  if (file.type === "plan") return <PlanEditor planId={file.id} />
-
-  return <DirectoryPage directory={file} />
-}
-
-const FileLoader: React.FC = () => {
-  const dispatch = useDispatch()
-
-  const file = useSelector((state) => {
-    const { fileId } = selectAppState(state)
-    return fileId && filesSelectors.selectById(state, fileId)
-  })
-
-  useFetch(async () => {
-    const entities = await parseUrlEntities()
-    if (entities) {
-      dispatch(filesSlice.actions.import({ ...entities, to: "temp" }))
-      return
-    }
-
-    if (file) return
-
-    dispatch(openFirstPlan())
-  }, [dispatch, file])
-
-  if (!file) return null
-
-  return (
-    <>
-      <DirectoryBreadcrumbs file={file} />
-      {renderFile(file)}
-    </>
-  )
-}
+import FileViewer from "./FileViewer"
 
 const Bottom = styled.div`
   height: 400px;
@@ -58,13 +19,20 @@ const AppContent: React.FC = () => {
   const dispatch = useDispatch()
   const explorerOpen = useSelector((state) => selectAppState(state).explorerOpen)
   const toggleExplorerOpen = () => dispatch(appSlice.actions.toggleExplorerOpen())
+
+  const asyncEntitiesImport = useAsync(async () => {
+    const entities = await parseUrlEntities()
+    if (!entities) return
+    dispatch(filesSlice.actions.import({ ...entities, to: "temp" }))
+  }, [])
+
+  if (asyncEntitiesImport.loading) return null
+
   return (
     <DragLayerProvider>
       <AppBar explorerOpen={explorerOpen} onExplorerOpen={toggleExplorerOpen} />
       <ExplorerDrawer open={explorerOpen}>
-        <Container>
-          <FileLoader />
-        </Container>
+        <FileViewer />
         <Bottom />
       </ExplorerDrawer>
     </DragLayerProvider>
