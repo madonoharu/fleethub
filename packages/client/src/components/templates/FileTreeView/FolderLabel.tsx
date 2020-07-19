74f82cd3fde52897fac5352273cea1f810bfe81d
まどの <madonoharu@gmail.com>
2020-07-19 13:09:04 +0900

    Fix FileTree

--- packages/client/src/components/templates/FileTreeView/FolderLabel.tsx
+++ packages/client/src/components/templates/FileTreeView/FolderLabel.tsx
@@ -10,77 +10,38 @@ import DeleteIcon from "@material-ui/icons/Delete"
 
 import { MoreVertButton, AddButton, RemoveButton, EditButton, MenuList } from "../../../components"
 import { FolderEntity } from "../../../store"
-import { usePopover } from "../../../hooks"
+import { usePopover, useFile } from "../../../hooks"
 
-import FileLabel, { FileLabelProps } from "./FileLabel"
+import FileLabel from "./FileLabel"
 
 type Props = {
   file: FolderEntity
+}
 
-  onOpen: (id: string) => void
-  onFileUpdate: (update: Update<FolderEntity>) => void
-  onPlanCreate: (id: string) => void
-  onFolderCreate: (id: string) => void
-  onCopy: (id: string) => void
-  onRemove: (id: string) => void
-} & Pick<FileLabelProps, "onMove" | "isParentOf">
-
-const FolderLabel: React.FCX<Props> = ({
-  className,
-  file,
-  onOpen,
-  onPlanCreate,
-  onFolderCreate,
-  onCopy,
-  onRemove,
-  onMove,
-  isParentOf,
-}) => {
+const FolderLabel: React.FCX<Props> = ({ className, file }) => {
   const Popover = usePopover()
 
-  const handleOpen = () => {
-    onOpen(file.id)
-  }
-
-  const handlePlanCreate = () => {
-    onPlanCreate(file.id)
-    Popover.hide()
-  }
-
-  const handleFolderCreate = () => {
-    onFolderCreate(file.id)
-    Popover.hide()
-  }
-
-  const handleCopy = () => {
-    onCopy(file.id)
-    Popover.hide()
-  }
-
-  const handleRemove = () => {
-    onRemove(file.id)
-  }
+  const { actions, canDrop } = useFile(file.id)
 
   const list = [
-    { icon: <AddIcon />, text: "編成を作成", onClick: handlePlanCreate },
-    { icon: <CreateNewFolderIcon />, text: "フォルダを作成", onClick: handleFolderCreate },
-    { icon: <FileCopyIcon />, text: "コピー", onClick: handleCopy },
-    { icon: <DeleteIcon />, text: "削除", onClick: handleRemove },
+    { icon: <AddIcon />, text: "編成を作成", onClick: actions.createPlan },
+    { icon: <CreateNewFolderIcon />, text: "フォルダを作成", onClick: actions.createFolder },
+    { icon: <FileCopyIcon />, text: "コピー", onClick: actions.copy },
+    { icon: <DeleteIcon />, text: "削除", onClick: actions.remove },
   ]
 
   return (
     <FileLabel
       className={className}
       file={file}
-      isParentOf={isParentOf}
-      onMove={onMove}
       icon={<FolderIcon />}
       text={file.name}
+      canDrop={canDrop}
+      onDrop={actions.drop}
       action={
         <>
-          <EditButton size="small" onClick={handleOpen} />
-          <AddButton size="small" onClick={handlePlanCreate} />
-          <RemoveButton size="small" onClick={handleRemove} />
+          <EditButton size="small" onClick={actions.open} />
+          <RemoveButton size="small" onClick={actions.remove} />
           <MoreVertButton size="small" onClick={Popover.show} />
           <Popover>
             <MenuList list={list} />
