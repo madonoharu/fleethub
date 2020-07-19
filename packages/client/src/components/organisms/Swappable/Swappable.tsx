eff524cf3ecc61ec36f1df27f6a0a99f26a5ad08
まどの <madonoharu@gmail.com>
2020-07-19 14:32:32 +0900

    Add useSwap

--- packages/client/src/components/organisms/Swappable/Swappable.tsx
+++ packages/client/src/components/organisms/Swappable/Swappable.tsx
@@ -1,29 +1,24 @@
 import React from "react"
 import styled from "styled-components"
-import { useForkRef, colors } from "@material-ui/core"
+import { colors } from "@material-ui/core"
 
-import { batch } from "../../../utils"
-import { useDrag, useDrop } from "../../../hooks"
+import { useSwap, SwapSpec } from "../../../hooks"
 
 const Container = styled.div`
-  transition: box-shadow 400ms ease;
-  border-radius: 4px;
   &.droppable {
+    border-radius: 4px;
     box-shadow: 0px 0px 2px 2px ${colors.yellow[300]};
   }
+
   &.dragging {
     opacity: 0.3;
   }
 `
 
-type SwappableProps<T> = {
+type SwappableProps<T> = SwapSpec<T> & {
   className?: string
   style?: React.CSSProperties
 
-  type: string
-  state: T
-  setState: (value: T) => void
-  canDrag?: boolean
   children?: React.ReactNode
 }
 
@@ -32,32 +27,19 @@ type Component = {
 }
 
 const Swappable: Component = ({ className, style, type, state, setState, canDrag, children }) => {
-  const item = { type, state, setState, canDrag }
-
   const elem = (
     <Container className={className} style={style}>
       {children}
     </Container>
   )
 
-  const dragRef = useDrag({
-    item,
-    canDrag: item.canDrag,
+  const ref = useSwap({
+    type,
+    state,
+    setState,
+    canDrag,
     dragLayer: elem,
   })
-
-  const dropRef = useDrop({
-    accept: item.type,
-    drop: (dragItem: typeof item) => {
-      batch(() => {
-        item.setState(dragItem.state)
-        dragItem.setState(item.state)
-      })
-    },
-    canDrop: (dragItem) => dragItem.state !== item.state,
-  })
-
-  const ref = useForkRef(dragRef, dropRef)
   return React.cloneElement(elem, { ref })
 }
 
