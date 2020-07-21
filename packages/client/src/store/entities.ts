f16bd7eba9ed30c86fbe16b3e0d4fefea9156653
まどの <madonoharu@gmail.com>
2020-07-21 22:03:12 +0900

    Fix cloneEntities

--- packages/client/src/store/entities.ts
+++ packages/client/src/store/entities.ts
@@ -1,15 +1,16 @@
 import { nanoid, AppThunk, Dictionary, combineReducers } from "@reduxjs/toolkit"
-import { DefaultRootState } from "react-redux"
-
-import { filesSelectors, flatFile, isDirectory, filesSlice, FhEntities } from "./filesSlice"
-import { plansSelectors, plansSlice } from "./plansSlice"
 import { isNonNullable } from "@fleethub/utils"
-import { publishFiles } from "../firebase"
+
+import { filesSelectors, flatFile, isDirectory, filesSlice, FilesData } from "./filesSlice"
+import { plansSlice } from "./plansSlice"
+import { selectEntitiesState } from "./selectors"
 
 export const entitiesReducer = combineReducers({
   files: filesSlice.reducer,
   plans: plansSlice.reducer,
 })
+
+export type EntitiesState = ReturnType<typeof entitiesReducer>
 
 const replaceAll = <T>(array: T[], searchValue: T, replaceValue: T) => {
   const cloned = array.concat()
@@ -31,38 +32,33 @@ const cloneDictionary = <T extends { id: string }>(entities: Dictionary<T>, chan
     })
     .filter(isNonNullable)
 
-export const cloneEntities = (state: DefaultRootState, entry: string): FhEntities => {
-  const fileEntities = filesSelectors.selectEntities(state)
-  const planEntities = plansSelectors.selectEntities(state)
+export const cloneEntities = (state: EntitiesState, id: string): FilesData => {
+  const fileEntities = state.files.entities
+  const planEntities = state.plans.entities
 
-  const changes: Array<[string, string]> = flatFile(fileEntities, entry).map((file) => [file.id, nanoid()])
+  const changes: Array<[string, string]> = flatFile(fileEntities, id).map((file) => [file.id, nanoid()])
+  const nextId = changes.find((change) => change[0] === id)?.[1] || ""
 
   const clonedFiles = cloneDictionary(fileEntities, changes)
   const clonedPlans = cloneDictionary(planEntities, changes)
 
   const dirs = clonedFiles.filter(isDirectory)
-  changes.forEach(([prevId, nextId]) => {
+  changes.forEach((change) => {
     dirs.forEach((dir) => {
-      dir.children = replaceAll(dir.children, prevId, nextId)
+      dir.children = replaceAll(dir.children, ...change)
     })
   })
 
-  return { entry, files: clonedFiles, plans: clonedPlans }
+  return { id: nextId, files: clonedFiles, plans: clonedPlans }
 }
 
 export const copyFile = (id: string, to?: string): AppThunk => (dispatch, getState) => {
   const state = getState()
-  const cloned = cloneEntities(state, id)
+  const cloned = cloneEntities(selectEntitiesState(state), id)
 
   if (!to) {
     to = filesSelectors.selectAll(state).find((file) => isDirectory(file) && file.children.includes(id))?.id
   }
 
   dispatch(filesSlice.actions.set({ ...cloned, to }))
-}
-
-export const shareFile = (id: string): AppThunk => (dispatch, getState) => {
-  const state = getState()
-  const cloned = cloneEntities(state, id)
-  publishFiles(cloned)
 }
