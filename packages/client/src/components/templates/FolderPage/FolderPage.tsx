59f2a6006fae05cdee9b4e3510e86d4c3f5479f3
まどの <madonoharu@gmail.com>
2020-07-21 19:49:03 +0900

    Fix FolderPage

--- packages/client/src/components/templates/FolderPage/FolderPage.tsx
+++ packages/client/src/components/templates/FolderPage/FolderPage.tsx
@@ -1,12 +1,11 @@
 import React from "react"
 import styled from "styled-components"
-import { useDispatch } from "react-redux"
 
 import { Container, Button } from "@material-ui/core"
 import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder"
 import AddIcon from "@material-ui/icons/Add"
 
-import { filesSlice, FolderEntity } from "../../../store"
+import { FolderEntity } from "../../../store"
 import { useFile } from "../../../hooks"
 
 import { TextField } from "../../atoms"
@@ -15,38 +14,40 @@ import { FileDropZone } from "../../organisms"
 import FolderPageItem from "./FolderPageItem"
 
 type Props = {
-  folder: FolderEntity
+  id: string
 }
 
-const FolderPage: React.FCX<Props> = ({ className, folder }) => {
-  const dispatch = useDispatch()
+const FolderPage: React.FCX<Props> = ({ className, id }) => {
+  const { file, actions, canDrop } = useFile(id)
 
-  const { actions, canDrop } = useFile(folder.id)
+  if (file?.type !== "folder") return null
+
+  const { name, children } = file
 
   const handlePlanCreate = () => {
-    dispatch(filesSlice.actions.createPlan({ to: folder.id }))
+    actions.createPlan()
   }
 
   const handleFolderCreate = () => {
-    dispatch(filesSlice.actions.createFolder(folder.id))
+    actions.createFolder()
   }
 
-  const handleNameChange = (name: string) => {
-    dispatch(filesSlice.actions.update({ id: folder.id, changes: { name } }))
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    actions.update({ name: event.currentTarget.value })
   }
 
   return (
     <FileDropZone className={className} onDrop={actions.drop} canDrop={canDrop}>
       <Container>
-        <TextField value={folder.name} onChange={(event) => handleNameChange(event.currentTarget.value)} />
+        <TextField value={name} onChange={handleNameChange} />
         <Button onClick={handlePlanCreate} startIcon={<AddIcon />}>
           編成を作成
         </Button>
         <Button onClick={handleFolderCreate} startIcon={<CreateNewFolderIcon />}>
           フォルダを作成
         </Button>
-        {folder.children.map((id) => (
-          <FolderPageItem key={id} id={id} parent={folder.id} />
+        {children.map((id) => (
+          <FolderPageItem key={id} id={id} parent={id} />
         ))}
       </Container>
     </FileDropZone>
