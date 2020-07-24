c6cac82327ea9c2ef2a970096cb7bd20e4a23964
まどの <madonoharu@gmail.com>
2020-07-24 21:33:07 +0900

    Improve ReactGkcoi

---
+++ packages/client/src/components/organisms/ReactGkcoi/ReactGkcoi.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { generate, DeckBuilder } from "gkcoi"
+import styled from "styled-components"
+import { useAsync } from "react-async-hook"
+
+import { CircularProgress } from "@material-ui/core"
+import { Alert } from "@material-ui/lab"
+
+import CanvasViewer from "./CanvasViewer"
+
+const StyledCircularProgress = styled(CircularProgress)`
+  display: block;
+  margin: auto;
+`
+
+type Props = {
+  deck: DeckBuilder
+}
+
+const ReactGkcoi: React.FCX<Props> = ({ className, deck }) => {
+  const asyncCanvas = useAsync(generate, [deck])
+
+  if (asyncCanvas.status === "not-requested") return null
+  if (asyncCanvas.status === "loading") return <StyledCircularProgress size={80} />
+
+  if (asyncCanvas.status === "error") {
+    return (
+      <Alert color="error" variant="outlined">
+        Error
+      </Alert>
+    )
+  }
+
+  const canvas = asyncCanvas.result
+  if (!canvas) return null
+
+  return <CanvasViewer className={className} canvas={canvas} />
+}
+
+export default ReactGkcoi
