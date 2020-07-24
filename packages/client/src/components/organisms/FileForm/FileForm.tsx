88ee1a8fea71304af4ebb7902dfa91ac72266e52
まどの <madonoharu@gmail.com>
2020-07-25 02:24:45 +0900

    Add FileForm

---
+++ packages/client/src/components/organisms/FileForm/FileForm.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import styled from "styled-components"
+
+import { FileEntity } from "../../../store"
+
+import { FolderIcon, PlanIcon, Flexbox } from "../../atoms"
+import { CopyButton, RemoveButton } from "../../molecules"
+
+import TextField from "../TextField"
+
+type Props = {
+  file: FileEntity
+  onCopy: () => void
+  onRemove: () => void
+  onNameChange: (name: string) => void
+  onDescriptionChange: (name: string) => void
+}
+
+const FileForm: React.FCX<Props> = ({ className, file, onCopy, onRemove, onNameChange, onDescriptionChange }) => {
+  const icon = file.type === "folder" ? <FolderIcon /> : <PlanIcon />
+
+  return (
+    <div className={className}>
+      <Flexbox>
+        <TextField placeholder="name" fullWidth startLabel={icon} value={file.name} onChange={onNameChange} />
+        <CopyButton title="コピーする" onClick={onCopy} />
+        <RemoveButton title="削除する" onClick={onRemove} />
+      </Flexbox>
+
+      <TextField
+        label="説明"
+        variant="outlined"
+        fullWidth
+        value={file.description}
+        onChange={onDescriptionChange}
+        multiline
+      />
+    </div>
+  )
+}
+
+export default FileForm
