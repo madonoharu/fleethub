7b76b57f1e332db6538f409bb86ebad2b3a91ea7
まどの <madonoharu@gmail.com>
2020-07-25 03:34:21 +0900

    Remove usePlanFile

--- packages/client/src/hooks/useFhPlan.ts
+++ packages/client/src/hooks/useFhPlan.ts
@@ -4,14 +4,9 @@ import { useSelector } from "react-redux"
 import { plansSelectors } from "../store"
 
 import { useFhSystem } from "./useFhSystem"
-import { useFile } from "./useFile"
 
-export const usePlanFile = (id: string) => {
+export const useFhPlan = (id: string) => {
   const { createPlan } = useFhSystem()
-
   const state = useSelector((state) => plansSelectors.selectById(state, id))
-  const plan = useMemo(() => state && createPlan(state), [createPlan, state])
-  const { file, actions } = useFile(id)
-
-  return { plan, file, actions }
+  return useMemo(() => state && createPlan(state), [createPlan, state])
 }
