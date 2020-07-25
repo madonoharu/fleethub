9b3a082cef63a69e3e80d831b80a521cb4cb9b1e
まどの <madonoharu@gmail.com>
2020-07-25 16:01:08 +0900

    Fix GearSelect

--- packages/client/src/components/organisms/EquipmentList/EquipmentList.tsx
+++ packages/client/src/components/organisms/EquipmentList/EquipmentList.tsx
@@ -7,16 +7,18 @@ import { Update } from "../../../utils"
 import EquipmentListItem, { Props as EquipmentListItemProps } from "./EquipmentListItem"
 
 type Props = {
+  type?: "airbase"
   equipment: Equipment
   update: Update<EquipmentState>
 } & Pick<EquipmentListItemProps, "canEquip" | "makeGetNextBonuses">
 
-const EquipmentList: React.FCX<Props> = ({ className, equipment, update, canEquip, makeGetNextBonuses }) => {
+const EquipmentList: React.FCX<Props> = ({ className, type, equipment, update, canEquip, makeGetNextBonuses }) => {
   return (
     <div className={className}>
       {equipment.items.map((item) => (
         <EquipmentListItem
           key={item.key}
+          type={type}
           gearKey={item.key}
           gear={item.gear}
           currentSlotSize={item.currentSlotSize}
