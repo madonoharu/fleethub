d219154bac77c17205c40a5a21f5a069e1442926
まどの <madonoharu@gmail.com>
2020-07-25 23:02:29 +0900

    Add ShipSearchResult

---
+++ packages/client/src/components/templates/ShipList/searchShip.ts
@@ -0,0 +1,22 @@
+import { ShipBase } from "@fleethub/core"
+
+const searchById = (ships: ShipBase[], searchValue: string) => {
+  const str = /^id(\d+)/.exec(searchValue)?.[1]
+  if (!str) return
+
+  const id = Number(str)
+  return ships.find((ship) => ship.shipId === id)
+}
+
+const searchShip = (ships: ShipBase[], searchValue: string) => {
+  const idFound = searchById(ships, searchValue)
+  if (idFound) {
+    return [idFound]
+  }
+
+  return ships.filter(
+    (ship) => ship.name.toUpperCase().includes(searchValue.toUpperCase()) || ship.ruby.includes(searchValue)
+  )
+}
+
+export default searchShip
