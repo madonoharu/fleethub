b1916fd2c9daeabd85a99f613a50011deebac95d
まどの <madonoharu@gmail.com>
2020-07-25 22:33:43 +0900

    Fix Checkbox

--- packages/client/src/components/atoms/Checkbox/Checkbox.tsx
+++ packages/client/src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,18 +1,18 @@
 import React from "react"
 import styled from "styled-components"
 
-import { FormControlLabel, Checkbox as MuiCheckbox } from "@material-ui/core"
+import { FormControlLabel, Checkbox as MuiCheckbox, CheckboxProps as MuiCheckboxProps } from "@material-ui/core"
 
 type Props = {
   checked?: boolean
   onChange?: (value: boolean) => void
   label?: React.ReactNode
-}
+} & Pick<MuiCheckboxProps, "size">
 
-const Checkbox: React.FCX<Props> = ({ className, checked, onChange, label }) => {
+const Checkbox: React.FCX<Props> = ({ className, checked, onChange, label, size }) => {
   const handleChange = (event: unknown, value: boolean) => onChange?.(value)
 
-  const elem = <MuiCheckbox checked={checked} onChange={handleChange} />
+  const elem = <MuiCheckbox size={size} checked={checked} onChange={handleChange} />
 
   if (!label) return elem
 
