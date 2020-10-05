4ac9225cd66d3bd5004e3bc0312f4e67a22598f6
まどの <madonoharu@gmail.com>
2020-10-05 20:21:52 +0900

    Add utils types

---
+++ packages/utils/src/fhTypes/ShipClass.ts
@@ -0,0 +1,491 @@
+export enum ShipClass {
+  /** 綾波型 */
+  AyanamiClass = 1,
+
+  /** 伊勢型 */
+  IseClass = 2,
+
+  /** 加賀型 */
+  KagaClass = 3,
+
+  /** 球磨型 */
+  KumaClass = 4,
+
+  /** 暁型 */
+  AkatsukiClass = 5,
+
+  /** 金剛型 */
+  KongouClass = 6,
+
+  /** 古鷹型 */
+  FurutakaClass = 7,
+
+  /** 高雄型 */
+  TakaoClass = 8,
+
+  /** 最上型 */
+  MogamiClass = 9,
+
+  /** 初春型 */
+  HatsuharuClass = 10,
+
+  /** 祥鳳型 */
+  ShouhouClass = 11,
+
+  /** 吹雪型 */
+  FubukiClass = 12,
+
+  /** 青葉型 */
+  AobaClass = 13,
+
+  /** 赤城型 */
+  AkagiClass = 14,
+
+  /** 千歳型 */
+  ChitoseClass = 15,
+
+  /** 川内型 */
+  SendaiClass = 16,
+
+  /** 蒼龍型 */
+  SouryuuClass = 17,
+
+  /** 朝潮型 */
+  AsashioClass = 18,
+
+  /** 長門型 */
+  NagatoClass = 19,
+
+  /** 長良型 */
+  NagaraClass = 20,
+
+  /** 天龍型 */
+  TenryuuClass = 21,
+
+  /** 島風型 */
+  ShimakazeClass = 22,
+
+  /** 白露型 */
+  ShiratsuyuClass = 23,
+
+  /** 飛鷹型 */
+  HiyouClass = 24,
+
+  /** 飛龍型 */
+  HiryuuClass = 25,
+
+  /** 扶桑型 */
+  FusouClass = 26,
+
+  /** 鳳翔型 */
+  HoushouClass = 27,
+
+  /** 睦月型 */
+  MutsukiClass = 28,
+
+  /** 妙高型 */
+  MyoukouClass = 29,
+
+  /** 陽炎型 */
+  KagerouClass = 30,
+
+  /** 利根型 */
+  ToneClass = 31,
+
+  /** 龍驤型 */
+  RyuujouClass = 32,
+
+  /** 翔鶴型 */
+  ShoukakuClass = 33,
+
+  /** 夕張型 */
+  YuubariClass = 34,
+
+  /** 海大VI型 */
+  KaidaiVIClass = 35,
+
+  /** 巡潜乙型改二 */
+  JunsenTypeBKai2 = 36,
+
+  /** 大和型 */
+  YamatoClass = 37,
+
+  /** 夕雲型 */
+  YuugumoClass = 38,
+
+  /** 巡潜乙型 */
+  JunsenTypeB = 39,
+
+  /** 巡潜3型 */
+  Junsen3Class = 40,
+
+  /** 阿賀野型 */
+  AganoClass = 41,
+
+  /** 霧の艦隊 */
+  FleetOfFog = 42,
+
+  /** 大鳳型 */
+  TaihouClass = 43,
+
+  /** 潜特型(伊400型潜水艦) */
+  I400Class = 44,
+
+  /** 特種船丙型 */
+  AkitsuMaruClass = 45,
+
+  /** 三式潜航輸送艇 */
+  Type3SubmergenceTransportVehicle = 46,
+
+  /** Bismarck級 */
+  BismarckClass = 47,
+
+  /** Z1型 */
+  Z1Class = 48,
+
+  /** 工作艦 */
+  RepairShip = 49,
+
+  /** 大鯨型 */
+  TaigeiClass = 50,
+
+  /** 龍鳳型 */
+  RyuuhouClass = 51,
+
+  /** 大淀型 */
+  OoyodoClass = 52,
+
+  /** 雲龍型 */
+  UnryuuClass = 53,
+
+  /** 秋月型 */
+  AkizukiClass = 54,
+
+  /** Admiral Hipper級 */
+  AdmiralHipperClass = 55,
+
+  /** 香取型 */
+  KatoriClass = 56,
+
+  /** UボートIXC型 */
+  TypeIXCClass = 57,
+
+  /** V.Veneto級 */
+  VittorioVenetoClass = 58,
+
+  /** 秋津洲型 */
+  AkitsushimaClass = 59,
+
+  /** 改風早型 */
+  RevisedKazahayaClass = 60,
+
+  /** Maestrale級 */
+  MaestraleClass = 61,
+
+  /** 瑞穂型 */
+  MizuhoClass = 62,
+
+  /** Graf Zeppelin級 */
+  GrafZeppelinClass = 63,
+
+  /** Zara級 */
+  ZaraClass = 64,
+
+  /** Iowa級 */
+  IowaClass = 65,
+
+  /** 神風型 */
+  KamikazeClass = 66,
+
+  /** Queen Elizabeth級 */
+  QueenElizabethClass = 67,
+
+  /** Aquila級 */
+  AquilaClass = 68,
+
+  /** Lexington級 */
+  LexingtonClass = 69,
+
+  /** C.Teste級 */
+  CommandantTesteClass = 70,
+
+  /** 巡潜甲型改二 */
+  JunsenTypeAKai2 = 71,
+
+  /** 神威型 */
+  KamoiClass = 72,
+
+  /** Гангут級 */
+  GangutClass = 73,
+
+  /** 占守型 */
+  ShimushuClass = 74,
+
+  /** 春日丸級 */
+  KasugaMaruClass = 75,
+
+  /** 大鷹型 */
+  TaiyouClass = 76,
+
+  /** 択捉型 */
+  EtorofuClass = 77,
+
+  /** Ark Royal級 */
+  ArkRoyalClass = 78,
+
+  /** Richelieu級 */
+  RichelieuClass = 79,
+
+  /** Guglielmo Marconi級 */
+  MarconiClass = 80,
+
+  /** Ташкент級 */
+  TashkentClass = 81,
+
+  /** J級 */
+  JClass = 82,
+
+  /** Casablanca級 */
+  CasablancaClass = 83,
+
+  /** Essex級 */
+  EssexClass = 84,
+
+  /** 日振型 */
+  HiburiClass = 85,
+
+  /** 呂号潜水艦 */
+  RoSeries = 86,
+
+  /** John C.Butler級 */
+  JohnCButlerClass = 87,
+
+  /** Nelson級 */
+  NelsonClass = 88,
+
+  /** Gotland級 */
+  GotlandClass = 89,
+
+  /** 日進型 */
+  NisshinClass = 90,
+
+  /** Fletcher級 */
+  FletcherClass = 91,
+
+  /** L.d.S.D.d.Abruzzi級 */
+  DucaDegliAbruzziClass = 92,
+
+  /** Colorado級 */
+  ColoradoClass = 93,
+
+  /** 御蔵型 */
+  MikuraClass = 94,
+
+  /** Northampton級 */
+  NorthamptonClass = 95,
+
+  /** Perth級 */
+  PerthClass = 96,
+
+  /** R1運送船 */
+  R1 = 97,
+
+  /** De Ruyter級 */
+  DeRuyterClass = 98,
+
+  /**  Atlanta級 */
+  AtlantaClass = 99,
+
+  /** 迅鯨型 */
+  JingeiClass = 100,
+
+  /** 松型 */
+  MatsuClass = 101,
+
+  /** 巡潜丙型 */
+  JunsenTypeC = 103,
+
+  /** 丁型海防艦 */
+  TypeDCoastalDefenseShip = 104,
+
+  /** 駆逐イ級 */
+  DestroyerIClass = 1001,
+  /** 駆逐ロ級 */
+  DestroyerRoClass = 1002,
+  /** 駆逐ハ級 */
+  DestroyerHaClass = 1003,
+  /** 駆逐ニ級 */
+  DestroyerNiClass = 1004,
+  /** 軽巡ホ級 */
+  LightCruiserHoClass = 1005,
+  /** 軽巡ヘ級 */
+  LightCruiserHeClass = 1006,
+  /** 軽巡ト級 */
+  LightCruiserToClass = 1007,
+  /** 雷巡チ級 */
+  TorpedoCruiserChiClass = 1008,
+  /** 重巡リ級 */
+  HeavyCruiserRiClass = 1009,
+  /** 軽母ヌ級 */
+  LightCarrierNuClass = 1010,
+  /** 戦艦ル級 */
+  BattleshipRuClass = 1011,
+  /** 空母ヲ級 */
+  StandardCarrierWoClass = 1012,
+  /** 輸送ワ級 */
+  TransportShipWaClass = 1013,
+  /** 潜水カ級 */
+  SubmarineKaClass = 1014,
+  /** 潜水ヨ級 */
+  SubmarineYoClass = 1015,
+  /** 戦艦タ級 */
+  BattleshipTaClass = 1016,
+  /** 戦艦レ級 */
+  BattleshipReClass = 1017,
+  /** 潜水ソ級 */
+  SubmarineSoClass = 1018,
+  /** 軽巡ツ級 */
+  LightCruiserTsuClass = 1019,
+  /** 重巡ネ級 */
+  HeavyCruiserNeClass = 1020,
+  /** 駆逐ナ級 */
+  DestroyerNaClass = 1021,
+  /** 浮遊要塞 */
+  FloatingFortress = 2001,
+  /** 泊地棲鬼 */
+  AnchorageDemon = 2002,
+  /** 泊地棲姫 */
+  AnchoragePrincess = 2003,
+  /** 装甲空母鬼 */
+  ArmoredCarrierDemon = 2004,
+  /** 装甲空母姫 */
+  ArmoredCarrierPrincess = 2005,
+  /** 南方棲鬼 */
+  SouthernDemon = 2006,
+  /** 南方棲戦鬼 */
+  SouthernWarDemon = 2007,
+  /** 南方棲戦姫 */
+  SouthernWarPrincess = 2008,
+  /** 護衛要塞 */
+  EscortFortress = 2009,
+  /** 飛行場姫 */
+  AirfieldPrincess = 2010,
+  /** 戦艦棲姫 */
+  BattleshipPrincess = 2011,
+  /** 港湾棲姫 */
+  HarbourPrincess = 2012,
+  /** 離島棲鬼 */
+  IsolatedIslandDemon = 2013,
+  /** 北方棲姫 */
+  NorthernPrincess = 2014,
+  /** 中間棲姫 */
+  MidwayPrincess = 2015,
+  /** 空母棲鬼 */
+  AircraftCarrierDemon = 2016,
+  /** 空母棲姫 */
+  AircraftCarrierPrincess = 2017,
+  /** 駆逐棲姫 */
+  DestroyerPrincess = 2018,
+  /** 空母水鬼 */
+  AircraftCarrierWaterDemon = 2019,
+  /** 軽巡棲鬼 */
+  LightCruiserDemon = 2020,
+  /** 戦艦水鬼 */
+  BattleshipWaterDemon = 2021,
+  /** 港湾水鬼 */
+  HarbourWaterDemon = 2022,
+  /** 泊地水鬼 */
+  AnchorageWaterDemon = 2023,
+  /** 水母棲姫 */
+  SeaplaneTenderPrincess = 2024,
+  /** 防空棲姫 */
+  AirDefensePrincess = 2025,
+  /** PT小鬼群 */
+  PTImpPack = 2026,
+  /** 軽巡棲姫 */
+  LightCruiserPrincess = 2027,
+  /** 潜水棲姫 */
+  SubmarinePrincess = 2028,
+  /** 駆逐水鬼 */
+  DestroyerWaterDemon = 2029,
+  /** 集積地棲姫 */
+  SupplyDepotPrincess = 2030,
+  /** 重巡棲姫 */
+  HeavyCruiserPrincess = 2031,
+  /** 砲台小鬼 */
+  ArtilleryImp = 2032,
+  /** 離島棲姫 */
+  IsolatedIslandPrincess = 2033,
+  /** 駆逐古鬼 */
+  AncientDestroyerDemon = 2034,
+  /** リコリス棲姫 */
+  LycorisPrincess = 2035,
+  /** 中枢棲姫 */
+  CentralPrincess = 2036,
+  /** 駆逐古姫 */
+  AncientDestroyerPrincess = 2037,
+  /** 潜水夏姫 */
+  SubmarineSummerPrincess = 2038,
+  /** 戦艦夏姫 */
+  BattleshipSummerPrincess = 2039,
+  /** 港湾夏姫 */
+  HarbourSummerPrincess = 2040,
+  /** 重巡夏姫 */
+  HeavyCruiserSummerPrincess = 2041,
+  /** 水母水姫 */
+  SeaplaneTenderWaterPrincess = 2042,
+  /** 深海海月姫 */
+  AbyssalJellyfishPrincess = 2043,
+  /** 深海双子棲姫 */
+  AbyssalTwinPrincesses = 2044,
+  /** 護衛棲姫 */
+  EscortPrincess = 2045,
+  /** 北端上陸姫 */
+  NorthernmostLandingPrincess = 2046,
+  /** 北方水姫 */
+  NorthernWaterPrincess = 2047,
+  /** 潜水新棲姫 */
+  NewSubmarinePrincess = 2048,
+  /** 戦艦仏棲姫 */
+  FrenchBattleshipPrincess = 2049,
+  /** 空母夏鬼 */
+  AircraftCarrierSummerDemon = 2050,
+  /** 空母夏姫 */
+  AircraftCarrierSummerPrincess = 2051,
+  /** 集積地夏姫 */
+  SupplyDepotSummerPrincess = 2052,
+  /** 欧州棲姫 */
+  EuropeanPrincess = 2053,
+  /** 海峡夜棲姫 */
+  NightStraitPrincesses = 2054,
+  /** 防空埋護姫 */
+  EntombedAntiAirGuardianPrincess = 2055,
+  /** 護衛棲水姫 */
+  EscortWaterPrincess = 2056,
+  /** 深海鶴棲姫 */
+  AbyssalCranePrincess = 2057,
+  /** 護衛独還姫 */
+  GermanEscortPrincess = 2058,
+  /** 船渠棲姫 */
+  DockPrincess = 2059,
+  /** 欧州水姫 */
+  EuropeanWaterPrincess = 2060,
+  /** 深海雨雲姫 */
+  AbyssalNimbusPrincess = 2061,
+  /** 深海日棲姫 */
+  AbyssalSunPrincess = 2062,
+  /** 北方棲妹 */
+  NorthernLittleSister = 2063,
+  /** 太平洋深海棲姫 */
+  AbyssalPacificPrincess = 2064,
+  /** 深海地中海棲姫 */
+  AbyssalMediterraneanPrincess = 2065,
+  /** アンツィオ沖棲姫 */
+  AnzioPrincess = 2066,
+  /** バタビア沖棲姫 */
+  BataviaPrincess = 2067,
+  /** 防空巡棲姫 */
+  AirDefenseCruiserPrincess = 2068,
+}
+
+export type ShipClassKey = keyof typeof ShipClass
