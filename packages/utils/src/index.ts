369aa3798359b500a8fce0618863e7495b4cb335
まどの <madonoharu@gmail.com>
2020-10-05 20:21:52 +0900

    Add utils types

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -13,6 +13,8 @@ export const mapValues = <T, R>(obj: T, fn: (value: T[keyof T], key: keyof T) =>
   return nextObj as Record<keyof T, R>
 }
 
+export const includes = <T>(array: readonly T[], value: unknown): value is T => (array as unknown[]).includes(value)
+
 export const uniq = <T>(array: T[]) => [...new Set(array)]
 
 export const atLeastOne = (xs: number[]) => 1 - xs.reduce((acc, x) => acc * (1 - x), 1)
@@ -21,3 +23,7 @@ export type Dict<K extends string | number | symbol, T> = Partial<Record<K, T>>
 
 export type NullableArray<T> = Array<T | undefined>
 export type PickByValue<T, ValueType> = Pick<T, { [Key in keyof T]-?: T[Key] extends ValueType ? Key : never }[keyof T]>
+
+export * from "./start2"
+export * from "./fhTypes"
+export * from "./templateLiteralTypes"
