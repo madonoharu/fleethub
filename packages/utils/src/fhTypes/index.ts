1b4acc948d1802cf5e07cb80eb80d697c9ceedb2
まどの <madonoharu@gmail.com>
2020-10-09 05:05:46 +0900

    Rename ruby -> yomi

--- packages/utils/src/fhTypes/index.ts
+++ packages/utils/src/fhTypes/index.ts
@@ -4,4 +4,7 @@ export * from "./ShipType"
 
 export * from "./GearId"
 export * from "./ShipId"
-export * from "./ShipRuby"
+export * from "./ShipYomi"
+
+export type SheetRow = Record<string, string | number | boolean | undefined>
+export * from "./MasterData"
