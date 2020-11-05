e0ff7dbe1d68c019048c31fe2d45108e743f2009
まどの <madonoharu@gmail.com>
2020-11-06 02:25:38 +0900

    Add getCloudinaryUrl

---
+++ packages/client/src/utils/cloudinary.ts
@@ -0,0 +1,11 @@
+export type CloudinaryOptions = {
+  publicId: string
+  folder: string
+  width: number
+  height: number
+}
+
+const cloudName = "djg1epjdj"
+
+export const getCloudinaryUrl = ({ publicId, folder, width, height }: CloudinaryOptions) =>
+  `https://res.cloudinary.com/${cloudName}/image/upload/c_scale,f_auto,q_auto,w_${width},h_${height}/${folder}/${publicId}.png`
