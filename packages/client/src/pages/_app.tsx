2ad8e74710e2adbe07b70e954de56fbe2e277f55
まどの <madonoharu@gmail.com>
2020-11-08 06:55:42 +0900

    Upgrade material-ui

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import type { AppProps } from "next/app"
 
+import { ThemeProvider } from "../styles"
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
