ba1f343f1cdaa10993044ea4d40505afa783d99a
まどの <madonoharu@gmail.com>
2020-11-08 01:33:30 +0900

    Add analyze

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,7 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const WebpackBar = require("webpackbar")
+
 module.exports = {
   env: {
     VERSION: require("./package.json").version,
+  },
+  webpack: (config, options) => {
+    config.plugins.push(new WebpackBar())
+
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static",
+          reportFilename: options.isServer ? "../analyze/server.html" : "./analyze/client.html",
+        })
+      )
+    }
+    return config
   },
 }
