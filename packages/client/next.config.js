a1d3ee83b785b645ddd17e45df18ca01ebad0bcb
まどの <madonoharu@gmail.com>
2020-11-07 21:37:20 +0900

    Fix config

---
+++ packages/client/next.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+module.exports = {
+  env: {
+    VERSION: require("./package.json").version,
+  },
+}
