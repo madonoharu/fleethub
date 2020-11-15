2b7dcb99f663282c7f0764e34ad0a116bbf4b64d
まどの <madonoharu@gmail.com>
2020-11-15 12:09:35 +0900

    Fix imports

--- packages/client/src/components/organisms/GearTooltip/index.tsx
+++ packages/client/src/components/organisms/GearTooltip/index.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import { GearBase, EquipmentBonuses } from "@fleethub/core"
-import styled from "@emotion/styled"
 
 import { Tooltip, TooltipProps } from "@material-ui/core"
 
 import { GearNameplate, Text } from "../../../components"
 
 import GearStatList from "./GearStatList"
-import { GearCategoryName } from "@fleethub/data"
 
 type ContentProps = {
   gear: GearBase
@@ -17,7 +15,7 @@ type ContentProps = {
 const Content: React.FC<ContentProps> = ({ gear, bonuses }) => (
   <div>
     <Text>
-      ID {gear.gearId} {GearCategoryName[gear.categoryId]}
+      ID {gear.gearId} {gear.categoryName}
     </Text>
     <GearNameplate wrap iconId={gear.iconId} name={gear.name} />
     <GearStatList gear={gear} bonuses={bonuses} />
