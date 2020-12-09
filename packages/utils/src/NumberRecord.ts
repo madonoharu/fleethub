db34448b1bb9d6d3204654e4c8e40d20ade51228
まどの <madonoharu@gmail.com>
2020-12-09 15:38:34 +0900

    Fix NumberRecord

--- packages/utils/src/NumberRecord.ts
+++ packages/utils/src/NumberRecord.ts
@@ -1,104 +1,96 @@
 import { uniq } from "./array"
 
-export type NumberRecordJson<K extends string | number> = Partial<Record<K, number>>
+type NumberDict<K> = K extends string ? Partial<Record<K, number>> : never
 
-export default class NumberRecord<K extends string | number> {
-  #data: NumberRecordJson<K>
+export default class NumberRecord<K> {
+  #data: Map<K, number>
 
-  static count = <K extends string | number>(array: K[]) => {
+  static count = <K>(array: K[]): NumberRecord<K> => {
     const counts = new NumberRecord<K>()
 
     array.forEach((item) => {
-      counts.mutableSet(item, counts.get(item) + 1)
+      counts.insert(item, counts.get(item) + 1)
     })
 
     return counts
   }
 
   /** Relative Frequency Distribution */
-  static rfd = <K extends string | number>(array: K[]) => NumberRecord.count(array).scale(1 / array.length)
+  static rfd = <K>(array: K[]): NumberRecord<K> => NumberRecord.count(array).scale(1 / array.length)
 
-  constructor(data: Partial<Record<K, number>> = {}) {
-    this.#data = { ...data }
-  }
-
-  public keys() {
-    return Object.keys(this.#data) as K[]
-  }
-
-  public values() {
-    return Object.values(this.#data)
-  }
-
-  public get(key: K) {
-    return (this.#data[key] || 0) as number
-  }
-
-  private mutableSet(key: K, value: number) {
-    if (value === 0) {
-      delete this.#data[key]
+  constructor(data?: NumberDict<K> | Map<K, number> | [K, number][]) {
+    if (Array.isArray(data) || data instanceof Map) {
+      data = new Map(data)
     } else {
-      this.#data[key] = value
+      data = (new Map(Object.entries(data || {})) as unknown) as Map<K, number>
+    }
+
+    this.#data = data
+  }
+
+  public insert(key: K, value: number): NumberRecord<K> {
+    if (value === 0) {
+      this.#data.delete(key)
+    } else {
+      this.#data.set(key, value)
     }
 
     return this
   }
 
-  public set(key: K, value: number) {
-    return new NumberRecord<K>(this.#data).mutableSet(key, value)
+  public keys(): K[] {
+    return [...this.#data.keys()]
   }
 
-  public sum() {
+  public values(): number[] {
+    return [...this.#data.values()]
+  }
+
+  public get(key: K): number {
+    return this.#data.get(key) || 0
+  }
+
+  public set(key: K, value: number): NumberRecord<K> {
+    return new NumberRecord<K>(this.#data).insert(key, value)
+  }
+
+  public sum(): number {
     return this.keys().reduce((value, key) => value + this.get(key), 0)
   }
 
-  public map(fn: (value: number, key: K) => number) {
+  public map(fn: (value: number, key: K) => number): NumberRecord<K> {
     const next = new NumberRecord<K>()
 
     this.keys().forEach((key) => {
-      next.mutableSet(key, fn(this.get(key), key))
+      next.insert(key, fn(this.get(key), key))
     })
 
     return next
   }
 
-  public add(data: NumberRecord<K> | NumberRecordJson<K>): NumberRecord<K> {
-    const result = new NumberRecord<K>()
+  public update(key: K, fn: (value: number, key: K) => number): NumberRecord<K> {
+    const value = this.get(key)
+    return this.set(key, fn(value, key))
+  }
 
-    let record: NumberRecord<K>
-    if (data instanceof NumberRecord) {
-      record = data
-    } else {
-      record = new NumberRecord(data)
-    }
+  public add(record: NumberRecord<K>): NumberRecord<K> {
+    const result = new NumberRecord<K>()
 
     const keys = uniq([...this.keys(), ...record.keys()])
 
     keys.forEach((key) => {
       const value = this.get(key) + record.get(key)
-      result.mutableSet(key, value)
+      result.insert(key, value)
     })
 
     return result
   }
 
-  public scale(multiplier: number) {
+  public scale(multiplier: number): NumberRecord<K> {
     return this.map((value) => value * multiplier)
   }
 
-  public toArray() {
-    const array = new Array<[K, number]>()
-
-    Object.entries(this.#data).forEach((entry) => {
-      if (typeof entry[1] === "number") {
-        array.push(entry as [K, number])
-      }
-    })
-
-    return array
-  }
-
-  public toJSON() {
-    return { ...this.#data }
+  public toArray(): [K, number][] {
+    return [...this.#data.entries()]
   }
 }
