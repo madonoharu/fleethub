b4de31f444094d15dc8a3b1c827517c1e40a7eb3
まどの <madonoharu@gmail.com>
2020-12-14 02:41:40 +0900

    Add tests

---
+++ packages/utils/tests/misc.test.ts
@@ -0,0 +1,15 @@
+import { isNonNullable, range } from "../src"
+
+describe("utils/misc", () => {
+  it("range", () => {
+    expect(range(5)).toEqual([0, 1, 2, 3, 4, 5])
+  })
+
+  it("cloneJson", () => {
+    expect(isNonNullable(0)).toBe(true)
+    expect(isNonNullable(false)).toBe(true)
+    expect(isNonNullable("")).toBe(true)
+    expect(isNonNullable(undefined)).toBe(false)
+    expect(isNonNullable(null)).toBe(false)
+  })
+})
---
+++ packages/utils/tests/string.test.ts
@@ -0,0 +1,11 @@
+import { capitalize, uncapitalize } from "../src"
+
+describe("utils/string", () => {
+  it("capitalize", () => {
+    expect(capitalize("red")).toBe("Red")
+  })
+
+  it("uncapitalize", () => {
+    expect(uncapitalize("SkyBlue")).toBe("skyBlue")
+  })
+})
