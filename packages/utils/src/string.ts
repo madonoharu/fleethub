0d108992df57ad939cf2ecb62f33fdedcf4cee42
まどの <madonoharu@gmail.com>
2020-12-14 02:41:01 +0900

    Fix utils

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,5 +1,3 @@
-export const isString = (value: unknown): value is string => typeof value === "string"
-
 export const capitalize = <T extends string>(str: T) => (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>
 
 export const uncapitalize = <T extends string>(str: T) =>
