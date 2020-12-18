4daaad91d5baf878e5d55ef4b0fdc267ab7a308a
まどの <madonoharu@gmail.com>
2020-12-18 19:50:42 +0900

    Fix ShipDetailScreen

--- packages/client/src/components/organisms/ShipDetailScreen/ShipDetailScreen.tsx
+++ packages/client/src/components/organisms/ShipDetailScreen/ShipDetailScreen.tsx
@@ -1,22 +1,33 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
-import { Ship, ShipState } from "@fleethub/core"
+import { Ship, ShipState, FleetType, Plan, Formation, AirState } from "@fleethub/core"
 import styled from "@emotion/styled"
 
-import { NumberInput, ShipBanner } from "../../molecules"
+import { NumberInput, ShipBanner, StatIcon } from "../../molecules"
 
 type ShipDetailScreenProps = {
   ship: Ship
   onChange?: (state: ShipState) => void
 }
 
-const ShipDetailScreen: React.FCX<ShipDetailScreenProps> = ({ className, ship, onChange }) => {
-  const statKeys = ["firepower", "torpedo", "antiAir", "armor"] as const
+type ShipPosition = {
+  isFlagship: boolean
+  index: number
+  fleetType: FleetType
+}
 
-  const { t, i18n } = useTranslation()
+type BattleContext = {
+  formation: Formation
+  airState: AirState
+}
+
+const ShipDetailScreen: React.FCX<ShipDetailScreenProps> = ({ className, ship, onChange }) => {
+  const statKeys = ["maxHp", "firepower", "torpedo", "antiAir", "armor", "asw", "los"] as const
+
+  const { t } = useTranslation("terms")
 
   const renderStat = (key: typeof statKeys[number]) => {
-    const { value, diff, naked, equipment, bonus } = ship[key]
+    const { value, diff } = ship[key]
 
     const handleChange = (next: number) => {
       const delta = next - value
@@ -25,10 +36,17 @@ const ShipDetailScreen: React.FCX<ShipDetailScreenProps> = ({ className, ship, o
 
     return (
       <div key={key} css={{ display: "flex" }}>
-        <NumberInput startLabel={t(key)} value={value} onChange={handleChange} />
-        <div>
-          素{naked} + 装備{equipment} + ボーナス{bonus} {t("test")}
-        </div>
+        <NumberInput
+          startLabel={
+            <div css={{ display: "flex", fontSize: "0.75rem" }}>
+              <StatIcon icon={key} />
+              {t(key)}
+            </div>
+          }
+          value={value}
+          onChange={handleChange}
+        />
+        <div>{diff}</div>
       </div>
     )
   }
