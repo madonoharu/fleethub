3b96a502db25001d02bc8cb4523ad8056bd7a298
まどの <madonoharu@gmail.com>
2020-12-18 19:48:05 +0900

    Add AttackAnalysisScreen

---
+++ packages/client/src/components/organisms/AttackAnalysisScreen/HitRateDetailScreen.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { HitRateDetail } from "@fleethub/core"
+import { Typography } from "@material-ui/core"
+
+import { toPercent } from "../../../utils"
+
+type HitRateDetailScreenProps = {
+  detail: HitRateDetail
+}
+
+const HitRateDetailScreen: React.FC<HitRateDetailScreenProps> = ({ detail }) => {
+  return (
+    <Typography component="div">
+      <p>命中率 {toPercent(detail.hitRate)}</p>
+      <p>通常命中率 {toPercent(detail.normalRate)}</p>
+      <p>クリティカル率 {toPercent(detail.criticalRate)}</p>
+
+      <p>命中項 {detail.accuracyTerm}</p>
+      <p>回避項 {detail.evasionTerm}</p>
+      <p>疲労補正 {detail.moraleModifier}</p>
+
+      <p>クリティカル率係数 {detail.criticalRateMultiplier}</p>
+      <p>クリティカル率ボーナス {detail.criticalRateBonus}</p>
+    </Typography>
+  )
+}
+
+export default HitRateDetailScreen
