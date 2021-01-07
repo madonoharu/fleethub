ea7b5990e2378473e00b00ffe34971a0830562cc
まどの <madonoharu@gmail.com>
2021-01-07 17:36:46 +0900

    Remove greet

--- packages/sim/src/lib.rs
+++ packages/sim/src/lib.rs
@@ -1,16 +1,15 @@
-#![feature(concat_idents)]
-
 mod const_id;
 mod constants;
 mod factory;
 mod gear;
+mod gear_list;
 mod master;
 mod ship;
 
-use wasm_bindgen::prelude::*;
-use web_sys::console::log_1;
-
-#[wasm_bindgen]
-pub fn greet() {
-    log_1(&JsValue::from("Hello, sim!".to_string()));
-}
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
