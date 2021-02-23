ad796474d98cb728b2aca8b74bb518fae1792b02
まどの <madonoharu@gmail.com>
2021-02-23 10:32:55 +0900

    Add test

--- packages/sim/src/utils/random_range.rs
+++ packages/sim/src/utils/random_range.rs
@@ -16,3 +16,33 @@ pub trait RandomRange<T> {
         self.range().map(|v| self.gen(v)).collect()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand_xoshiro::Xoshiro256PlusPlus;
+
+    struct TestStruct {
+        base: i32,
+    }
+
+    impl RandomRange<i32> for TestStruct {
+        fn gen(&self, random_value: i32) -> i32 {
+            random_value + 1
+        }
+
+        fn range(&self) -> Range<i32> {
+            0..self.base
+        }
+    }
+
+    #[test]
+    fn test_random_range() {
+        use rand::prelude::*;
+
+        let obj = TestStruct { base: 4 };
+        let mut rng = Xoshiro256PlusPlus::seed_from_u64(0);
+        assert_eq!(obj.to_vec(), vec![1, 2, 3, 4]);
+        assert_eq!(obj.choose(&mut rng), 2);
+    }
+}
