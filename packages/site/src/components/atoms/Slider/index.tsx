f1d0b9ae91ef94471621861a2a76c1544dfa8dca
まどの <madonoharu@gmail.com>
2021-05-30 00:49:31 +0900

    fix Slider

--- packages/site/src/components/atoms/Slider/index.tsx
+++ packages/site/src/components/atoms/Slider/index.tsx
@@ -7,15 +7,16 @@ type Props = Omit<SliderProps, "onChange" | "value"> & {
   onChange?: (value: number) => void;
 };
 
-const Component = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
+const Slider = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   const { onChange } = props;
   const handleChange = React.useCallback(
-    (event: React.ChangeEvent<{}>, value: number | number[]) => {
+    (event: Event, value: number | number[]) => {
       if (typeof value === "number" && onChange) onChange(value);
     },
     [onChange]
   );
-  return <MuiSlider {...props} onChange={handleChange} />;
+
+  return <MuiSlider ref={ref} {...props} onChange={handleChange} />;
 });
 
-export default styled(Component)``;
+export default styled(Slider)``;
