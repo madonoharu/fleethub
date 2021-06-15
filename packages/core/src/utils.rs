3c23e05111fcaa06df88ab7989fdb959bfcf34ce
まどの <madonoharu@gmail.com>
2021-06-15 12:44:01 +0900

    add xxh3

--- packages/core/src/utils.rs
+++ packages/core/src/utils.rs
@@ -1,7 +1,9 @@
 mod num_map;
 mod random_range;
 mod to_distribution;
+mod xxh3;
 
 pub use num_map::NumMap;
 pub use random_range::RandomRange;
 pub use to_distribution::ToDistribution;
+pub use xxh3::xxh3;
