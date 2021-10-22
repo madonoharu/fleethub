6300f0b9c0a902f800844fcba2139b9826593212
MadonoHaru <madonoharu@gmail.com>
2021-10-22 20:10:07 +0900

    0.0.2

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,7 +2,6 @@ export * from "./array";
 export * from "./constants";
 export * from "./math";
 export * from "./misc";
-export { default as NumberRecord } from "./NumberRecord";
 export * from "./object";
 export * from "./string";
 export * from "./types";
