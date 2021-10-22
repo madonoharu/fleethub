28e060071bd4ab7ff7d4ceff805bbca8bbd0e012
MadonoHaru <madonoharu@gmail.com>
2021-10-22 20:00:07 +0900

    add build-core

---
+++ scripts/build-core.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+PKG_DIR=packages/core/pkg
+NODE_DIR=packages/core/node
+
+export WASM_BINDGEN_WEAKREF=1
+wasm-pack build crates/fh-core -d $(realpath $PKG_DIR)
+wasm-pack build crates/fh-core -d $(realpath $NODE_DIR) -t nodejs
+
+rm -f $PKG_DIR/package.json
+rm -f $PKG_DIR/README.md
+rm -f $PKG_DIR/.gitignore
+rm -f $NODE_DIR/package.json
+rm -f $NODE_DIR/README.md
+rm -f $NODE_DIR/.gitignore
