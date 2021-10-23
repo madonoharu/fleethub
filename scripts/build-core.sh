83140d3e71a4516efa9aabbfed5329d76d8bf50e
MadonoHaru <madonoharu@gmail.com>
2021-10-23 15:52:53 +0900

    shをbashに変更

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -6,12 +6,7 @@ PKG_DIR=packages/fleethub-core/pkg
 NODE_DIR=packages/fleethub-core/node
 
 export WASM_BINDGEN_WEAKREF=1
-wasm-pack build crates/fh-core -d $(realpath $PKG_DIR)
-wasm-pack build crates/fh-core -d $(realpath $NODE_DIR) -t nodejs
+wasm-pack build crates/fh-core -d "$(realpath $PKG_DIR)"
+wasm-pack build crates/fh-core -d "$(realpath $NODE_DIR)" -t nodejs
 
-rm -f $PKG_DIR/package.json
-rm -f $PKG_DIR/README.md
-rm -f $PKG_DIR/.gitignore
-rm -f $NODE_DIR/package.json
-rm -f $NODE_DIR/README.md
-rm -f $NODE_DIR/.gitignore
+rm -rf {$PKG_DIR,$NODE_DIR}/{package.json,README.md,.gitignore}
