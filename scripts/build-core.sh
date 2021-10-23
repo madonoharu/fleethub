83bf54e972099d9ea4a0dd8faec5fe7cb2177d53
MadonoHaru <madonoharu@gmail.com>
2021-10-23 16:19:50 +0900

    ts-genを追加

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -8,5 +8,6 @@ NODE_DIR=packages/fleethub-core/node
 export WASM_BINDGEN_WEAKREF=1
 wasm-pack build crates/fh-core -d "$(realpath $PKG_DIR)"
 wasm-pack build crates/fh-core -d "$(realpath $NODE_DIR)" -t nodejs
+cargo run ts-gen
 
 rm -rf {$PKG_DIR,$NODE_DIR}/{package.json,README.md,.gitignore}
