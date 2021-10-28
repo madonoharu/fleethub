9fcec050206e632342b999895663bce974642936
MadonoHaru <madonoharu@gmail.com>
2021-10-29 03:23:59 +0900

    :wrench: next.configを修正

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -1,7 +1,8 @@
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const withTM = require("next-transpile-modules")([]);
 const { i18n } = require("./next-i18next.config");
 
+/**
+ * @type import("next").NextConfig
+ */
 const config = {
   env: {
     VERSION: require("./package.json").version,
@@ -10,27 +11,12 @@ const config = {
   localePath: "./public/locales",
 
   webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "webassembly/sync",
-    });
-
-    config.experiments.syncWebAssembly = true;
+    config.experiments.asyncWebAssembly = true;
     config.output.webassemblyModuleFilename =
       (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
 
-    if (process.env.ANALYZE) {
-      config.plugins.push(
-        new BundleAnalyzerPlugin({
-          analyzerMode: "static",
-          reportFilename: isServer
-            ? "../analyze/server.html"
-            : "./analyze/client.html",
-        })
-      );
-    }
     return config;
   },
 };
 
-module.exports = withTM(config);
+module.exports = config;
