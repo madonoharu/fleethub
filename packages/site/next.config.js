8d38ef976a9966577d8276374e0e75ce7dfe54f2
MadonoHaru <madonoharu@gmail.com>
2021-10-30 23:37:57 +0900

    :bug: nftTracingを追加

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -10,10 +10,15 @@ const config = {
   i18n,
   localePath: "./public/locales",
 
+  experimental: {
+    // https://github.com/vercel/next.js/issues/24700
+    nftTracing: true,
+  },
+
   webpack: (config, { isServer }) => {
     config.experiments.asyncWebAssembly = true;
     config.output.webassemblyModuleFilename =
-      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
+      (isServer ? "../" : "./") + "static/wasm/webassembly.wasm";
 
     return config;
   },
