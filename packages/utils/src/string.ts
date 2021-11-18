ca801db595f9648f6cdc0d80113eb4717acd6cfd
MadonoHaru <madonoharu@gmail.com>
2021-11-19 02:24:04 +0900

    :sparkles: uppercase, lowercaseを追加

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,5 +1,15 @@
-export const capitalize = <T extends string>(str: T) =>
-  (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
+export function uppercase<T extends string>(str: T): Uppercase<T> {
+  return str.toUpperCase() as Uppercase<T>;
+}
 
-export const uncapitalize = <T extends string>(str: T) =>
-  (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
+export function lowercase<T extends string>(str: T): Lowercase<T> {
+  return str.toLowerCase() as Lowercase<T>;
+}
+
+export function capitalize<T extends string>(str: T): Capitalize<T> {
+  return (str.charAt(0).toUpperCase() + str.slice(1)) as Capitalize<T>;
+}
+
+export function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
+  return (str.charAt(0).toLowerCase() + str.slice(1)) as Uncapitalize<T>;
+}
