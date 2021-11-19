f64c2c89326830339ec3f56b5c81cdcb2cd23449
MadonoHaru <madonoharu@gmail.com>
2021-11-19 18:46:30 +0900

    :art: next.configを修正

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -1,5 +1,25 @@
 const { i18n } = require("./next-i18next.config");
 
+// https://github.com/vercel/next.js/issues/29362#issuecomment-971377869
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: "WasmChunksFixPlugin" },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return;
+            compilation.deleteAsset(pathname);
+
+            const name = pathname.split("/")[1];
+            const info = compilation.assetsInfo.get(pathname);
+            compilation.emitAsset(name, source, info);
+          })
+      );
+    });
+  }
+}
+
 /**
  * @type import("next").NextConfig
  */
@@ -15,12 +35,30 @@ const config = {
     esmExternals: false,
   },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, dev }) => {
     config.experiments.asyncWebAssembly = true;
-    config.output.webassemblyModuleFilename =
-      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
+
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+      config.plugins.push(new WasmChunksFixPlugin());
+    }
 
     return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:all*.wasm",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
