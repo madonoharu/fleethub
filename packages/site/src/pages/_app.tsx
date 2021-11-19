f53c37c66724bff9e779d7e498b3b90a46fe7172
MadonoHaru <madonoharu@gmail.com>
2021-11-19 21:46:08 +0900

    :adhesive_bandage: fix index

--- packages/site/src/pages/_app.tsx
+++ packages/site/src/pages/_app.tsx
@@ -1,48 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import { createEquipmentBonuses } from "equipment-bonus";
 import { NextComponentType } from "next";
 import { appWithTranslation } from "next-i18next";
 import { AppContext, AppInitialProps, AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import React from "react";
 
-import { fetchMasterData } from "../firebase";
-import { FhCoreContext } from "../hooks";
-import { StoreProvider } from "../store";
-
 import { createEmotionCache, ThemeProvider } from "../styles";
-
-const loader = async () => {
-  const [module, masterData] = await Promise.all([
-    import("fleethub-core"),
-    fetchMasterData(),
-  ]);
-
-  if (process.env.NODE_ENV === "development") {
-    module.set_panic_hook();
-  }
-
-  let core: import("fleethub-core").FhCore | null;
-
-  try {
-    core = new module.FhCore(masterData, createEquipmentBonuses);
-  } catch (error) {
-    console.error(error);
-  }
-
-  const App: React.FC = ({ children }) => {
-    return (
-      <FhCoreContext.Provider value={core && { masterData, module, core }}>
-        <StoreProvider masterData={masterData}>{children}</StoreProvider>
-      </FhCoreContext.Provider>
-    );
-  };
-
-  return App;
-};
-
-const FhCoreProvider = dynamic(loader);
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -59,9 +22,7 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, MyAppProps> = ({
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider>
-        <FhCoreProvider>
-          <Component {...pageProps} />
-        </FhCoreProvider>
+        <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   );
--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,12 +1,48 @@
 /** @jsxImportSource @emotion/react */
+import { createEquipmentBonuses } from "equipment-bonus";
 import type { GetStaticProps, NextComponentType, NextPageContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { AppContent } from "../components/templates";
+import { fetchMasterData } from "../firebase";
+import { FhCoreContext } from "../hooks";
+import { StoreProvider } from "../store";
+
+const loader = async () => {
+  const [module, masterData] = await Promise.all([
+    import("fleethub-core"),
+    fetchMasterData(),
+  ]);
+
+  if (process.env.NODE_ENV === "development") {
+    module.set_panic_hook();
+  }
+
+  let core: import("fleethub-core").FhCore | null;
+
+  try {
+    core = new module.FhCore(masterData, createEquipmentBonuses);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const Provider: React.FC = ({ children }) => {
+    return (
+      <FhCoreContext.Provider value={core && { masterData, module, core }}>
+        <StoreProvider masterData={masterData}>{children}</StoreProvider>
+      </FhCoreContext.Provider>
+    );
+  };
+
+  return Provider;
+};
+
+const FhCoreProvider = dynamic(loader);
 
 const Index: NextComponentType<NextPageContext, unknown> = () => {
   return (
@@ -15,23 +51,28 @@ const Index: NextComponentType<NextPageContext, unknown> = () => {
         <title>作戦室</title>
       </Head>
 
-      <DndProvider backend={HTML5Backend}>
-        <AppContent />
-      </DndProvider>
+      <FhCoreProvider>
+        <DndProvider backend={HTML5Backend}>
+          <AppContent />
+        </DndProvider>
+      </FhCoreProvider>
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale = "" }) => {
+  const ssrConfig = await serverSideTranslations(locale, [
+    "common",
+    "gears",
+    "gear_types",
+    "ships",
+    "ctype",
+  ]);
+
   return {
+    revalidate: 1,
     props: {
-      ...(await serverSideTranslations(locale, [
-        "common",
-        "gears",
-        "gear_types",
-        "ships",
-        "ctype",
-      ])),
+      ...ssrConfig,
     },
   };
 };
