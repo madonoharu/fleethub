56ee57a5ba55b9eb52befb1a9e11274bdceb857c
Madono Haru <madonoharu@gmail.com>
2022-01-22 18:03:32 +0900

    :recycle: fleethub-coreの出力先を変更

--- packages/fleethub-core/tests/ship.test.ts
+++
@@ -1,8 +0,0 @@
-import { Ship } from "..";
-
-it("Ship", () => {
-  const ship = Ship.default();
-  expect(ship.ship_type).toBe("Unknown");
-  expect(ship.ship_class).toBe("Unknown");
-  expect(ship.has_attr("Kai2")).toBe(false);
-});
