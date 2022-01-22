8f31d774790ab1145f3f02154513ad82648124cc
Madono Haru <madonoharu@gmail.com>
2022-01-22 18:03:32 +0900

    :recycle: fleethub-coreの出力先を変更

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-PREFIX=packages/fleethub-core
-PKG_DIR=$PREFIX/pkg
-NODE_DIR=$PREFIX/node
+BUILD_PATH=crates/fleethub-core
 
 export WASM_BINDGEN_WEAKREF=1
-wasm-pack build crates/fleethub-core -d "$(realpath $PKG_DIR)"
-wasm-pack build crates/fleethub-core -d "$(realpath $NODE_DIR)" -t nodejs
-cargo run ts-gen $PREFIX/bindings.d.ts
+wasm-pack build $BUILD_PATH
+cargo run ts-gen $BUILD_PATH/bindings.d.ts
 
-rm -rf {$PKG_DIR,$NODE_DIR}/{package.json,README.md,.gitignore}
+rm -rf $BUILD_PATH/pkg/{package.json,README.md,.gitignore}
 
 VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "fleethub-core") | .version')
-cat $PREFIX/package.json | jq ".version |= ${VERSION}" > $PREFIX/package.json.tmp
-mv $PREFIX/package.json.tmp $PREFIX/package.json
+cat $BUILD_PATH/package.json | jq ".version |= ${VERSION}" > $BUILD_PATH/package.json.tmp
+mv $BUILD_PATH/package.json.tmp $BUILD_PATH/package.json
